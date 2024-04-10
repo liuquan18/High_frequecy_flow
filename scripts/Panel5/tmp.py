471cb693bf2ac8f2dfb34db91215a0227d175438
Quan <quan.liu@mpimet.mpg.de>
2024-04-10 15:38:27 +0200

    nothing

---
+++ scripts/Panel5/tmp.py
@@ -0,0 +1,17 @@
+# %%
+import xarray as xr
+import numpy as np
+import pandas as pd
+
+dict = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6}
+
+
+def func1(a, b):
+    print(
+        f"a={a}",
+        f"b = {b}",
+    )
+
+
+# %%
+# The format is terrible, Ctrl + S to change
---
+++ scripts/spctrum_example/periodgram.py
@@ -0,0 +1,24 @@
+#%%
+import xarray as xr
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+rng = np.random.default_rng()
+
+# %%
+fs = 10e3
+N = 1e5
+amp = 2*np.sqrt(2)
+freq = 1234.0
+noise_power = 0.001 * fs / 2
+time = np.arange(N) / fs
+x = amp*np.sin(2*np.pi*freq*time)
+x += rng.normal(scale=np.sqrt(noise_power), size=time.shape)
+# %%
+f, Pxx_den = signal.periodogram(x, fs, )
+plt.semilogy(f, Pxx_den)
+plt.ylim([1e-7, 1e2])
+plt.xlabel('frequency [Hz]')
+plt.ylabel('PSD [V**2/Hz]')
+plt.show()
+# %%
