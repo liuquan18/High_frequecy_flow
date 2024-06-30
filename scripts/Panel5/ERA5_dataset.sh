fcbbe08ef9f7b3d1e2e71ffe9c247d16a26ab99e
Quan <quan.liu@mpimet.mpg.de>
2024-06-30 15:06:20 +0200

    no old files

--- scripts/Panel5/ERA5_dataset.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-module load cdo
-
-# to data directory
-cd /pool/data/ERA5/E5/pl/an/1H/129/
-
-# merge files starting with "1980-06"
-cdo -f nc -P 36 -mergetime -apply,-sellevel,50000 [ E5pl00_1H_1980-06* ] /work/mh0033/m300883/High_frequecy_flow/data/example_blocking_event/ERA5_1980-06.nc
