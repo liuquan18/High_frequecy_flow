3c3a208a12d421a5ba94deb84e8a63ed79cd9f79
Quan <quan.liu@mpimet.mpg.de>
2024-06-30 17:40:27 +0200

    ensmean

---
+++ script/pre_process/ens_mean_monthly.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+module load cdo
+
+month=$1
+echo "Ensemble mean for month ${month}"
+
+FROM=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/season/zg_${month}/
+TO=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/season/zg_${month}_ensmean/
+
+cdo ensmean ${FROM}*.nc ${TO}zg_Amon_MPI-ESM1-2-LR_HIST_ssp585_ensmean_${month}.nc
