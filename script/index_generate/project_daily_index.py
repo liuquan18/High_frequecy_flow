493911b6fff01b2f17701f8e9cba7f5124647c6f
Quan <quan.liu@mpimet.mpg.de>
2024-07-01 19:35:10 +0200

    project daily index

---
+++ script/index_generate/project_daily_index.py
@@ -0,0 +1,35 @@
+#%%
+import xarray as xr
+from src.index_generate.project_index import project_field_to_pattern
+import mpi4py.MPI as MPI
+import numpy as np
+
+# %%
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+name = MPI.Get_processor_name()
+
+# %%
+period = 'first10'
+year_range = '1850_1859'
+# period = 'last10'
+# year_range = '2091_2100'
+
+daily_field_path = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/daily/zg_JJA_ano_{period}"
+eof_path = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/season/EOF_result/{period[:-2]}_pattern_projected.nc"
+# %%
+members_all = list(range(1, 51)) # all members
+# %%
+members_single = np.array_split(members_all, size)[rank] # members on this core
+# %%
+for i, member in enumerate (members_single):
+    print(f"Rank {rank}, member {member}/{members_single[-1]}")
+    daily_field = xr.open_dataset(f"{daily_field_path}/zg_day_MPI-ESM1-2-LR_historical_r{member}i1p1f1_gn_18500601-18590831_ano.nc")
+    daily_field = daily_field.zg.sel(plev = 50000).drop('plev')
+    eof = xr.open_dataset(eof_path).__xarray_dataarray_variable__
+
+    projected_pcs = project_field_to_pattern(daily_field, eof, standard=False)
+    projected_pcs.to_netcdf(f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/daily/projected_pc/zg_JJA_ano_{year_range}_r{member}_nonstd.nc")
+    
+#%%
