b45eafc56448a02f04ca2c98e9be3c8685f4aed0
Quan <quan.liu@mpimet.mpg.de>
2024-07-02 10:55:47 +0200

    standarize the index with mean and std of all ens and time

---
+++ script/index_generate/standardize_daily_index.py
@@ -0,0 +1,25 @@
+#%%
+import xarray as xr
+import numpy as np
+import glob
+# %%
+period = "last10"
+# %%
+# calculate the mean and std from all members and time
+all_pcs = xr.open_mfdataset(f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/daily/projected_pc_{period}_nonstd/*.nc", 
+                            combine = 'nested', concat_dim = 'member')
+mean = all_pcs.pc.mean()
+std = all_pcs.pc.std()
+# %%
+# standardize every member and save to a new folder with same name
+from_folder = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/daily/projected_pc_{period}_nonstd/"
+to_folder = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/projected_pc/projected_pc_{period}/"
+
+for file in glob.glob(from_folder + "*.nc"):
+    member = file.split("_")[-2]
+    pc = xr.open_dataset(file).pc
+    pc = (pc - mean) / std
+
+    pc.to_netcdf(to_folder + file.split("/")[-1][:-10]+".nc")
+
+# %%
