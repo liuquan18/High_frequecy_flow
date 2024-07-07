c81acea408b65cb9e8048e8d904964663214f99f
Quan <quan.liu@mpimet.mpg.de>
2024-07-07 10:52:28 +0200

    make the normal year and leap year the dayofyear the same

--- src/extremes/extreme_threshold.py
+++ src/extremes/extreme_threshold.py
@@ -8,8 +8,22 @@ def threshold(pc, N = 7):
     df_window = pd.concat([df['pc'].shift(periods = i, freq = '1D').rename(i) for i in [-3,-2,-1,0,1,2,3]], axis=1).dropna(axis = 0, how = 'any')
     df_stack = df_window.stack().reset_index()
     df_stack.columns = ['time','window','pc']
+
+    # make the dayofyear the same for both the normal year and leap year
+    times = pd.to_datetime(df_stack.time.values)
+
+    # Identify leap years
+    is_leap_year = times.is_leap_year
+
+    # Adjust dayofyear for dates from March 1st onward in leap years
+    adjusted_dayofyear = times.dayofyear - is_leap_year * ((times.month > 2).astype(int))
+
+    # Now, incorporate this adjustment back into your xarray object
+    df_stack['adjusted_dayofyear'] = adjusted_dayofyear
+
+
     # groupby dayofyear 
-    G = df_stack.groupby(df_stack.time.dt.dayofyear)
+    G = df_stack.groupby('adjusted_dayofyear')
     # 1.5 standard deviation as the threshold, suppose the mean is already zero (anomaly data)
     pos_threshold = 1.5 * G['pc'].std()
     neg_threshold = -1.5 * G['pc'].std()
