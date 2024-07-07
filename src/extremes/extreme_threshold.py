db758e588f85d88d9ba5373983e9145be8521156
Quan <quan.liu@mpimet.mpg.de>
2024-07-07 19:03:17 +0200

    threshold using all ens

--- src/extremes/extreme_threshold.py
+++ src/extremes/extreme_threshold.py
@@ -1,19 +1,13 @@
 #%%
-import xarray as xr
 import pandas as pd
-import datetime
 # %%
-def threshold(pc, N = 7):
+def threshold(df):
     """
     extract the 1.5 standard deviation as the threshold
     """
-    df = pc.to_dataframe()
-    df_window = pd.concat([df['pc'].shift(periods = i, freq = '1D').rename(i) for i in [-3,-2,-1,0,1,2,3]], axis=1).dropna(axis = 0, how = 'any')
-    df_stack = df_window.stack().reset_index()
-    df_stack.columns = ['time','window','pc']
 
     # make the dayofyear the same for both the normal year and leap year
-    times = pd.to_datetime(df_stack.time.values)
+    times = pd.to_datetime(df.time.values)
 
     # Identify leap years
     is_leap_year = times.is_leap_year
@@ -22,11 +16,10 @@ def threshold(pc, N = 7):
     adjusted_dayofyear = times.dayofyear - is_leap_year * ((times.month > 2).astype(int))
 
     # Now, incorporate this adjustment back into your xarray object
-    df_stack['adjusted_dayofyear'] = adjusted_dayofyear
-
+    df['adjusted_dayofyear'] = adjusted_dayofyear
 
     # groupby dayofyear 
-    G = df_stack.groupby('adjusted_dayofyear')
+    G = df.groupby('adjusted_dayofyear')
     # 1.5 standard deviation as the threshold, suppose the mean is already zero (anomaly data)
     pos_threshold = 1.5 * G['pc'].std()
     neg_threshold = -1.5 * G['pc'].std()
@@ -34,7 +27,21 @@ def threshold(pc, N = 7):
     neg_threshold = neg_threshold.reset_index()
     pos_threshold.columns = ['dayofyear','threshold']
     neg_threshold.columns = ['dayofyear','threshold']
-    
 
     return pos_threshold, neg_threshold
+#%%
+def construct_window(df, window = 7):
+    """
+    create a dataframe, with 3 days before and after the day as the window
+    """
+    # set time as index
+    df = df.set_index('time')
+
+    # create a window
+    windows = [i for i in range(int(-(window -1)/2), int((window-1)/2 + 1))]
+    df_window = pd.concat([df['pc'].shift(periods = i, freq = '1D').rename(i) for i in windows], axis=1).dropna(axis = 0, how = 'any')
+    df_stack = df_window.stack().reset_index()
+    df_stack.columns = ['time','window','pc']
+
+    return df_stack
 
