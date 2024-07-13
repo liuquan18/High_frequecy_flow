d180edf646e23bf353c43e20e0a28da1ab7dac7a
Quan <quan.liu@mpimet.mpg.de>
2024-07-13 10:19:28 +0200

    enable all plevs

--- script/extreme_extract/threshold_allens.py
+++ script/extreme_extract/threshold_allens.py
@@ -9,11 +9,12 @@ pc = xr.open_mfdataset("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP
 # %%
 df = pc.to_dataframe().reset_index()
 # %%
-df_window = df.groupby('ens')[['time','pc']].apply(construct_window, window = 7)
+df_window = df.groupby(['plev','ens'])[['time','pc']].apply(construct_window, window = 7)
 # %%
-df_window = df_window.reset_index()[['ens','time','window','pc']]
+df_window = df_window.reset_index()[['plev','ens','time','window','pc']]
 # %%
-pos_threshold, neg_threshold = threshold(df_window)
+pos_threshold = df_window.groupby('plev')[['ens','time','window','pc']].apply(threshold, type = 'pos')
+neg_threshold = df_window.groupby('plev')[['ens','time','window','pc']].apply(threshold, type = 'neg')
 # %%
 pos_threshold.to_csv('/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/threshold/pos_threshold_first10_allens.csv', index = False)
 neg_threshold.to_csv('/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/threshold/neg_threshold_first10_allens.csv', index = False)
