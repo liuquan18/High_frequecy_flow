e0eedcc1590504092c0e37ea484b70dcd6d12327
Quan <quan.liu@mpimet.mpg.de>
2024-07-23 20:38:31 +0200

    support more than 1 days of missing

--- src/composite/zg_mermean_composite.py
+++ src/composite/zg_mermean_composite.py
@@ -83,15 +83,17 @@ def composite_zg_mermean(zg, date_range):
         if sel_zg.time.size < 61:
             if start_time < pd.Timestamp(f"{start_time.year}-05-01"):
                 logging.info("start_time is before May 1st, filling the missing days with zeros")
-                add_data = sel_zg.isel(time = 0).copy()
-                add_data['time'] = start_time
+                missing_days = 61 - sel_zg.time.size 
+                add_data = sel_zg.isel(time = slice(None, missing_days)).copy()
+                add_data['time'] = pd.date_range(end = f"{start_time.year}-05-01", periods=missing_days, freq='D')
                 # change values of add_data to 0
                 add_data = xr.zeros_like(add_data)
                 sel_zg = xr.concat([add_data, sel_zg], dim='time')
             elif end_time > pd.Timestamp(f"{end_time.year}-09-30"):
                 logging.info("end_time is after September 30th, filling the missing days with zeros")
-                add_data = sel_zg.isel(time = -1).copy()
-                add_data['time'] = end_time
+                missing_days = sel_zg.time.size - 61
+                add_data = sel_zg.isel(time = slice(missing_days, None)).copy()
+                add_data['time'] = pd.date_range(f"{end_time.year}-10-01", periods=missing_days, freq='D')
                 add_data = xr.zeros_like(add_data)
                 sel_zg = xr.concat([sel_zg, add_data], dim='time')
                 
