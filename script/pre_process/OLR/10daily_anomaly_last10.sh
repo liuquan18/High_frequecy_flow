27b9da340f8a64f4996642360d2c7a1effd15ff2
Quan <quan.liu@mpimet.mpg.de>
2024-08-06 16:53:40 +0200

    OLR processed

---
+++ script/pre_process/OLR/10daily_anomaly_last10.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+module load cdo
+module load parallel
+
+# lists
+base_dir=/work/ik1017/CMIP6/data/CMIP6/ScenarioMIP/MPI-M/MPI-ESM1-2-LR/ssp585/
+to_dir=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/OLR_daily_anomaly/last10_OLR_daily_ano/
+tmp_dir=/scratch/m/m300883/OLR/
+
+
+
+export to_dir base_dir tmp_dir
+
+# define the function
+Anomaly() {
+
+    infile=$1
+    monthfile=$2
+    savedir=$3
+    outfile=${savedir}$(basename ${infile})
+    # include month Mayb and september for later rolling window
+    cdo -monsub -selmon,5/9 -sellonlatbox,-180,80,-30,30 ${infile} -sellonlatbox,-180,80,-30,30 ${monthfile} ${outfile}
+}
+
+export -f Anomaly
+
+for member in {1..50}
+do
+    infile_list1=$(find ${base_dir}r${member}i1p1f1/day/ -name 'rlut_day_MPI-ESM1-2-LR_ssp585_r'${member}'i1p1f1_gn_20750101-20941231.nc')
+    infile_list2=$(find ${base_dir}r${member}i1p1f1/day/ -name 'rlut_day_MPI-ESM1-2-LR_ssp585_r'${member}'i1p1f1_gn_20950101-21001231.nc')
+    for infile in $infile_list1
+    do
+        daily_list1+="$infile "
+    done
+    for infile in $infile_list2
+    do
+        daily_list2+="$infile "
+    done
+done
+
+# Remove the trailing space
+daily_list1=$(echo $daily_list1 | sed 's/ $//')
+daily_list2=$(echo $daily_list2 | sed 's/ $//')
+
+month_ens1=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/OLR_season_global/rlut_Amon_MPI-ESM1-2-LR_HIST_ensmean_207505-209409.nc
+month_ens2=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/OLR_season_global/rlut_Amon_MPI-ESM1-2-LR_HIST_ensmean_209505-210009.nc
+
+savedir1=${tmp_dir}list1/
+savedir2=${tmp_dir}list2/
+
+
+parallel --jobs 20 Anomaly ::: ${daily_list1} ::: ${month_ens1} ::: ${savedir1}
+
+parallel --jobs 20 Anomaly ::: ${daily_list2} ::: ${month_ens2} ::: ${savedir2}
+
+
--- script/pre_process/OLR/4daily_anomaly_last10_merge.sh
+++ script/pre_process/OLR/4daily_anomaly_last10_merge.sh
@@ -6,8 +6,8 @@ module load parallel
 # define function to merge time
 merge(){
     member=$1
-    infile1=/scratch/m/m300883/OLR/list1/rlut_day_MPI-ESM1-2-LR_ssp585_r${member}i1p1f1_gn_20750101-20941231_ano.nc
-    infile2=/scratch/m/m300883/OLR/list2/rlut_day_MPI-ESM1-2-LR_ssp585_r${member}i1p1f1_gn_20950101-21001231_ano.nc
+    infile1=/scratch/m/m300883/OLR/list1/rlut_day_MPI-ESM1-2-LR_ssp585_r${member}i1p1f1_gn_20750101-20941231.nc
+    infile2=/scratch/m/m300883/OLR/list2/rlut_day_MPI-ESM1-2-LR_ssp585_r${member}i1p1f1_gn_20950101-21001231.nc
 
     savedir=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/OLR_daily_anomaly/last10_OLR_daily_ano/
     # cdo meregetime
@@ -18,4 +18,4 @@ merge(){
 
 export -f merge
 
-parallel --jobs 20 merge ::: {1..30}
+parallel --jobs 20 merge ::: {1..50}
