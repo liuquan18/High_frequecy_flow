8e3a4885087a0437d99b11f69e5ef43275bacccc
Quan <quan.liu@mpimet.mpg.de>
2024-08-07 15:30:48 +0200

    reduce the ntasks

--- script/5OLR/NA_zg_relate_OLR_reconstruct_submitter.sh
+++ script/5OLR/NA_zg_relate_OLR_reconstruct_submitter.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=04:00:00
 #SBATCH --partition=compute
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=10
+#SBATCH --ntasks-per-node=5
 #SBATCH --mem=0
 #SBATCH --mail-type=FAIL
 #SBATCH --account=mh0033
@@ -11,4 +11,4 @@
 
 
 
-mpirun -n 10 python NA_zg_relate_OLR_reconstruct.py $1
+mpirun -n 5 python NA_zg_relate_OLR_reconstruct.py $1
