47122d88743a4a90e8dfd0b03ae961bdcf1e7210
Quan <quan.liu@mpimet.mpg.de>
2024-08-13 10:07:45 +0200

    residue in script

--- src/extremes/extreme_extract.py
+++ src/extremes/extreme_extract.py
@@ -2,25 +2,6 @@
 import pandas as pd
 from scipy import ndimage
 
-#%%
-def subtract_threshold(pc, threshold):
-    # xarray exclude the data at 1st may to 3rd May, and the data during 28th Sep and 31st Sep
-    # Create a mask for the dates to exclude
-    mask_exclude = (
-        ((pc['time.month'] == 5) & (pc['time.day'] >= 1) & (pc['time.day'] <= 3)) |
-        ((pc['time.month'] == 9) & (pc['time.day'] >= 28) & (pc['time.day'] <= 30))
-    )
-    mask_keep = ~mask_exclude
-    pc = pc.where(mask_keep, drop = True)
-
-
-    df = pc.to_dataframe()
-
-    G =  df.groupby(df.index.year)['pc']
-
-    # subtract the threshold from original data
-    residues = G.transform(lambda x: x - threshold['threshold'].values)
-    return residues.reset_index()
 
 # %%
 def extract_pos_extremes(df):
@@ -48,6 +29,55 @@ def extract_pos_extremes(df):
 
     Events = Events[['start_time','end_time','duration','sum','mean','max', 'min']]
     return Events
+
+#%%
+def add_pos_sign_times(df:pd.DataFrame, events:pd.DataFrame) -> pd.DataFrame:
+    """
+    Add sign_start_time and sign_end_time to the events DataFrame.
+    
+    Parameters:
+    df (pd.DataFrame): The original DataFrame with 'time' and 'pc' columns.
+    events (pd.DataFrame): The DataFrame of extreme events with 'start_time' and 'end_time' columns.
+    
+    Returns:
+    pd.DataFrame: The events DataFrame with added 'sign_start_time' and 'sign_end_time' columns.
+    """
+    # apply median_filter
+    df['pc'] = ndimage.median_filter(df['pc'], size=3)
+
+    # Create a column for positive values
+    df['positive'] = df['pc'] > 0
+    
+    # Create a grouper that increments every time the sign changes
+    sign_grouper = df.groupby(df.time.dt.year)['pc'].transform(lambda x: x.lt(0).cumsum())
+
+    # Grouby by year and the sign_grouper
+    G = df[df['pc']>0].groupby([df.time.dt.year, sign_grouper])
+
+    # get the sign_start_time and sign_end_time
+    sign_times = G.agg(
+        sign_start_time = pd.NamedAgg(column = 'time',aggfunc='min'),
+        sign_end_time = pd.NamedAgg(column = 'time',aggfunc='max'),
+        ).reset_index()
+    
+    # functions to find corresponding sign_start_time and sign_end_time
+
+    def find_sign_times (row):
+        mask = (sign_times['sign_start_time'] <= row['start_time']) & (sign_times['sign_end_time'] >= row['end_time'])
+        if mask.any():
+            return sign_times.loc[mask.idxmax(), ['sign_start_time', 'sign_end_time']]
+        return pd.Series({'sign_start_time': pd.NaT, 'sign_end_time': pd.NaT})
+    
+    # Apply the function to each row in events
+    sign_times_for_events = events.apply(find_sign_times, axis=1)
+    
+    # Add the new columns to the events DataFrame
+    events['sign_start_time'] = sign_times_for_events['sign_start_time']
+    events['sign_end_time'] = sign_times_for_events['sign_end_time']
+    events['sign_duration'] = (events['sign_end_time'] - events['sign_start_time']).dt.days + 1
+    
+    return events
+
 # %%
 def extract_neg_extremes(df):
     """
@@ -75,3 +105,5 @@ def extract_neg_extremes(df):
 
     Events = Events[['start_time','end_time','duration','sum','mean','max', 'min']]
     return Events
+
+
