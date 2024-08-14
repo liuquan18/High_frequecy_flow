f1cea9f93830fa1e52764706661bcfe80c8442fe
Quan <quan.liu@mpimet.mpg.de>
2024-08-14 09:27:54 +0200

    indo and amazon

--- script/5OLR/cross_correlation.py
+++ script/5OLR/cross_correlation.py
@@ -28,6 +28,14 @@ def read_data(period, ens, plev=25000, extreme_type="pos", duration_lim=30):
     NAO_file = glob.glob(NAO_dir + f"*r{ens}.nc")[0]
     NAO = xr.open_dataset(NAO_file).pc.sel(plev=plev)
 
+    # to dataframe
+    OLR_indo = OLR_indo.to_dataframe().reset_index()[["time", "rlut"]].set_index("time")
+    OLR_amaz = OLR_amaz.to_dataframe().reset_index()[["time", "rlut"]].set_index("time")
+    NAO = NAO.to_dataframe().reset_index()[["time", "pc"]].set_index("time")
+
+    return OLR_indo, OLR_amaz, NAO
+
+def read_extremes(period: str, ens:int, duration_lim: int=30, extreme_type="pos", plev=25000):
     extreme_dir = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/{extreme_type}_extreme_events/{extreme_type}_extreme_events_{period}/"
     extreme_file = glob.glob(extreme_dir + f"*r{ens}.csv")[0]
     extreme = pd.read_csv(extreme_file)
@@ -37,13 +45,7 @@ def read_data(period, ens, plev=25000, extreme_type="pos", duration_lim=30):
     extreme = sel_event_above_duration(
         extreme, duration=duration_lim, by="sign_duration"
     )
-
-    # to dataframe
-    OLR_indo = OLR_indo.to_dataframe().reset_index()[["time", "rlut"]].set_index("time")
-    OLR_amaz = OLR_amaz.to_dataframe().reset_index()[["time", "rlut"]].set_index("time")
-    NAO = NAO.to_dataframe().reset_index()[["time", "pc"]].set_index("time")
-
-    return OLR_indo, OLR_amaz, NAO, extreme
+    return extreme
 
 
 # %%
@@ -82,116 +84,79 @@ def cross_corr(OLR, NAO, extremes, OLR_roll=3, lag_lim=40):
         CCFs = None
 
     return CCFs
+#%%
+def event_ccf(period, extreme_type = 'pos'):
+    CCFs_indo = []
+    CCFs_amaz = []
+    for ens in range(1, 51):
+        OLR_indo, OLR_amaz, NAO = read_data(period, ens, 25000, extreme_type)
+        pos_extremes = read_extremes(period, ens, 30, extreme_type)
+        CCF_indo = cross_corr(OLR_indo, NAO, pos_extremes, OLR_roll=3)
+        CCF_amaz = cross_corr(OLR_amaz, NAO, pos_extremes, OLR_roll=3)
+        CCFs_indo.append(CCF_indo)
+        CCFs_amaz.append(CCF_amaz)
+    CCFs_indo = [CCF for CCF in CCFs_indo if CCF is not None]
+    CCFs_amaz = [CCF for CCF in CCFs_amaz if CCF is not None]
 
+    CCFs_indo = pd.concat(CCFs_indo, axis=1)
+    CCFs_amaz = pd.concat(CCFs_amaz, axis=1)
 
-# %%
-# first 10 years
-CCFs_first10_pos = [
-    cross_corr(*read_data("first10", i, 25000, "pos")) for i in range(1, 51)
-]
+    CCFs_indo.columns = ["even_id" + str(i) for i in range(len(CCFs_indo.columns))]
+    CCFs_amaz.columns = ["even_id" + str(i) for i in range(len(CCFs_amaz.columns))]
 
-# Filter out None elements before concatenation
-CCFs_first10_pos = [CCF for CCF in CCFs_first10_pos if CCF is not None]
+    return CCFs_indo, CCFs_amaz
 
-# Concatenate the filtered list
-CCFs_first10_pos = pd.concat(CCFs_first10_pos, axis=1)
-
-# columns name as 'even_id', and values range from o to len(columns)
-CCFs_first10_pos.columns = [
-    "even_id" + str(i) for i in range(len(CCFs_first10_pos.columns))
-]
-
-
-# %%
-# last 10 years
-CCFs_last10_pos = [
-    cross_corr(*read_data("last10", i, 25000, "pos")) for i in range(1, 51)
-]
-
-# filter out None elements before concatenation
-CCFs_last10_pos = [CCF for CCF in CCFs_last10_pos if CCF is not None]
-
-# Concatenate the filtered list
-CCFs_last10_pos = pd.concat(CCFs_last10_pos, axis=1)
-
-# columns name as 'even_id', and values range from o to len(columns)
-CCFs_last10_pos.columns = [
-    "even_id" + str(i) for i in range(len(CCFs_last10_pos.columns))
-]
-
-
-# %%
-fig, axes = plt.subplots(1, 2, figsize=(20, 10))
-for column in CCFs_first10_pos.columns:
-    if column != "median":
-        axes[0].plot(
-            CCFs_first10_pos.index, CCFs_first10_pos[column], color="grey", alpha=0.3
-        )
-axes[0].plot(
-    CCFs_first10_pos.index,
-    CCFs_first10_pos.median(axis=1),
+#%%
+def plot_ccf(CCFs_first10_pos_indo, ax):
+    for column in CCFs_first10_pos_indo.columns:
+        ax.plot(
+        CCFs_first10_pos_indo.index, CCFs_first10_pos_indo[column], color="grey", alpha=0.3
+    )
+    ax.plot(
+    CCFs_first10_pos_indo.index,
+    CCFs_first10_pos_indo.median(axis=1),
     color="black",
     linewidth=2,
     label="median",
 )
 
 # count how many columns are below 0 and plot as bar * -1 use a twin y axis
-axes_twin_0 = axes[0].twinx()
-axes_twin_0.set_ylim(-0.6, 0.61)
-axes[0].set_ylim(-0.85, 0.86)
-axes_twin_0.bar(
-    CCFs_first10_pos.index,
-    ((CCFs_first10_pos < 0).sum(axis=1)) / len(CCFs_first10_pos.columns) * -1,
+    ax_twin = ax.twinx()
+    ax_twin.set_ylim(-0.6, 0.61)
+    ax.set_ylim(-0.85, 0.86)
+    ax.bar(
+    CCFs_first10_pos_indo.index,
+    ((CCFs_first10_pos_indo < 0).sum(axis=1)) / len(CCFs_first10_pos_indo.columns) * -1,
     color="red",
     alpha=0.3,
     label="negative",
 )
 
 # hline at y = 0
-axes[0].hlines(y=0, xmin=-50, xmax=0, linestyles="--")
-axes_twin_0.hlines(y=-0.5, xmin=-50, xmax=0, linestyles="--")
+    ax.hlines(y=0, xmin=-50, xmax=0, linestyles="--")
+    ax.hlines(y=-0.5, xmin=-50, xmax=0, linestyles="--")
 
-axes[0].set_xlabel("Lags")
-axes[0].set_ylabel("Correlation")
+    ax.set_xlabel("Lags")
+    ax.set_ylabel("Correlation")
 
-axes[0].set_title("First 10 years")
-axes[0].set_xlim(-40, 0)
+    ax.set_xlim(-40, 0)
+# %%
+# first 10 years
+CCFs_first10_pos_indo, CCFs_first10_pos_amaz = event_ccf("first10", "pos")
 
-for column in CCFs_last10_pos.columns:
-    axes[1].plot(
-        CCFs_last10_pos.index, CCFs_last10_pos[column], color="grey", alpha=0.3
-    )
-axes[1].plot(
-    CCFs_last10_pos.index,
-    CCFs_last10_pos.median(axis=1),
-    color="black",
-    linewidth=2,
-    label="median",
-)
+# last 10 years
+CCFs_last10_pos_indo, CCFs_last10_pos_amaz = event_ccf("last10", "pos")
 
-# count how many columns are below 0 and plot as bar * -1
-axes[1].set_ylim(-0.85, 0.86)
-axes_twin_1 = axes[1].twinx()
-axes_twin_1.set_ylim(-0.6, 0.61)
+# %%
+fig, axes = plt.subplots(2, 2, figsize=(20, 10))
 
-axes_twin_1.bar(
-    CCFs_last10_pos.index,
-    ((CCFs_last10_pos < 0).sum(axis=1)) / len(CCFs_last10_pos.columns) * -1,
-    color="red",
-    alpha=0.3,
-    label="negative",
-)
 
-# hline at y = 0
-axes[1].hlines(y=0, xmin=-50, xmax=0, linestyles="--")
-axes[1].set_xlabel("Lags")
-
-axes_twin_1.hlines(y=-0.5, xmin=-50, xmax=0, linestyles="--")
-
-axes[1].set_title("Last 10 years")
-axes[1].set_xlim(-40, 0)
-plt.savefig(
-    "/work/mh0033/m300883/High_frequecy_flow/docs/plots/OLR_composite_e2c/Indo_pacific_OLR_NAO_pos_ccf.png"
-)
+plot_ccf(CCFs_first10_pos_indo, axes[0, 0])
+plot_ccf(CCFs_last10_pos_indo, axes[0, 1])
+plot_ccf(CCFs_first10_pos_amaz, axes[1, 0])
+plot_ccf(CCFs_last10_pos_amaz, axes[1, 1])
+# plt.savefig(
+#     "/work/mh0033/m300883/High_frequecy_flow/docs/plots/OLR_composite_e2c/Indo_pacific_OLR_NAO_pos_ccf.png"
+# )
 
 # %%
