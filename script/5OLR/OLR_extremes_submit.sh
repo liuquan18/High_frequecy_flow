6351bc6df67e3c43c69d79639a0dcfaa60340ed7
Quan <quan.liu@mpimet.mpg.de>
2024-08-19 16:10:11 +0200

    sbatch

---
+++ script/5OLR/OLR_extremes_submit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=extreme
+#SBATCH --time=09:00:00
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks=10
+#SBATCH --mem=0
+#SBATCH --mail-type=FAIL
+#SBATCH --account=mh0033
+#SBATCH --output=extreme.%j.out
+
+
+mpirun -n 10 python -u OLR_extremes.py $1 # 0,1
+# python -u hello0index_generator.py $1 $2 $3
