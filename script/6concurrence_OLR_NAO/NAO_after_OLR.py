54011ec6bc16b179e69c7969a4d6f0726257fbf5
Quan <quan.liu@mpimet.mpg.de>
2024-08-21 11:47:21 +0200

    use src function

--- script/6concurrence_OLR_NAO/NAO_after_OLR.py
+++ script/6concurrence_OLR_NAO/NAO_after_OLR.py
@@ -13,39 +13,9 @@ import src.extremes.extreme_read as er
 # %%
 logging.basicConfig(level=logging.INFO)
 
-
+#%%
+from src.OLR_NAO.OLR_NAO_association import read_extremes
 # %%
-# %%
-def read_extremes(period, member, extreme_type="pos", dur_lim=8):
-    """
-    Read the NAO and OLR extremes
-    """
-    NAO_dir = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/{extreme_type}_extreme_events/{extreme_type}_extreme_events_{period}/"
-    OLR_dir = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/OLR_extremes_pos/OLR_extremes_pos_{period}/"  # no extreme_type in the OLR_extremes
-
-    # read NAO positive extremes
-    NAO_file = glob.glob(
-        f"{NAO_dir}troposphere_{extreme_type}_extreme_events*r{member}.csv"
-    )[0]
-    NAO_pos = pd.read_csv(NAO_file)
-    NAO_pos = NAO_pos[NAO_pos["plev"] == 25000]
-
-    # select extremes based on minimum duration
-    NAO_pos = er.sel_event_above_duration(
-        NAO_pos, duration=dur_lim, by="extreme_duration"
-    )
-    # select columns
-    NAO_pos = NAO_pos[["sign_start_time", "extreme_duration"]]
-
-    # read OLR extremes
-    OLR_file = glob.glob(f"{OLR_dir}OLR_extremes*r{member}.csv")[0]
-    OLR = pd.read_csv(OLR_file)
-    # select extremes based on minimum duration but not limited in JJA
-    OLR = OLR[OLR["extreme_duration"] >= dur_lim]
-    # select columns
-    OLR = OLR[["sign_start_time", "extreme_duration", "lat", "lon"]]
-
-    return NAO_pos, OLR
 
 
 # %%
@@ -81,7 +51,7 @@ def NAO_after_OLR(OLR, NAO, lag=[-16,-6]):
 
 
 # %%
-def NAO_after_OLR_all(period, dur_lim=8, extreme_type="pos", lag=[-16,-6]):
+def NAO_after_OLR_all(period,  extreme_type="pos", lag=[-16,-6]):
     """
     select OLR extremes that are before the NAO extremes by lag days for all members
     """
@@ -90,7 +60,7 @@ def NAO_after_OLR_all(period, dur_lim=8, extreme_type="pos", lag=[-16,-6]):
     for member in range(1, 51):
         logging.info(f"member {member}")
         NAO_pos, OLR = read_extremes(
-            period, member, extreme_type=extreme_type, dur_lim=dur_lim
+            period, member, extreme_type=extreme_type, lim_OLR = 10
         )
         concurrence = NAO_after_OLR(OLR, NAO_pos, lag=lag)
         if concurrence.empty:
@@ -147,7 +117,7 @@ for ax in [ax1, ax2]:
     ax.set_xticks(range(-180, 180, 60), crs=ccrs.PlateCarree())
     ax.set_xticklabels([f"{lon}°" for lon in range(-180, 180, 60)])
     
-plt.savefig("/work/mh0033/m300883/High_frequecy_flow/docs/plots/OLR_extremes/NAO_pos_after_OLR_concurrence.png")
+# plt.savefig("/work/mh0033/m300883/High_frequecy_flow/docs/plots/OLR_extremes/NAO_pos_after_OLR_concurrence.png")
 
 # %%
 
