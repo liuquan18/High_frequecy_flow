20f7a36b1f21689c907616a14852e8c46d37a295
Quan <quan.liu@mpimet.mpg.de>
2024-08-22 14:30:18 +0200

    2 in parallel

--- script/5OLR/OLR_threshold_single_ens_submit.sh
+++ script/5OLR/OLR_threshold_single_ens_submit.sh
@@ -3,12 +3,12 @@
 #SBATCH --time=08:00:00
 #SBATCH --partition=compute
 #SBATCH --nodes=1
-#SBATCH --ntasks=5
+#SBATCH --ntasks=2
 #SBATCH --mem=0
 #SBATCH --mail-type=FAIL
 #SBATCH --account=mh0033
 #SBATCH --output=threshold.%j.out
 
 
-mpirun -n 5 python -u OLR_threshold_single_ens.py 
+mpirun -n 2 python -u OLR_threshold_single_ens.py 
 # python -u hello0index_generator.py $1 $2 $3
