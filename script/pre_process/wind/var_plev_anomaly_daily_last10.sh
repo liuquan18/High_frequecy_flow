f98e7004df05dbaa416459164f897c7cab64f1cf
Quan <quan.liu@mpimet.mpg.de>
2024-08-24 17:01:58 +0200

    last10

--- script/pre_process/wind/var_plev_anomaly_daily_last10.sh
+++ script/pre_process/wind/var_plev_anomaly_daily_last10.sh
@@ -1,7 +1,29 @@
+
 #!/bin/bash
 # subtract the ensemble mean of the monthly data from the daily data using cdo monsub
 module load cdo
 module load parallel
+var=$1 # zg, ua, va
+
+from_path=/work/ik1017/CMIP6/data/CMIP6/ScenarioMIP/MPI-M/MPI-ESM1-2-LR/ssp585/
+to_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/${var}_daily_global/${var}_MJJAS_ano_last10/
+tmp_path=/scratch/m/m300883/${var}_daily_global/
+
+mkdir -p ${to_path} ${tmp_path}
+
+export from_path to_path tmp_path var
+
+# function to merge the daily data
+Merge(){
+    member=$1
+
+    dailyfile1=$(find ${from_path}r${member}i1p1f1/day/ -name ${var}'_day_MPI-ESM1-2-LR_ssp585_r'${member}'i1p1f1_gn_20750101-20941231.nc')
+    dailyfile2=$(find ${base_dir}r${member}i1p1f1/day/ -name ${var}'_day_MPI-ESM1-2-LR_ssp585_r'${member}'i1p1f1_gn_20950101-21001231.nc')
+
+    # merge the two daily files
+    cdo -O -mergetime -selyear,2091/2094 $dailyfile1 -selyear,2095/2100 $dailyfile2 ${tmp_path}${var}_day_MPI-ESM1-2-LR_ssp585_r${member}i1p1f1_gn_20910101-21001231.nc
+
+}
 
 # define function called Anomaly
 Anomaly() {
@@ -9,14 +31,15 @@ Anomaly() {
     member=$1
     echo $member
 
-    # mergetime of daily data for the last 10 years
-    cdo -O -sellevel,100000,85000,70000,50000,25000 -selmonth,5,6,7,8,9 -selyear,2091/2100 -mergetime /pool/data/CMIP6/data/ScenarioMIP/MPI-M/MPI-ESM1-2-LR/ssp585/r${member}i1p1f1/day/zg/gn/v????????/zg_day_MPI-ESM1-2-LR_ssp585_r${member}i1p1f1_gn_20750101-20941231.nc /pool/data/CMIP6/data/ScenarioMIP/MPI-M/MPI-ESM1-2-LR/ssp585/r${member}i1p1f1/day/zg/gn/v????????/zg_day_MPI-ESM1-2-LR_ssp585_r${member}i1p1f1_gn_20950101-21001231.nc /scratch/m/m300883/MPI_GE_CMIP6/daily_ssp585/zg_day_MPI-ESM1-2-LR_ssp585_r${member}i1p1f1_gn_20750101-21001231.nc
-
-    # anomaly
-    cdo -monsub /scratch/m/m300883/MPI_GE_CMIP6/daily_ssp585/zg_day_MPI-ESM1-2-LR_ssp585_r${member}i1p1f1_gn_20750101-21001231.nc\
-        -sellevel,100000,85000,70000,50000,25000 /work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/zg_season_global/zg_month_ensmean_2091_2100.nc\
-        /work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/zg_daily_global/zg_MJJAS_ano_last10/zg_day_MPI-ESM1-2-LR_ssp585_r${member}i1p1f1_gn_20910501-21000931_ano.nc
+    dailyfile=${tmp_path}${var}_day_MPI-ESM1-2-LR_ssp585_r${member}i1p1f1_gn_20910101-21001231.nc
+    monthlyfile=$(find /work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/${var}_season_global/ -name ${var}_*_209105-210009.nc)
+    anomalyfile=${to_path}${var}_day_MPI-ESM1-2-LR_historical_r${member}i1p1f1_gn_20910501-21000931_ano.nc
+    # include month Mayb and september for later rolling window
+    cdo -monsub -sellevel,100000,85000,70000,50000,25000 -selmonth,5/9 -selyear,2091/2100 $dailyfile \
+        -sellevel,100000,85000,70000,50000,25000 $monthlyfile \
+        ${anomalyfile}
 }
 
-export -f Anomaly
+export -f Anomaly Merge
+parallel --jobs 20 Merge ::: {1..50}
 parallel --jobs 20 Anomaly ::: {1..50}
