6c91c9ed644bc9243ba182f092048526332baf2f
Quan <quan.liu@mpimet.mpg.de>
2024-08-26 17:25:18 +0200

    save plots

--- script/7wave/NAO_AWB_events_composite.py
+++ script/7wave/NAO_AWB_events_composite.py
@@ -113,11 +113,17 @@ for ens in range(1,51):
 last10_pos_AWBs = pd.concat(last10_pos_AWBs)
 
 # %%
-# %%
-plot_tracks(first10_pos_AWBs)
-plot_tracks(last10_pos_AWBs)
-# %%
 fig, ax = plt.subplots(2, 1, figsize=(10,5), subplot_kw=dict(projection=ccrs.PlateCarree(-120)))
 plot_tracks(first10_pos_AWBs, ax[0])
+ax[0].set_title('AWB events before NAO+ events in the First 10 years ')
+
 plot_tracks(last10_pos_AWBs, ax[1])
+ax[1].set_title('AWB events before NAO+ events in the Last 10 years ')
+ax[1].set_xticks(range(-180, 180, 60), crs=ccrs.PlateCarree())
+ax[1].set_xticklabels([f"{lon}°" for lon in range(-180, 180, 60)])
+for ax in [ax[0], ax[1]]:
+    ax.set_yticks(range(0, 90, 30), crs=ccrs.PlateCarree())
+    ax.set_yticklabels([f"{lat}°" for lat in range(0, 90, 30)])
+
+plt.savefig('/work/mh0033/m300883/High_frequecy_flow/docs/plots/wave_break/AWB_before_NAO.png')
 # %%
