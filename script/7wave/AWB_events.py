ffa4b1d555746f8503808f080830f3ab0a0fa57f
Quan <quan.liu@mpimet.mpg.de>
2024-08-26 14:16:31 +0200

    for all ensemble members

--- script/7wave/AWB_events.py
+++ script/7wave/AWB_events.py
@@ -6,11 +6,32 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import matplotlib.gridspec as gridspec
 import glob
+import logging
+import sys
+logging.basicConfig(level=logging.WARNING)
 # %%
 import src.ConTrack.contrack as ct
 #%%
 import importlib
 importlib.reload(ct)
+#%%
+# nodes and cores
+try:
+    node = int(sys.argv[1])
+except ValueError:
+    logging.warning("no node number provided, using default node 0")
+    node = 0
+
+try:
+    from mpi4py import MPI
+
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()  # [0,1,2,3,4,5,6,7,8,9]
+    size = comm.Get_size()  # 10
+except:
+    logging.warning("::: Warning: Proceeding without mpi4py! :::")
+    rank = 0
+    size = 1
 # %%
 def awb_event(period: str, ens : int, plev : int = 25000):
     WB = ct.contrack()
@@ -45,3 +66,13 @@ def awb_event(period: str, ens : int, plev : int = 25000):
     outname = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/AWB_events/AWB_events_{period}/{outname}"
     WB_df.to_csv(outname)
 # %%
+periods = ["first10", "last10"]
+period = periods[node]
+#%%
+members_all = list(range(1, 51))  # all members
+members_single = np.array_split(members_all, size)[rank]  # members on this core
+
+#%%
+for ens in members_single:
+    print(f"Period {period}: Rank {rank}, member {ens}/{members_single[-1]}")
+    awb_event(period, ens)
