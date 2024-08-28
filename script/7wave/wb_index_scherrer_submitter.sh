e4b1c312478ad09fe9e93c9eae6abaca2b40b5ac
Quan <quan.liu@mpimet.mpg.de>
2024-08-28 15:40:28 +0200

    memory

--- script/7wave/wb_index_scherrer_submitter.sh
+++ script/7wave/wb_index_scherrer_submitter.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=wb
-#SBATCH --time=04:00:00
+#SBATCH --time=08:00:00
 #SBATCH --partition=compute
 #SBATCH --nodes=1
-#SBATCH --ntasks=20
+#SBATCH --ntasks=2
 #SBATCH --mem=0
 #SBATCH --mail-type=FAIL
 #SBATCH --account=mh0033
 #SBATCH --output=wb.%j.out
 
 
-mpirun -n 5 python -u wb_index_scherrer.py $1 # 0,1
+mpirun -n 2 python -u wb_index_scherrer.py $1 # 0,1
