ce5deba0cd6e8851e7a0dda57f8ade076d7334bd
Quan <quan.liu@mpimet.mpg.de>
2024-09-01 15:49:05 +0200

    use src function

--- script/E_vector/E_vector_period_composite.py
+++ script/E_vector/E_vector_period_composite.py
@@ -17,53 +17,7 @@ import src.extremes.extreme_read as ext_read
 import src.composite.composite_plot as composite_plot
 
 # %%
-import importlib
-
-importlib.reload(composite)
-importlib.reload(ext_read)
-importlib.reload(composite_plot)
-
-# %%
-
-
-# %%
-def composite_single_ens(variable, period, ens, plev, freq_label=None):
-    pos_extreme, neg_extreme = ext_read.read_extremes(period, 8, ens, plev=plev)
-    variable_ds = composite.read_variable(variable, period, ens, plev, freq_label)
-
-    pos_comp, neg_comp = composite.event_composite(
-        variable_ds, pos_extreme, neg_extreme
-    )
-    return pos_comp, neg_comp
-
-
-# %%
-def composite_variable(variable, plev, freq_label, period, stat="mean"):
-    pos_comps = []
-    neg_comps = []
-
-    for i in range(1, 51):
-        pos_comp, neg_comp = composite_single_ens(variable, period, i, plev, freq_label)
-
-        pos_comps.append(pos_comp)
-        neg_comps.append(neg_comp)
-
-    # exclude None from the list
-    pos_comps = [x for x in pos_comps if x is not None]
-    neg_comps = [x for x in neg_comps if x is not None]
-
-    pos_comps = xr.concat(pos_comps, dim="event")
-    neg_comps = xr.concat(neg_comps, dim="event")
-
-    if stat == "mean":
-        pos_comps = pos_comps.mean(dim="event")
-        neg_comps = neg_comps.mean(dim="event")
-    elif stat == "count":
-        pos_comps = pos_comps.count(dim="event")
-        neg_comps = neg_comps.count(dim="event")
-
-    return pos_comps, neg_comps
-
+from src.composite.composite import composite_variable
 
 # %%
 plev = 25000
