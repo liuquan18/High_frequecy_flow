69f625e656c640f17f8045a5f50e02edd69a48e2
Quan <quan.liu@mpimet.mpg.de>
2024-09-02 19:18:46 +0200

    test

--- script/9maintain_triger/WB_threshold.py
+++ script/9maintain_triger/WB_threshold.py
@@ -4,19 +4,18 @@ import xarray as xr
 import eventextreme.extreme_threshold as et
 # %%
 wb = xr.open_mfdataset("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/wavebreak_index/wb_first10/*.nc",combine = 'nested', concat_dim='ens')
+
+wb.load()
 # %%
 wb = wb.wave_breaking_index
 # %%
 # spatial smoothing
 wb = wb.rolling(lat=7, lon=7, center=True).mean()
+#%%
+
+wb_sel = wb.sel(lat = slice(40,50), lon = slice(310,320))
 
 #%%
-wb_dayofyear = wb.groupby('time.dayofyear').std('time')
+wb_threshold = wb_sel.mean(dim  = ('lat','lon')).quantile(0.95, dim = ('time', 'ens'))
 
-#%%
-wb_dayofyear_ens = wb_dayofyear.mean('ens')
-
-#%%
-# save the result
-wb_dayofyear_ens.to_netcdf("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/wavebreak_events/wave_break_event_threshold/wb_dayofyear_threshold.nc")
 # %%
