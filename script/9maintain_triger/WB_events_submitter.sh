56ecf9c7575c567107c3b9c6920e4958bda518b1
Quan <quan.liu@mpimet.mpg.de>
2024-09-02 17:08:38 +0200

    memory

--- script/9maintain_triger/WB_events_submitter.sh
+++ script/9maintain_triger/WB_events_submitter.sh
@@ -3,11 +3,11 @@
 #SBATCH --time=08:00:00
 #SBATCH --partition=compute
 #SBATCH --nodes=1
-#SBATCH --ntasks=5
+#SBATCH --ntasks=2
 #SBATCH --mem=0
 #SBATCH --mail-type=FAIL
 #SBATCH --account=mh0033
 #SBATCH --output=wb.%j.out
 
 
-mpirun -n 5 python -u WB_events.py $1 # 0,1
+mpirun -n 2 python -u WB_events.py $1 # 0,1
