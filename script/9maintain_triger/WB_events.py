ffda2b51acba786def2d8f217ec7d351f4a87f5e
Quan <quan.liu@mpimet.mpg.de>
2024-09-03 10:46:23 +0200

    also for CWB

--- script/9maintain_triger/WB_events.py
+++ script/9maintain_triger/WB_events.py
@@ -8,6 +8,7 @@ import matplotlib.gridspec as gridspec
 import glob
 import logging
 import sys
+import os
 
 logging.basicConfig(level=logging.WARNING)
 # %%
@@ -24,6 +25,14 @@ try:
 except ValueError:
     logging.warning("no node number provided, using default node 0")
     node = 0
+
+# %%
+try:
+    break_type = sys.argv[2]
+except ValueError:
+    logging.warning("no break type provided, using default AWB")
+    break_type = "AWB"
+
 
 # %%
 # nodes and cores
@@ -45,22 +54,31 @@ period = periods[node]
 tags = ["1850_1859", "2091_2100"]
 tag = tags[node]
 
-#%%
-break_type = 'AWB'
+# %%
+
 
 # %%
 index_dir = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/wavebreak_index/wb_{period}/"
 event_dir = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/wavebreak_events/{break_type}_events_{period}/"
 flag_dir = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/wavebreak_events/{break_type}_flag_{period}/"
+
+# %%
+# mkdir if not exist
+if not os.path.exists(event_dir):
+    os.makedirs(event_dir)
+if not os.path.exists(flag_dir):
+    os.makedirs(flag_dir)
+
 # %%
 members_all = list(range(1, 51))  # all members
 members_single = np.array_split(members_all, size)[rank]  # members on this core
-#%%
+
+
+# %%
 # %%
 def wb_event(ds, persistence: int = 5, threshold: int = 15, gorl: str = ">="):
     WB = ct.contrack()
     WB.ds = ds
-
 
     # apply median filter to the data along time
     WB.ds = WB.ds.rolling(time=3, center=True).median()
@@ -80,7 +98,8 @@ def wb_event(ds, persistence: int = 5, threshold: int = 15, gorl: str = ">="):
     )
     return WB.ds
 
-#%%
+
+# %%
 def event_cycle(ds):
     WB = ct.contrack()
     WB.ds = ds
@@ -93,31 +112,44 @@ def event_cycle(ds):
 
 
 # %%
-for i, ens in enumerate(members_single):
+for i, ens in enumerate(members_single, break_type):
     logging.warning(f"Period {period}: Rank {rank}, member {i+1}/{len(members_single)}")
+
+    threshold = 15 if break_type == "AWB" else -15
+    gorl = ">=" if break_type == "AWB" else "<="
 
     file = glob.glob(f"{index_dir}wb_*r{ens}i1p1f1*.nc")[0]
 
     ds = xr.open_dataset(file)
-    ds = ds.groupby("time.year").apply(wb_event, persistence=5)
+    ds = ds.groupby("time.year").apply(
+        wb_event, persistence=5, threshold=threshold, gorl=gorl
+    )
 
     # save the flag
     ds["flag"].to_netcdf(flag_dir + file.split("/")[-1])
-    
 
     WB_xr = ds.groupby("time.year").apply(event_cycle)
     WB_df = []
     for year in WB_xr["year"].values:
         wb_df = WB_xr.sel(year=year).values
-        wb_df = pd.DataFrame(wb_df, columns= ['Flag','Date','Longitude','Latitude','Intensity','Size'])
+        wb_df = pd.DataFrame(
+            wb_df,
+            columns=["Flag", "Date", "Longitude", "Latitude", "Intensity", "Size"],
+        )
         # change value of 'Flag' to 'year'+'Flag'
-        wb_df['Flag'] = year * 1000 + wb_df['Flag'] # different year may have the same flag
+        wb_df["Flag"] = (
+            year * 1000 + wb_df["Flag"]
+        )  # different year may have the same flag
         WB_df.append(wb_df)
     WB_df = pd.concat(WB_df)
     # dropna
     WB_df = WB_df.dropna()
 
-    # save life cycle 
-    WB_df.to_csv(event_dir + file.split("/")[-1].replace("wb_", "wb_events_").replace(".nc", ".csv"), index=False)
+    # save life cycle
+    WB_df.to_csv(
+        event_dir
+        + file.split("/")[-1].replace("wb_", "wb_events_").replace(".nc", ".csv"),
+        index=False,
+    )
 
 # %%
