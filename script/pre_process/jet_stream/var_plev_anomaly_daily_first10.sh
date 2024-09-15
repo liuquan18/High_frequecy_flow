b866026755a66e525a326ab37a3dc5a2f1dcb387
Quan <quan.liu@mpimet.mpg.de>
2024-09-15 06:34:08 +0200

    with cdo

--- script/pre_process/jet_stream/var_plev_anomaly_daily_first10.sh
+++ script/pre_process/jet_stream/var_plev_anomaly_daily_first10.sh
@@ -20,8 +20,11 @@ Softlink() {
     echo $member
 
     dailyfile=$(find ${from_path}r${member}i1p1f1/day/${var}/gn/ -name ${var}_day_MPI-ESM1-2-LR_historical_r${member}i1p1f1_gn_18500101-18691231.nc)
-    # softlink the data to to_path
-    ln -s ${dailyfile} ${to_path}
+    
+    # select the first 10 years
+    tofile=${to_path}${var}_day_MPI-ESM1-2-LR_historical_r${member}i1p1f1_gn_18500501-18590931_ano.nc
+    cdo -sellevel,100000,85000,70000,50000,25000 -selmonth,5/9 -selyear,1850/1859 $dailyfile \
+        ${tofile}   
 
 }
 
