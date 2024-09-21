c787533b1b5041fc9524ff685811412b6998efd9
Quan <quan.liu@mpimet.mpg.de>
2024-09-21 09:01:09 +0200

    done

--- script/pre_process/wind/var_climatology.sh
+++ script/pre_process/wind/var_climatology.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 module load cdo
 
-cdo -monmean /work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/ua_season_global/ua_Amon_MPI-ESM1-2-LR_HIST_ensmean_185005-185909.nc /work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/ua_season_global/ua_Amon_MPI-ESM1-2-LR_HIST_climatology_185005-185909.nc
+cdo -ymonmean /work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/ua_season_global/ua_Amon_MPI-ESM1-2-LR_HIST_ensmean_185005-185909.nc /work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/ua_season_global/ua_Amon_MPI-ESM1-2-LR_HIST_climatology_185005-185909.nc
 
-cdo -monmean /work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/ua_season_global/ua_Amon_MPI-ESM1-2-LR_HIST_ensmean_209105-210009.nc /work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/ua_season_global/ua_Amon_MPI-ESM1-2-LR_HIST_climatology_209105-210009.nc
+cdo -ymonmean /work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/ua_season_global/ua_Amon_MPI-ESM1-2-LR_HIST_ensmean_209105-210009.nc /work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/ua_season_global/ua_Amon_MPI-ESM1-2-LR_HIST_climatology_209105-210009.nc
