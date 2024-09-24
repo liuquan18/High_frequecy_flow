b4f2a4838632aebcf7cea1ae47ff0d69d0e45c7a
Quan <quan.liu@mpimet.mpg.de>
2024-09-24 02:40:44 +0200

    into src

---
+++ src/jet_stream/jet_stream_plotting.py
@@ -0,0 +1,29 @@
+from src.plotting.util import erase_white_line
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+import numpy as np
+
+# %%
+def plot_uhat(ax, uhat_first, u_hat_last=None, levels=np.arange(-12, 13, 2)):
+
+    ax.coastlines()
+    ax.set_global()
+
+    uhat_first = erase_white_line(uhat_first)
+
+    uhat_first.plot.contourf(
+        ax=ax,
+        transform=ccrs.PlateCarree(),
+        add_colorbar=False,
+        levels=levels,
+    )
+    if u_hat_last is not None:
+        u_hat_last = erase_white_line(u_hat_last)
+        u_hat_last.plot.contour(
+            ax=ax,
+            transform=ccrs.PlateCarree(),
+            colors="w",
+            add_colorbar=False,
+            levels=levels[levels != 0],
+        )
+    return ax
