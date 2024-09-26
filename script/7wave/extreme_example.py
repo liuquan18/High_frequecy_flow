3aeab0054deea04de0ff7647afff3897cfcb02ce
Quan <quan.liu@mpimet.mpg.de>
2024-09-26 04:43:26 +0200

    change ens

--- script/7wave/extreme_example.py
+++ script/7wave/extreme_example.py
@@ -7,7 +7,7 @@ import cartopy.crs as ccrs
 import matplotlib.gridspec as gridspec
 
 # %%
-pos_ens = 2
+pos_ens = 38
 neg_ens = 3
 # %%
 pos_extreme = pd.read_csv(
@@ -42,7 +42,7 @@ pos_mf = xr.open_dataset(
     f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/E_N_daily_global/E_N_MJJAS_first10_prime/E_N_day_MPI-ESM1-2-LR_historical_r{pos_ens}i1p1f1_gn_18500501-18590931.nc"
 ).ua
 # eddy driven jet
-pos_mf_zonal = pos_mf.sel(plev = slice(100000, 70000)).mean(dim = 'plev')
+pos_mf_zonal = pos_mf.sel(plev=slice(100000, 70000)).mean(dim="plev")
 pos_mf_zonal = pos_mf_zonal.sel(lon=slice(240, 360)).mean(dim="lon")
 pos_mf_zonal = pos_mf_zonal.sel(time=slice(pos_start_date, pos_end_date))
 pos_mf_event = pos_mf.sel(time=pos_start_date, plev=25000).squeeze()
@@ -89,7 +89,7 @@ neg_mf = xr.open_dataset(
     f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/E_N_daily_global/E_N_MJJAS_first10_prime/E_N_day_MPI-ESM1-2-LR_historical_r{neg_ens}i1p1f1_gn_18500501-18590931.nc"
 ).ua
 # eddy driven jet
-neg_mf_zonal = neg_mf.sel(plev = slice(100000, 70000)).mean(dim = 'plev')
+neg_mf_zonal = neg_mf.sel(plev=slice(100000, 70000)).mean(dim="plev")
 neg_mf_zonal = neg_mf_zonal.sel(lon=slice(240, 360)).mean(dim="lon")
 neg_mf_zonal = neg_mf_zonal.sel(time=slice(neg_start_date, neg_end_date))
 neg_mf_event = neg_mf.sel(time=neg_start_date, plev=25000).squeeze()
@@ -135,10 +135,10 @@ cs_neg = neg_uhat.T.plot.contour(
 )
 
 mf_zonal_plot_pos = pos_mf_zonal.T.plot.contourf(
-    ax=ax3, levels=np.arange(-20, 21, 5), add_colorbar=False
+    ax=ax3, levels=np.arange(-8, 9, 2), add_colorbar=False
 )
 mf_zonal_plot_neg = neg_mf_zonal.T.plot.contourf(
-    ax=ax4, levels=np.arange(-20, 21, 5), add_colorbar=False
+    ax=ax4, levels=np.arange(-8, 9, 2), add_colorbar=False
 )
 
 
@@ -208,5 +208,5 @@ cbar = plt.colorbar(co_mf_neg, cax=cbar_ax, orientation="horizontal")
 cbar.set_label(r"$m^2/s^2$ (zonal mean scaled by 1/5)", loc="center")
 plt.tight_layout(rect=[0, 0.1, 1, 1])
 
-plt.savefig("/work/mh0033/m300883/High_frequecy_flow/docs/plots/wave/extreme_example.png", dpi=300)
+# plt.savefig("/work/mh0033/m300883/High_frequecy_flow/docs/plots/wave/extreme_example.png", dpi=300)
 # %%
