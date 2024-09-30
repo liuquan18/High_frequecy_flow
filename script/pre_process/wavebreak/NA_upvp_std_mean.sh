efb57d424f6e01000d85ce834986a823b1a34771
Quan <quan.liu@mpimet.mpg.de>
2024-09-30 11:16:10 +0200

    avg rather than mean

--- script/pre_process/wavebreak/NA_upvp_std_mean.sh
+++ script/pre_process/wavebreak/NA_upvp_std_mean.sh
@@ -6,15 +6,15 @@ module load parallel
 period=first10
 
 
-wb_index_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/E_N_daily_global/NA_eddy_upvp_${period}/
+wb_index_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/E_N_daily_global/NA_eddy_upvp_first10/
 
 to_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/E_N_daily_global/NA_eddy_upvp_threshold/
 
 
 mkdir -p ${to_path}
 
-files=$(find ${wb_index_path} -name wb_index_*.nc)
+files=$(find ${wb_index_path} -name *.nc)
 
 
 cdo -P 10 -ensmean -apply,ydaystd [ ${files} ] ${to_path}NA_eddy_upvp_${period}_std.nc
-cdo -P 10 -ensmean -apply,ydaymean [ ${files} ] ${to_path}NA_eddy_upvp_${period}_mean.nc
+cdo -P 10 -ensmean -apply,ydayavg [ ${files} ] ${to_path}NA_eddy_upvp_${period}_mean.nc
