b029f94e0d5e5173f349d4002ad1aabbe165d5c8
Quan <quan.liu@mpimet.mpg.de>
2024-10-17 16:45:12 +0200

    tested

--- script/12skwave_breaking/wave_break_year.py
+++ script/12skwave_breaking/wave_break_year.py
@@ -6,96 +6,153 @@ import xarray as xr
 import metpy.calc as mpcalc
 import metpy.units as mpunits
 import cartopy.crs as ccrs
+from cdo import *   # python version
+import glob
 
-# %%
-
-ua_data = xr.open_dataset("/work/mh0033/m300883/High_frequecy_flow/data/tmp/u.nc")
-u = ua_data.ua
 #%%
-va_data = xr.open_dataset("/work/mh0033/m300883/High_frequecy_flow/data/tmp/v.nc")
-v = va_data.va
 # %%
-u = u.sel(plev = 25000, time = '1850-05-01')
-v = v.sel(plev = 25000, time = '1850-05-01')
-# %%
-u = u * mpunits.units('m/s')
-v = v * mpunits.units('m/s')
-# %%
-u = u.metpy.assign_crs(grid_mapping_name='latitude_longitude', earth_radius=6371229)
-v = v.metpy.assign_crs(grid_mapping_name='latitude_longitude', earth_radius=6371229)
-# %%
-# calculate absolute vorticity
-avor = mpcalc.absolute_vorticity(u, v)
-# %%
+def remap(ifile,var = 'ua', plev = 25000):
+    cdo = Cdo()
 
-avor.name = 'AV'
+    ofile = cdo.remapnn("r192x96", input=ifile, options = "-f nc",  returnXArray = var)
+
+    ofile = ofile.sel(plev = plev)
+
+    return ofile
+
+
 # %%
-smoothed = wb.calculate_smoothed_field(data = avor,
+def abs_vorticity(u,v):
+    u = u * mpunits.units('m/s')
+    v = v * mpunits.units('m/s')
+    u = u.metpy.assign_crs(grid_mapping_name='latitude_longitude', earth_radius=6371229)
+    v = v.metpy.assign_crs(grid_mapping_name='latitude_longitude', earth_radius=6371229)
+    avor = mpcalc.absolute_vorticity(u, v)
+    avor.name = 'AV'
+
+    # smooth data
+    smoothed = wb.calculate_smoothed_field(data = avor,
                                        passes = 5)
-# %%
-cdoflux = xr.open_dataset("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/E_N_daily_global/E_N_MJJAS_first10_prime/E_N_day_MPI-ESM1-2-LR_historical_r1i1p1f1_gn_18500501-18590931.nc")
-# %%
-cdoflux = cdoflux.sel(time = '1850-05-01', plev = 25000).ua
-#%%
-cdoflux['time'] = cdoflux.indexes['time'].to_datetimeindex()
-cdoflux['lat'] = smoothed.lat
-cdoflux['lon'] = smoothed.lon
-# %%
-mflux = wb.calculate_momentum_flux(u = u, v = v)
+    
+    return smoothed
 
 # %%
-# calculate contours
-contours = wb.calculate_contours(data=smoothed,
-                                 contour_levels=[7.5* 1e-5 ,9.4* 1e-5, 11.2* 1e-5],
+def wavebreaking(avor, mflux):
+    # calculate contours
+    contours = wb.calculate_contours(data=avor,
+                                 contour_levels=[7.5* 1e-5],
                                  periodic_add=120, # optional
                                  original_coordinates=False) # optional
-#%%
-# calculate overturnings
-overturnings = wb.calculate_overturnings(data=smoothed,
-                                         contour_levels=[7.5* 1e-5 ,9.4* 1e-5, 11.2* 1e-5],
+    
+    # calculate overturnings index
+    overturnings = wb.calculate_overturnings(data=avor,
+                                         contour_levels=[7.5* 1e-5],
                                          contours=contours, #optional
                                          range_group=5, # optional
                                          min_exp=5, # optional
-                                         intensity=cdoflux, # optional
+                                         intensity=mflux, # optional
                                          periodic_add=120) # optional
+    
+    # classify
+    events = overturnings
 
-# %%
-events = overturnings
+    # positive (anticyclonic) as 2, negative (cyclonic) as 1
+    events = events.assign(mean_var=(events.intensity > 0).astype(int) + 1)
+
+    return events
+
 
 #%%
-# mean_var to 1 if events.intensity is negative, to 2 if positive
-events = events.assign(mean_var=(events.intensity < 0).astype(int) + 1)
+ens = 1
+period = 'first10'
+#%%
+def read_data( ens, period):
+    base_dir = '/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/'
+    ufile = glob.glob(base_dir + f'ua_daily_global/ua_MJJAS_{period}/ua_day_*_r{ens}i1p1f1_gn_*.nc')[0]
+    vfile = glob.glob(base_dir + f'va_daily_global/va_MJJAS_{period}/va_day_*_r{ens}i1p1f1_gn_*.nc')[0]
+    mfluxfile = glob.glob(base_dir + f'E_N_daily_global/E_N_MJJAS_{period}_prime/E_N_day_*_r{ens}i1p1f1_gn_*.nc')[0]
+
+    u = remap(ufile, 'ua')
+    v = remap(vfile, 'va')
+    mflux = remap(mfluxfile, 'ua')
 
 
-# %%
+    try:
+        mflux['time'] = mflux.indexes['time'].to_datetimeindex()
+
+    except AttributeError:
+        pass
+    return u,v, mflux
+
+
+
+#%%
+u, v , mflux = read_data(ens, period)
+
+#%%
+avor = abs_vorticity(u,v)
+
+#%%
+events = wavebreaking(avor, mflux)
+
+#%%
 # anticyclonic and cyclonic by intensity for the Northern Hemisphere
 anticyclonic = events[events.intensity >= 0]
 cyclonic = events[events.intensity < 0]
 
 
 # %%
-flag_array = wb.to_xarray(data = smoothed,
+anti_array = wb.to_xarray(data = avor,
                           events = anticyclonic)
+cy_array = wb.to_xarray(data = avor,
+                          events = cyclonic)
+
 # %%
 # import cartopy for projection
 
-wb.plot_step(flag_data=flag_array,
+wb.plot_step(flag_data=anti_array,
              step="1850-05-01T12:00:00", #index or date
-             data=smoothed, # optional
-             contour_levels=np.arange(0,1,0.2), # optional
+             data=avor, # optional
+            #  contour_levels=[0, 7.5*1e-5], # optional
              proj=ccrs.PlateCarree(), # optional
              size=(12,8), # optional
              periodic=True, # optional
+
              labels=True,# optional
              levels=None, # optional
              cmap="Blues", # optional
              color_events="gold", # optional
              title="") # optional
 
+
 # %%
 import matplotlib.pyplot as plt
 fig, ax = plt.subplots(subplot_kw=dict(projection=ccrs.PlateCarree()), figsize=(12, 8))
-p = smoothed.isel(time = 0).plot.contourf(ax=ax, transform=ccrs.PlateCarree(), cmap="Blues", levels=np.arange(-10,11,2)* 1e-5)
+p = avor.isel(time = 1).plot.contourf(ax=ax, transform=ccrs.PlateCarree(), cmap="Blues", levels=np.arange(-10,11,2)* 1e-5)
 ax.coastlines()
-f = flag_array.isel(time = 0).plot.contour(ax=ax, transform=ccrs.PlateCarree(), colors="gold", levels=[0, 1,2])
+f = anti_array.isel(time = 1).plot.contour(ax=ax, transform=ccrs.PlateCarree(), colors="gold", levels=[0, 1,2])
+
+# %%
+tracked = wb.track_events(events=anticyclonic,
+                            time_range=24, #time range for temporal tracking in hours
+                            method="by_overlap", #method for tracking ["by_overlap", "by_distance"], optional
+                            buffer=0, # buffer in degrees for polygons overlapping, optional
+                            overlap=0, # minimum overlap percentage, optinal
+                            distance=1000) # distance in km for method "by_distance"
+
+
+
+# %%
+wb.plot_tracks(data=avor,
+               events=tracked,
+               proj=ccrs.PlateCarree(), # optional
+               size=(12,8), # optional
+               min_path=0, # optional
+               plot_events=True, # optional
+               labels=True, # optional
+               title="") # optional
+
+
+
+
 # %%
