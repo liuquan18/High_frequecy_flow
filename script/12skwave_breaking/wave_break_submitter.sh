ca03c0f61df399748872cfe66f1e1095ced10415
Quan <quan.liu@mpimet.mpg.de>
2024-10-17 17:10:10 +0200

    rename

---
+++ script/12skwave_breaking/wave_break_submitter.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=recon
+#SBATCH --time=04:00:00
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=5
+#SBATCH --mem=0
+#SBATCH --mail-type=FAIL
+#SBATCH --account=mh0033
+#SBATCH --output=recon.%j.out
+
+
+
+mpirun -n 5 python NA_zg_relate_OLR_reconstruct.py $1 $2
