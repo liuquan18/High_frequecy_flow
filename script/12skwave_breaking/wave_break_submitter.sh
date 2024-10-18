a93429b3e09a005db3e5548bf95134da4c98c4e3
Quan <quan.liu@mpimet.mpg.de>
2024-10-18 11:05:42 +0200

    rename

--- script/12skwave_breaking/wave_break_submitter.sh
+++ script/12skwave_breaking/wave_break_submitter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=recon
+#SBATCH --job-name=wb
 #SBATCH --time=08:00:00
 #SBATCH --partition=compute
 #SBATCH --nodes=1
@@ -7,7 +7,7 @@
 #SBATCH --mem=0
 #SBATCH --mail-type=FAIL
 #SBATCH --account=mh0033
-#SBATCH --output=recon.%j.out
+#SBATCH --output=wb.%j.out
 
 
 
