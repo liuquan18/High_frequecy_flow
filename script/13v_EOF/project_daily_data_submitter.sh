610d8c15bf920ae8bbdbc65a2c6c35e70abc00a2
Quan <quan.liu@mpimet.mpg.de>
2024-10-21 14:39:55 +0200

    project

---
+++ script/13v_EOF/project_daily_data_submitter.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=ind_gen
+#SBATCH --time=04:00:00
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks=5
+#SBATCH --mem=0
+#SBATCH --mail-type=FAIL
+#SBATCH --account=mh0033
+#SBATCH --output=ind_gen.%j.out
+
+
+mpirun -n 5 python -u project_daily_data.py $1
+# python -u hello0index_generator.py $1 $2 $3
