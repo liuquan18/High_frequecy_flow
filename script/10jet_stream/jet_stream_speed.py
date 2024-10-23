8d97b76780ac9af863a945e8dd6e4497b55aafb9
Quan <quan.liu@mpimet.mpg.de>
2024-10-23 17:38:26 +0200

    no warning

--- script/10jet_stream/jet_stream_speed.py
+++ script/10jet_stream/jet_stream_speed.py
@@ -15,77 +15,77 @@ from src.jet_stream.jet_speed_and_location import jet_stream_anomaly, jet_event
 from src.jet_stream.jet_stream_plotting import plot_uhat
 
 # %%
-# jet speed
-jet_speed_first10_pos = jet_event(jet_speed_first10_ano, first10_pos_events)
-jet_speed_first10_neg = jet_event(jet_speed_first10_ano, first10_neg_events)
+# # jet speed
+# jet_speed_first10_pos = jet_event(jet_speed_first10_ano, first10_pos_events)
+# jet_speed_first10_neg = jet_event(jet_speed_first10_ano, first10_neg_events)
 
-jet_speed_last10_pos = jet_event(jet_speed_last10_ano, last10_pos_events)
-jet_speed_last10_neg = jet_event(jet_speed_last10_ano, last10_neg_events)
-# %%
-# plot jet speed anomaly
-fig, axes = plt.subplots(3, 1, figsize=(10, 10))
+# jet_speed_last10_pos = jet_event(jet_speed_last10_ano, last10_pos_events)
+# jet_speed_last10_neg = jet_event(jet_speed_last10_ano, last10_neg_events)
+# # %%
+# # plot jet speed anomaly
+# fig, axes = plt.subplots(3, 1, figsize=(10, 10))
 
-# jet speed anomaly
-sns.histplot(
-    jet_speed_first10_ano.values.flatten(),
-    label="first10",
-    color="b",
-    bins=np.arange(-5, 5.2, 0.5),
-    stat="density",
-    ax=axes[0],
-)
+# # jet speed anomaly
+# sns.histplot(
+#     jet_speed_first10_ano.values.flatten(),
+#     label="first10",
+#     color="b",
+#     bins=np.arange(-5, 5.2, 0.5),
+#     stat="density",
+#     ax=axes[0],
+# )
 
-sns.histplot(
-    jet_speed_last10_ano.values.flatten(),
-    label="last10",
-    color="r",
-    bins=np.arange(-5, 5.2, 0.5),
-    stat="density",
-    alpha=0.5,
-    ax=axes[0],
-)
+# sns.histplot(
+#     jet_speed_last10_ano.values.flatten(),
+#     label="last10",
+#     color="r",
+#     bins=np.arange(-5, 5.2, 0.5),
+#     stat="density",
+#     alpha=0.5,
+#     ax=axes[0],
+# )
 
 
-axes[0].set_title("Jet speed anomaly all")
+# axes[0].set_title("Jet speed anomaly all")
 
-sns.histplot(
-    jet_speed_first10_pos,
-    label="first10_pos",
-    color="b",
-    bins=np.arange(-5, 5.2, 0.5),
-    stat="density",
-    ax=axes[1],
-)
+# sns.histplot(
+#     jet_speed_first10_pos,
+#     label="first10_pos",
+#     color="b",
+#     bins=np.arange(-5, 5.2, 0.5),
+#     stat="density",
+#     ax=axes[1],
+# )
 
-sns.histplot(
-    jet_speed_last10_pos,
-    label="last10_pos",
-    color="r",
-    bins=np.arange(-5, 5.2, 0.5),
-    stat="density",
-    alpha=0.5,
-    ax=axes[1],
-)
+# sns.histplot(
+#     jet_speed_last10_pos,
+#     label="last10_pos",
+#     color="r",
+#     bins=np.arange(-5, 5.2, 0.5),
+#     stat="density",
+#     alpha=0.5,
+#     ax=axes[1],
+# )
 
-axes[1].set_title("Jet speed anomaly positive NAO")
+# axes[1].set_title("Jet speed anomaly positive NAO")
 
-sns.histplot(
-    jet_speed_first10_neg,
-    label="first10",
-    color="b",
-    bins=np.arange(-5, 5.2, 0.5),
-    stat="density",
-    ax=axes[2],
-)
+# sns.histplot(
+#     jet_speed_first10_neg,
+#     label="first10",
+#     color="b",
+#     bins=np.arange(-5, 5.2, 0.5),
+#     stat="density",
+#     ax=axes[2],
+# )
 
-sns.histplot(
-    jet_speed_last10_neg,
-    label="last10",
-    color="r",
-    bins=np.arange(-5, 5.2, 0.5),
-    stat="density",
-    alpha=0.5,
-    ax=axes[2],
-)
+# sns.histplot(
+#     jet_speed_last10_neg,
+#     label="last10",
+#     color="r",
+#     bins=np.arange(-5, 5.2, 0.5),
+#     stat="density",
+#     alpha=0.5,
+#     ax=axes[2],
+# )
 
-axes[2].set_title("Jet speed anomaly negative NAO")
+# axes[2].set_title("Jet speed anomaly negative NAO")
