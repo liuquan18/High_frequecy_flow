ded80a6f940402342ecb8e1625106f8acdeb4207
Quan <quan.liu@mpimet.mpg.de>
2024-10-25 10:52:42 +0200

    saved

---
+++ script/14monthly/NAO_jet_wb_plot.py
@@ -0,0 +1,6 @@
+#%%
+import xarray as xr
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+# %%
--- script/14monthly/jet_loc_SouthNorth.py
+++ script/14monthly/jet_loc_SouthNorth.py
@@ -11,38 +11,38 @@ import glob
 logging.basicConfig(level=logging.INFO)
 # %%
 ############## read NAO ###################
-# %%
-first_eofs = xr.open_dataset(
-    "/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/season/EOF_result_first_last2091-2100/first10_eofs.nc"
-)
-last_eofs = xr.open_dataset(
-    "/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/season/EOF_result_first_last2091-2100/last10_eofs.nc"
-)
+def read_NAO_extremes():
+    """both first10 and last10 are need to standardize"""
+    first_eofs = xr.open_dataset(
+        "/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/season/EOF_result_first_last2091-2100/first10_eofs.nc"
+    )
+    last_eofs = xr.open_dataset(
+        "/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/season/EOF_result_first_last2091-2100/last10_eofs.nc"
+    )
 
-# %%
-first_pc = first_eofs.pc.sel(mode="NAO")
-last_pc = last_eofs.pc.sel(mode="NAO")
+    first_pc = first_eofs.pc.sel(mode="NAO")
+    last_pc = last_eofs.pc.sel(mode="NAO")
 
-# %%
-# standardize
-mean = first_pc.mean(dim=("time", "ens"))
-std = first_pc.std(dim=("time", "ens"))
+    # standardize
+    mean = first_pc.mean(dim=("time", "ens"))
+    std = first_pc.std(dim=("time", "ens"))
 
-first_pc = (first_pc - mean) / std
-last_pc = (last_pc - mean) / std
-# %%
-# find time and ens where the value of pc is above 1.5
-first_pos = first_pc.where(first_pc > 1.5)
-last_pos = last_pc.where(last_pc > 1.5)
+    first_pc = (first_pc - mean) / std
+    last_pc = (last_pc - mean) / std
+    
+    # find time and ens where the value of pc is above 1.5
+    first_pos = first_pc.where(first_pc > 1.5)
+    last_pos = last_pc.where(last_pc > 1.5)
 
-# %%
-first_neg = first_pc.where(first_pc < -1.5)
-last_neg = last_pc.where(last_pc < -1.5)
+    
+    first_neg = first_pc.where(first_pc < -1.5)
+    last_neg = last_pc.where(last_pc < -1.5)
 
+    return first_pos, last_pos, first_neg, last_neg
 # %%
 ################# jet location #####################
 # %%
-def read_anomaly(period, same_clim = True, eddy = True):
+def read_jet_anomaly(period, same_clim = True, eddy = True):
 
     # anomaly
     ano_dir = "/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/NA_jet_stream/loc_anomaly/"
@@ -51,9 +51,12 @@ def read_anomaly(period, same_clim = True, eddy = True):
 
     ano_path = f"{ano_dir}jet_stream_anomaly_{eddy_label}_{clima_label}_{period}.nc"
 
-
-
     loc_ano = xr.open_dataset(ano_path).lat_ano
+    # change time format
+    loc_ano['time'] = loc_ano.indexes['time'].to_datetimeindex()
+
+    # select JJA
+    loc_ano = loc_ano.sel(time=loc_ano['time.season'] == 'JJA')
 
     return loc_ano
 
@@ -77,7 +80,10 @@ def count_jet_loc(NAO_extreme, jet):
 
         # count the jet that is north (south) of climatology
 
-        jet_loc_count = selected_jet_ens.groupby('time.month').sum(dim='time')
+        jet_loc_count = selected_jet_ens.resample(time = '1M').sum(dim='time')
+
+        # select data only in JJA
+        jet_loc_count = jet_loc_count.sel(time=jet_loc_count['time.season'] == 'JJA')
 
         jet_counts.append(jet_loc_count)
 
@@ -93,13 +99,11 @@ eddy = False       # if True, use the eddy jet stream
 
 
 #%%
-# for plot the anomaly of all samples
-jet_loc_first10_all = read_anomaly("first10", same_clim = True, eddy = eddy)
-jet_loc_last10_all = read_anomaly("last10", same_clim = True, eddy = eddy)
 
+first_pos, last_pos, first_neg, last_neg = read_NAO_extremes()
 
-jet_loc_first10_ano = read_anomaly("first10", same_clim = same_clim, eddy = eddy)
-jet_loc_last10_ano = read_anomaly("last10", same_clim = same_clim, eddy = eddy)
+jet_loc_first10_ano = read_jet_anomaly("first10", same_clim = same_clim, eddy = eddy)
+jet_loc_last10_ano = read_jet_anomaly("last10", same_clim = same_clim, eddy = eddy)
 
 # %%
 # seperate the jet to north or south
@@ -112,31 +116,39 @@ last_jet_north = xr.where(jet_loc_last10_ano > 0, 1, 0)
 
 
 # %%
-first_pos_jet = count_jet_loc(first_pos, first_jet_north)
-last_pos_jet = count_jet_loc(last_pos, last_jet_north)
+first_pos_north = count_jet_loc(first_pos, first_jet_north)
+last_pos_north = count_jet_loc(last_pos, last_jet_north)
 
-first_neg_jet = count_jet_loc(first_neg, first_jet_south)
-last_neg_jet = count_jet_loc(last_neg, last_jet_south)
+first_pos_south = count_jet_loc(first_pos, first_jet_south)
+last_pos_south = count_jet_loc(last_pos, last_jet_south)
 
-#%%
-first_pos_jet = first_pos_jet.where(first_pos_jet != 0, drop=True)
-last_pos_jet = last_pos_jet.where(last_pos_jet != 0, drop=True)
+first_neg_north = count_jet_loc(first_neg, first_jet_north)
+last_neg_north = count_jet_loc(last_neg, last_jet_north)
 
-first_neg_jet = first_neg_jet.where(first_neg_jet != 0, drop=True)
-last_neg_jet = last_neg_jet.where(last_neg_jet != 0, drop=True)
+first_neg_south = count_jet_loc(first_neg, first_jet_south)
+last_neg_south = count_jet_loc(last_neg, last_jet_south)
+# %%
+# name
+first_pos_north.name = "jet_loc"
+last_pos_north.name = "jet_loc"
+first_pos_south.name = "jet_loc"
+last_pos_south.name = "jet_loc"
 
-#name
-first_pos_jet.name = 'jet_loc'
-last_pos_jet.name = 'jet_loc'
-first_neg_jet.name = 'jet_loc'
-last_neg_jet.name = 'jet_loc'
-
-
+first_neg_north.name = "jet_loc"
+last_neg_north.name = "jet_loc"
+first_neg_south.name = "jet_loc"
+last_neg_south.name = "jet_loc"
 # %%
 # save
-first_pos_jet.to_netcdf("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/season/physics/jet_loc_count/jet_loc_pos_north_first10.nc")
-last_pos_jet.to_netcdf("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/season/physics/jet_loc_count/jet_loc_pos_north_last10.nc")
+first_pos_north.to_netcdf("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/season/physics/jet_loc_count/jet_loc_pos_north_first10.nc")
+last_pos_north.to_netcdf("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/season/physics/jet_loc_count/jet_loc_pos_north_last10.nc")
 
-first_neg_jet.to_netcdf("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/season/physics/jet_loc_count/jet_loc_neg_south_first10.nc")
-last_neg_jet.to_netcdf("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/season/physics/jet_loc_count/jet_loc_neg_south_last10.nc")
+first_pos_south.to_netcdf("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/season/physics/jet_loc_count/jet_loc_pos_south_first10.nc")
+last_pos_south.to_netcdf("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/season/physics/jet_loc_count/jet_loc_pos_south_last10.nc")
+
+first_neg_north.to_netcdf("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/season/physics/jet_loc_count/jet_loc_neg_north_first10.nc")
+last_neg_north.to_netcdf("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/season/physics/jet_loc_count/jet_loc_neg_north_last10.nc")
+
+first_neg_south.to_netcdf("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/season/physics/jet_loc_count/jet_loc_neg_south_first10.nc")
+last_neg_south.to_netcdf("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/season/physics/jet_loc_count/jet_loc_neg_south_last10.nc")
 # %%
