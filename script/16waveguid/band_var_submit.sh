868e834d39c4555f6fbf121dc425ddfd6dbd9b77
Quan <quan.liu@mpimet.mpg.de>
2024-12-01 21:44:19 +0100

    153 cores

--- script/16waveguid/band_var_submit.sh
+++ script/16waveguid/band_var_submit.sh
@@ -2,12 +2,12 @@
 #SBATCH --job-name=mean
 #SBATCH --time=04:00:00
 #SBATCH --partition=compute
-#SBATCH --nodes=1
-#SBATCH --ntasks=10
+#SBATCH --nodes=10
+#SBATCH --ntasks=153
 #SBATCH --mem=0
 #SBATCH --mail-type=FAIL
 #SBATCH --account=mh0033
 #SBATCH --output=mean.%j.out
 
 
-mpirun -n 10 python -u band_variance.py $1
+mpirun -n 153 python -u band_variance.py $1 $2 # period, node
