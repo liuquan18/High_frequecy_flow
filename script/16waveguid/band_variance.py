398cce43dbf6c7b6e0bd08b8cefe7a0d22243c7a
Quan <quan.liu@mpimet.mpg.de>
2024-12-01 18:05:23 +0100

    load to memory

--- script/16waveguid/band_variance.py
+++ script/16waveguid/band_variance.py
@@ -47,12 +47,13 @@ logging.info(f"rank {rank+1}/{size} allocated dask cluster")
 
 # read mean 
 band_means = xr.open_mfdataset(f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/va_daily_global/va_bandmean_{period}/*.nc', combine = 'nested', concat_dim = 'ens')
+band_means.load()
 try:
     band_means = band_means.__xarray_dataarray_variable__
 except AttributeError:
     band_means = band_means.va
 mean = band_means.mean(dim = 'ens')
-
+mean.compute()
 
 for file in files_single:
     # logging.info(f"processing {os.path.basename(file)}")
