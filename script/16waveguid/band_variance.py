390037b2313ea8dc302297c7c057297167e518b5
Quan <quan.liu@mpimet.mpg.de>
2024-12-01 21:38:41 +0100

    use mpi4py for time

--- script/16waveguid/band_variance.py
+++ script/16waveguid/band_variance.py
@@ -15,6 +15,10 @@ logging.basicConfig(level=logging.INFO)
 
 # %%
 # nodes and cores
+period = sys.argv[1]
+node = int(sys.argv[2]) # node =  0-14
+
+#%%
 try:
     from mpi4py import MPI
 
@@ -28,9 +32,9 @@ except:
 
 #%%
 stat = 'var'
-# get period from command line
-period = sys.argv[1]
 
+
+#%%
 from_dir = f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/va_daily_global/va_MJJAS_{period}/'
 to_dir = f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/va_daily_global/va_band{stat}_{period}/'
 
@@ -39,24 +43,15 @@ if not os.path.exists(to_dir):
     os.makedirs(to_dir)
 
 files = glob.glob(from_dir + "*.nc")
-files_single = np.array_split(files, size)[rank] # each node process 5 of the files, totally 10 nodes are used
+files_node = np.array_split(files, 15)[node] # each node process 5 of the files, totally 10 nodes are used
 
-# allocate 10 dask clusters
-client, cluster = init_dask_slurm_cluster(scale =1, processes=20, memory='200GB', walltime='08:00:00')
-logging.info(f"rank {rank+1}/{size} allocated dask cluster")
 
 # read mean 
-band_means = xr.open_mfdataset(f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/va_daily_global/va_bandmean_{period}/*.nc', combine = 'nested', concat_dim = 'ens')
-band_means.load()
-try:
-    band_means = band_means.__xarray_dataarray_variable__
-except AttributeError:
-    band_means = band_means.va
-mean = band_means.mean(dim = 'ens')
-mean.compute()
+band_means = xr.open_mfdataset(f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/va_daily_global/va_bandmean_allens/va_bandmean_{period}_all.nc').__xarray_dataarray_variable__
 
-for file in files_single:
-    # logging.info(f"processing {os.path.basename(file)}")
+for i, file in enumerate(files_node):
+    logging.info("*************************")
+    logging.info(f'node: {node} {i+1}/{len(files_node)}')
 
     v_data = xr.open_dataset(file, chunks={
         'lat':96,
@@ -69,7 +64,25 @@ for file in files_single:
     # weight data with cos(lat)
     v_data = v_data * np.cos(np.deg2rad(v_data.lat))
 
-    variance = band_variance(v_data, mean = mean)
-    variance.to_netcdf(to_dir + f'{os.path.basename(file)}')
+
+    all_times = v_data.time.values
+    single_times = np.array_split(all_times, size)[rank]
+    variance_all = []
+    for i, time in enumerate(single_times):
+        logging.info(f'node: {node} rank: {rank} {i+1}/{len(single_times)}')
+        va = v_data.sel(time = time)
+        variance = band_variance(va, mean = band_means)
+
+        variance_all.append(variance)
+
+
+    # collect all ranks
+    variance_all = comm.gather(variance_all, root=0)
+
+    if rank == 0:
+        variance_all = [item for sublist in variance_all for item in sublist]
+        variance = xr.concat(variance_all, dim='time')
+
+        variance.to_netcdf(to_dir + f'{os.path.basename(file)}')
 
 # %%
