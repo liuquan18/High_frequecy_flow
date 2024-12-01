057a709ddf6ff24c8c6edef45af4f7a60bdb9480
Quan <quan.liu@mpimet.mpg.de>
2024-12-01 17:00:29 +0100

    submit

---
+++ script/16waveguid/band_var_submit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=mean
+#SBATCH --time=04:00:00
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks=10
+#SBATCH --mem=0
+#SBATCH --mail-type=FAIL
+#SBATCH --account=mh0033
+#SBATCH --output=mean.%j.out
+
+
+mpirun -n 10 python -u band_variance.py $1
