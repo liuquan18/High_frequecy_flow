1661c9eecf352b8bbf5b88391d995d4f88a3456a
Quan <quan.liu@mpimet.mpg.de>
2024-12-02 17:21:35 +0100

    unsuccesful

---
+++ script/16waveguid/variance_composite.py
@@ -0,0 +1,178 @@
+# %%
+import xarray as xr
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import seaborn as sns
+import pandas as pd
+import glob
+
+# %%
+import src.composite.composite as comp
+from src.extremes.extreme_read import read_extremes  # NAO extremes
+
+
+# %%
+def read_vvar(period, ens):
+    # Load data
+    vvar_path = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/va_daily_global/va_bandvar_{period}/"
+    vvar_file = glob.glob(f"{vvar_path}*r{ens}i1p1f1*.nc")[0]
+
+    vvar = xr.open_dataset(vvar_file).__xarray_dataarray_variable__
+    try:
+        vvar["time"] = vvar.indexes["time"].to_datetimeindex()
+    except AttributeError:
+        pass
+
+    return vvar
+
+
+# %%
+def composite(period):
+
+    clim_vvar = []
+    NAO_pos_vvar = []
+    NAO_neg_vvar = []
+
+    for ens in range(1, 51):
+        # read NAO
+        NAO_pos, NAO_neg = read_extremes(period, 8, ens, 25000)
+        # read vvar
+        vvar = read_vvar(period, ens)
+        clim_vvar.append(vvar)
+
+        if not NAO_pos.empty:
+            pos_range = comp.lead_lag_30days(NAO_pos, base_plev=25000)
+            vvar_pos = comp.date_range_composite(vvar, pos_range)
+            NAO_pos_vvar.append(vvar_pos)
+
+        if not NAO_neg.empty:
+            neg_range = comp.lead_lag_30days(NAO_neg, base_plev=25000)
+            vvar_neg = comp.date_range_composite(vvar, neg_range)
+            NAO_neg_vvar.append(vvar_neg)
+
+    # concatenate
+    NAO_pos_vvar = xr.concat(NAO_pos_vvar, dim="event")
+    NAO_neg_vvar = xr.concat(NAO_neg_vvar, dim="event")
+    clim_vvar = xr.concat(clim_vvar, dim="ens")
+
+    return NAO_pos_vvar, NAO_neg_vvar, clim_vvar
+
+
+# %%
+first_NAO_pos_vvar, first_NAO_neg_vvar, first_clim_vvar = composite("first10")
+# %%
+last_NAO_pos_vvar, last_NAO_neg_vvar, last_clim_vvar = composite("last10")
+# %%
+fig, axes = plt.subplots(2, 2, figsize=(10, 10))
+levels = np.arange(100, 146, 5)
+
+first_NAO_pos_vvar.mean(dim="event").plot.contourf(
+    x="lon", y="time", ax=axes[0, 0], levels=levels
+)
+last_NAO_pos_vvar.mean(dim="event").plot.contourf(
+    x="lon", y="time", ax=axes[0, 1], levels=levels
+)
+
+first_NAO_neg_vvar.mean(dim="event").plot.contourf(
+    x="lon", y="time", ax=axes[1, 0], levels=levels
+)
+last_NAO_neg_vvar.mean(dim="event").plot.contourf(
+    x="lon", y="time", ax=axes[1, 1], levels=levels
+)
+
+for ax in axes.flatten():
+    ax.set_ylim(10, -10)
+    ax.set_xlim(120, 360)
+
+
+# %%
+# maps
+fig, axes = plt.subplots(
+    2, 2, subplot_kw={"projection": ccrs.PlateCarree(central_longitude=-120)}
+)
+levels = np.arange(100, 146, 5)
+
+first_NAO_pos_vvar.mean(dim="event").sel(time=slice(-10, 5)).mean(
+    dim="time"
+).plot.contourf(
+    x="lon",
+    y="lat",
+    ax=axes[0, 0],
+    levels=levels,
+    extend = 'max',
+    transform=ccrs.PlateCarree(),
+    add_colorbar=False,
+)
+
+last_NAO_pos_vvar.mean(dim="event").sel(time=slice(-10, 5)).mean(
+    dim="time"
+).plot.contourf(
+    x="lon",
+    y="lat",
+    ax=axes[0, 1],
+    levels=levels,
+    extend = 'max',
+    transform=ccrs.PlateCarree(),
+    add_colorbar=False,
+)
+
+first_NAO_neg_vvar.mean(dim="event").sel(time=slice(-10, 5)).mean(
+    dim="time"
+).plot.contourf(
+    x="lon",
+    y="lat",
+    ax=axes[1, 0],
+    levels=levels,
+    extend = 'max',
+    transform=ccrs.PlateCarree(),
+    add_colorbar=False,
+)
+
+last_NAO_neg_vvar.mean(dim="event").sel(time=slice(-10, 5)).mean(
+    dim="time"
+).plot.contourf(
+    x="lon",
+    y="lat",
+    ax=axes[1, 1],
+    levels=levels,
+    extend = 'max',
+    transform=ccrs.PlateCarree(),
+    add_colorbar=False,
+)
+
+for ax in axes.flat:
+    ax.coastlines(color = 'k')
+
+plt.tight_layout()
+# %%
+# climatology maps
+fig, axes = plt.subplots(
+    1, 2, subplot_kw={"projection": ccrs.PlateCarree(central_longitude=-120)}
+)
+
+first_clim_vvar.mean(dim=("ens",'time')).plot.contourf(
+    x="lon",
+    y="lat",
+    ax=axes[0],
+    levels=levels,
+    extend = 'max',
+    transform=ccrs.PlateCarree(),
+    add_colorbar=False,
+)
+
+last_clim_vvar.mean(dim=("ens",'time')).plot.contourf(
+    x="lon",
+    y="lat",
+    ax=axes[1],
+    levels=levels,
+    extend = 'max',
+    transform=ccrs.PlateCarree(),
+    add_colorbar=False,
+)
+
+for ax in axes.flat:
+    ax.coastlines(color = 'k')
+# %%
