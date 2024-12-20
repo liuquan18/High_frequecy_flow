90c01f17451d552125970f9cf9441e383080b4d9
Quan <quan.liu@mpimet.mpg.de>
2024-12-20 15:09:55 +0100

    small cores

--- script/17convection/var_spatial_std_submitter.sh
+++ script/17convection/var_spatial_std_submitter.sh
@@ -3,11 +3,11 @@
 #SBATCH --time=01:00:00
 #SBATCH --partition=compute
 #SBATCH --nodes=1
-#SBATCH --ntasks=10
+#SBATCH --ntasks=5
 #SBATCH --mem=0
 #SBATCH --mail-type=FAIL
 #SBATCH --account=mh0033
 #SBATCH --output=std.%j.out
 
 
-mpirun -n 10 python -u var_spatial_std.py $1 $2 # node, var
+mpirun -n 5 python -u var_spatial_std.py $1 $2 # node, var
