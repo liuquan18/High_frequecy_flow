018318f28fdf60adfdd9f54654252dda1a99338a
Quan <quan.liu@mpimet.mpg.de>
2024-12-20 15:00:52 +0100

    submitter

---
+++ script/17convection/var_spatial_std_submitter.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=std
+#SBATCH --time=01:00:00
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks=10
+#SBATCH --mem=0
+#SBATCH --mail-type=FAIL
+#SBATCH --account=mh0033
+#SBATCH --output=std.%j.out
+
+
+mpirun -n 10 python -u var_spatial_std.py $1 $2 # node, var
