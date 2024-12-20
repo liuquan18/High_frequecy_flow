442f170b52320d91571a1bc84882bf2a5d5dd37a
Quan <quan.liu@mpimet.mpg.de>
2024-12-20 17:07:26 +0100

    larger mem

--- script/17convection/var_spatial_std_submitter.sh
+++ script/17convection/var_spatial_std_submitter.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=compute
 #SBATCH --nodes=1
 #SBATCH --ntasks=5
-#SBATCH --mem=0
+#SBATCH --mem=200G
 #SBATCH --mail-type=FAIL
 #SBATCH --account=mh0033
 #SBATCH --output=std.%j.out
