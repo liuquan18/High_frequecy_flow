658726d917a7c4e370a5aaeb076c0ddfe0af0f50
Quan <quan.liu@mpimet.mpg.de>
2024-12-22 19:52:07 +0100

    save

--- script/17convection/quick_compare.py
+++ script/17convection/quick_compare.py
@@ -9,7 +9,7 @@ import cartopy.crs as ccrs
 import cartopy
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import matplotlib.colors as mcolors
-
+import os
 # %%
 def read_data(var, decade):
     time_tag = f"{decade}0501-{decade+9}0930"
@@ -32,7 +32,7 @@ def read_data(var, decade):
 
 def bin_hus_on_tas(lon_df):
 
-    ts_diff_bins = np.arange(0, 11, 1)
+    ts_diff_bins = np.arange(0, 16, 1)
     ts_diff_bins = np.append(ts_diff_bins, np.inf)  # Add an extra bin for >10
 
     hus_bined = (
@@ -41,7 +41,7 @@ def bin_hus_on_tas(lon_df):
         .mean()
     )
     # add one column called tas_diff, which is the middle value of the bin
-    hus_bined["tas_diff"] = hus_bined.index.map(lambda x: x.mid if x.right != np.inf else 11)
+    hus_bined["tas_diff"] = hus_bined.index.map(lambda x: x.mid if x.right != np.inf else 16) # 16 is the value for >15
     # make the tas_diff as the index
     hus_bined = hus_bined.set_index("tas_diff")
     return hus_bined
@@ -60,10 +60,33 @@ last_hus = read_data("hus", 2090)
 last_data = xr.Dataset({"tas": last_tas, "hus": last_hus})
 last_df = last_data.to_dataframe()[["tas", "hus"]]
 
-# %%
-first_hus_bined = first_df.groupby("lon").apply(bin_hus_on_tas)
 
-last_hus_bined = last_df.groupby("lon").apply(bin_hus_on_tas)
+#%%
+
+# %%
+try:
+    first_tas_95 = xr.open_dataarray("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/tas_hur_variability/first_tas_95.nc")
+except FileNotFoundError:
+    first_tas_95 = first_df.groupby("lon").quantile(0.95).reset_index()
+    first_tas_95.to_netcdf("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/tas_hur_variability/first_tas_95.nc")
+
+try:
+    last_tas_95 = xr.open_dataarray("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/tas_hur_variability/last_tas_95.nc")
+except FileNotFoundError:
+    last_tas_95 = last_df.groupby("lon").quantile(0.95).reset_index()
+    last_tas_95.to_netcdf("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/tas_hur_variability/last_tas_95.nc")
+    
+
+#%%
+try:
+    first_hus_bined = xr.open_dataarray("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/tas_hur_variability/first_hus_bined.nc")
+    last_hus_bined = xr.open_dataarray("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/tas_hur_variability/last_hus_bined.nc")
+
+except FileNotFoundError:
+    first_hus_bined = first_df.groupby("lon").apply(bin_hus_on_tas)
+    last_hus_bined = last_df.groupby("lon").apply(bin_hus_on_tas)
+    first_hus_bined.to_netcdf("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/tas_hur_variability/first_hus_bined.nc")
+    last_hus_bined.to_netcdf("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/tas_hur_variability/last_hus_bined.nc")
 
 diff_hus_bined = last_hus_bined - first_hus_bined
 # %%
@@ -101,8 +124,7 @@ axes = np.array(
         [fig.add_subplot(gs[0, 0], projection=ccrs.PlateCarree(100)), fig.add_subplot(gs[0, 1])],  
         [fig.add_subplot(gs[1, 0], projection=ccrs.PlateCarree(100)), fig.add_subplot(gs[1, 1])],
         [fig.add_subplot(gs[2, 0], projection=ccrs.PlateCarree(100)), fig.add_subplot(gs[2, 1])],
-        [fig.add_subplot(gs[3, 0], projection=ccrs.PlateCarree(100)), None
-        ],
+        [fig.add_subplot(gs[3, 0], projection=ccrs.PlateCarree(100)), None],
     ]
 )
 seq_data_in_the_txt_file = np.loadtxt("/work/mh0033/m300883/High_frequecy_flow/data/colormaps-master/continuous_colormaps_rgb_0-1/prec_seq.txt")
@@ -111,20 +133,21 @@ div_data_in_the_txt_file = np.loadtxt("/work/mh0033/m300883/High_frequecy_flow/d
 seq_prec_cm = mcolors.LinearSegmentedColormap.from_list('colormap', seq_data_in_the_txt_file)
 div_prec_cm = mcolors.LinearSegmentedColormap.from_list('colormap', div_data_in_the_txt_file)
 
-
-y_tick_labels = np.arange(0.5, 10, 1)
+y_tick_labels = np.arange(0.5, 15, 1)
+# add 11 at the end
+y_tick_labels = np.append(y_tick_labels, '>15')
 
 first_plot = first_hus_bined_plot.plot(
     ax=axes[0, 0],
     cmap=seq_prec_cm,
     transform=ccrs.PlateCarree(),
-    levels=np.arange(0, 5, 0.5),
+    levels=np.arange(0, 6, 0.5),
     extend="max",
     add_colorbar=False,
 )
 axes[0, 0].set_title("1850-1859")
 axes[0, 0].set_extent([-180, 180, 0, 60], crs=ccrs.PlateCarree())
-axes[0, 0].set_yticks(np.arange(0, 60, 6) + 3)
+axes[0, 0].set_yticks(np.arange(0, 80, 5) + 2)
 axes[0, 0].set_yticklabels(y_tick_labels)
 axes[0, 0].set_ylabel("tas_diff")
 
@@ -137,13 +160,13 @@ last_plot = last_hus_bined_plot.plot(
     ax=axes[1, 0],
     cmap=seq_prec_cm,
     transform=ccrs.PlateCarree(),
-    levels=np.arange(0, 5, 0.5),
+    levels=np.arange(0, 6, 0.5),
     extend="max",
     add_colorbar=False,
 )
 axes[1, 0].set_title("2090-2099")
 axes[1, 0].set_extent([-180, 180, 0, 60], crs=ccrs.PlateCarree())
-axes[1, 0].set_yticks(np.arange(0, 60, 6) + 3)
+axes[1, 0].set_yticks(np.arange(0, 80, 5) + 2)
 axes[1, 0].set_yticklabels(y_tick_labels)
 axes[1, 0].set_ylabel("tas_diff")
 fig.colorbar(last_plot, cax=axes[1, 1], orientation="vertical")
@@ -159,7 +182,7 @@ diff_plot = diff_hus_bined_plot.plot(
 )
 axes[2, 0].set_title("2090-2099 - 1850-1859")
 axes[2, 0].set_extent([-180, 180, 0, 60], crs=ccrs.PlateCarree())
-axes[2, 0].set_yticks(np.arange(0, 60, 6) + 3)
+axes[2, 0].set_yticks(np.arange(0, 80, 5) + 2)
 axes[2, 0].set_yticklabels(y_tick_labels)
 axes[2, 0].set_ylabel("tas_diff")
 fig.colorbar(diff_plot, cax=axes[2, 1], orientation="vertical")
@@ -174,6 +197,12 @@ axes[3, 0].set_xlabel("lon")
 axes[3, 0].set_ylabel("lat")
 # add xticks and labels
 axes[3, 0].set_xticks(np.arange(-180, 181, 60))
+
+# Make the axes[3,0] smaller with a ratio compared to axes[0,0] and axes[1,0]
+box = axes[3, 0].get_position()
+axes[3, 0].set_position([axes[0,0].get_position().x0, box.y0, box.width * 0.78, box.height * 0.78])
+
+
 
 plt.tight_layout()
 plt.savefig("/work/mh0033/m300883/High_frequecy_flow/docs/plots/moisture/quick_compare.png")
