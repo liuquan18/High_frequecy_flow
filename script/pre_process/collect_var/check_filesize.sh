919edfc88fb9d2b9982ca0ef8047ed060e0b05e4
Quan <quan.liu@mpimet.mpg.de>
2024-12-26 10:28:28 +0100

    as input

--- script/pre_process/collect_var/check_filesize.sh
+++ script/pre_process/collect_var/check_filesize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-directory="/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/hus_daily_std"
-threshold_size=112838625
+directory=$1
+threshold_size=$2
 
 # Find files smaller than the threshold size and print their names
 find "$directory" -type f -size -${threshold_size}c -print
