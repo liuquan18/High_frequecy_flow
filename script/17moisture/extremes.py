15c7697c9803e9e7307c8b771413ecb77331a83f
Quan <quan.liu@mpimet.mpg.de>
2024-12-27 20:02:14 +0100

    extremes

--- script/17moisture/extremes.py
+++ script/17moisture/extremes.py
@@ -9,7 +9,7 @@ import glob
 import logging
 import sys
 import os
-logging.basicConfig(level=logging.WARNING)
+logging.basicConfig(level=logging.ERROR)
 # %%
 import src.ConTrack.contrack as ct
 #%%
@@ -31,17 +31,49 @@ def extract_extreme_1year(data, threshold, gorl = '>=', var = 'tas'):
         twosided=True,
     )
 
-    return EE.flag
+    return EE.dataset
+
+def run_cycle_1year(data, threshold, gorl = '>=', var = 'tas'):
+    EE = ct.contrack()
+    EE.ds = data
+
+    EE.set_up(force=True)
+    EE.run_contrack(
+        variable=var,
+        threshold=threshold, # quantile 0.99 of [0, 60] lat mean, 3.62 K for tas,  2 g/kg for hus, 12/-12 m/s for vt
+        gorl = gorl,
+        overlap=0.5,
+        persistence=5,
+        twosided=True,
+    )
+
+    EE_df = EE.run_lifecycle(flag = 'flag', variable=var)
+    EE_df = EE_df.to_xarray()
+
+    # drop na if row is all na
+    EE_df = EE_df.dropna(axis = 0, how = 'all')
+    
+    return EE_df
 # %%
 def extract_extremes(data, threshold, gorl = '>=', var = 'tas'):
 
     extremes = data.groupby('time.year').apply(extract_extreme_1year, threshold = threshold, gorl = gorl, var = var)
-    return extremes
+    cycles = data.groupby('time.year').apply(run_cycle_1year, threshold = threshold, gorl = gorl, var = var)
+    
+    cycles = cycles.to_dataframe().reset_index().dropna(axis = 0, how = 'any' )
+    # drop column 'index'
+    cycles = cycles.drop(columns = 'index')
+    cycles['Flag'] = cycles['year']*1000 + cycles['Flag']
+
+
+
+    return extremes, cycles
 
 
 # %%
 # nodes for different ensemble members
 node = sys.argv[1]
+var = sys.argv[2] # tas, hus, vt
 try:
     from mpi4py import MPI
 
@@ -55,10 +87,42 @@ except:
 
 #%%
 member = node
-data_path = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/zg_daily/r{member}i1p1f1/"
-save_path = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/vt_daily/r{member}i1p1f1/"
-
+data_path = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/{var}_daily_std/r{member}i1p1f1/"
+extreme_path = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/{var}_daily_extremes/r{member}i1p1f1/"
+cycle_path = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/{var}_daily_cycles/r{member}i1p1f1/"
 if rank == 0:
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
-    logging.info(f"Processing ensemble member {node}")
+    if not os.path.exists(extreme_path):
+        os.makedirs(extreme_path)
+    if not os.path.exists(cycle_path):
+        os.makedirs(cycle_path)
+
+    logging.error(f"Processing ensemble member {node}")
+
+# %%
+all_files =  glob.glob(data_path + "*.nc")
+single_files = np.array_split(all_files, size)[rank]
+# %%
+for i, daily_file in enumerate(single_files):
+    logging.error(f"rank {rank} Processing {i+1}/{len(single_files)}")
+
+    data = xr.open_dataset(daily_file)
+
+    if var == 'tas':
+        threshold = 3.62
+    elif var == 'hus':
+        data = data * 1000 # change unit to g/kg
+        threshold = 2
+    
+    extremes, cycles = extract_extremes(data, threshold = threshold, var = var)
+
+
+    outname = daily_file.split('/')[-1]
+
+    # replace 'day' with 'extreme'
+    extreme_name = outname.replace('day', 'extreme')
+    cycle_name = outname.replace('day', 'cycle').replace('.nc', '.csv')
+
+    extremes.flag.to_netcdf(extreme_path + extreme_name)
+    cycles.to_csv(cycle_path + cycle_name, index = False)
+
+    data.close()
