119caac253d5d9713fd4c227cd9f249de40d3e63
Quan <quan.liu@mpimet.mpg.de>
2024-12-28 15:13:15 +0100

    ymonsub

--- script/pre_process/daily_anomaly/first10_ensmean.sh
+++ script/pre_process/daily_anomaly/first10_ensmean.sh
@@ -10,4 +10,4 @@ file_name=vt_day_MPI-ESM1-2-LR_r*_gn_18500501-18590930.nc
 first_ens_list=($(find $base_path -name $file_name -print))
 
 
-cdo -ensmean -apply,monmean [ ${first_ens_list[@]} ] /work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/vt_monthly_ensmean/first10_vt_monthly_ensmean.nc
+cdo -ensmean -apply,ymonmean [ ${first_ens_list[@]} ] /work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/vt_monthly_ensmean/first10_vt_monthly_ymonmean_ensmean.nc
