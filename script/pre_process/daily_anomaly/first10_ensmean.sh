6db199259c30ef132fa9285735650ec5daa7ddfb
Quan <quan.liu@mpimet.mpg.de>
2024-12-28 14:23:46 +0100

    ensemble mean as a climatology

---
+++ script/pre_process/daily_anomaly/first10_ensmean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+module load cdo
+module load parallel
+
+base_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/vt_daily/
+file_name=vt_day_MPI-ESM1-2-LR_r*_gn_18500501-18590930.nc
+
+# Save the find command output to first_ens_list
+first_ens_list=($(find $base_path -name $file_name -print))
+
+
+cdo -ensmean -apply,monmean [ ${first_ens_list[@]} ] /work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/vt_monthly_ensmean/first10_vt_monthly_ensmean.nc
