350df342f84c4d0eff0a8cb4a9d94f6883d9bb27
Quan <quan.liu@mpimet.mpg.de>
2024-12-29 23:21:24 +0100

    plot save

--- script/17moisture/CC_tangent_regression_map.py
+++ script/17moisture/CC_tangent_regression_map.py
@@ -34,80 +34,103 @@ fig, axes = plt.subplots(
 
 # rows for 'slope', 'tangent', 'slope - tangent'
 # columns for 'first', 'last', 'difference'
-first_slope.plot(
+slope = first_slope.plot(
     ax=axes[0, 0],
     transform=ccrs.PlateCarree(),
     cmap="coolwarm",
-    levels=np.arange(-2.5, 4, 0.1),
+    levels=np.arange(-2, 2.1, 0.1),
     extend='both',
+    add_colorbar = False
 )
 
 first_tangent.plot(
     ax=axes[0, 1],
     transform=ccrs.PlateCarree(),
     cmap="coolwarm",
-    levels=np.arange(-2.5, 4, 0.1),
+    levels=np.arange(-2, 2.1, 0.1),
     extend='both',
-)
+    add_colorbar = False
+    )
 
 (first_slope - first_tangent).plot(
     ax=axes[0, 2],
     transform=ccrs.PlateCarree(),
     cmap="coolwarm",
-    levels=np.arange(-1, 1.1, 0.1),
+    levels=np.arange(-2, 2.1, 0.1),
     extend='both',
+    add_colorbar = False
 )
 
 last_slope.plot(
     ax=axes[1, 0],
     transform=ccrs.PlateCarree(),
     cmap="coolwarm",
-    levels=np.arange(-2.5, 4, 0.1),
+    levels=np.arange(-2, 2.1, 0.1),
     extend='both',
+    add_colorbar = False
 )
 
 last_tangent.plot(
     ax=axes[1, 1],
     transform=ccrs.PlateCarree(),
     cmap="coolwarm",
-    levels=np.arange(-2.5, 4, 0.1),
+    levels=np.arange(-2, 2.1, 0.1),
     extend='both',
+    add_colorbar = False
 )
 (last_slope - last_tangent).plot(
     ax=axes[1, 2],
     transform=ccrs.PlateCarree(),
     cmap="coolwarm",
-    levels=np.arange(-1, 1.1, 0.1),
+    levels=np.arange(-2, 2.1, 0.1),
     extend='both',
+    add_colorbar = False
 )
 
 (last_slope - first_slope).plot(
     ax=axes[2, 0],
     transform=ccrs.PlateCarree(),
     cmap="coolwarm",
-    levels=np.arange(-1, 1.1, 0.1),
+    levels=np.arange(-2, 2.1, 0.1),
     extend='both',
+    add_colorbar = False
 )
 
 (last_tangent - first_tangent).plot(
     ax=axes[2, 1],
     transform=ccrs.PlateCarree(),
     cmap="coolwarm",
-    levels=np.arange(-1, 1.1, 0.1),
+    levels=np.arange(-2, 2.1, 0.1),
     extend='both',
+    add_colorbar = False
 )
 
 ((last_slope - last_tangent) - (first_slope - first_tangent)).plot(
     ax=axes[2, 2],
     transform=ccrs.PlateCarree(),
     cmap="coolwarm",
-    levels=np.arange(-1, 1.1, 0.1),
+    levels=np.arange(-2, 2.1, 0.1),
     extend='both',
+    add_colorbar = False
 )
 
 for ax in axes.flat:
     ax.coastlines()
 
+axes[0, 0].set_title("1850-1859 slope")
+axes[0, 1].set_title("1850-1859 tangent")
+axes[0, 2].set_title("slope - tangent")
+axes[1, 0].set_title("2090-2099 slope")
+axes[1, 1].set_title("2090-2099 tangent")
+axes[1, 2].set_title("slope - tangent")
+axes[2, 0].set_title("2090-2099 - 1850-1859")
+axes[2, 1].set_title("2090-2099 - 1850-1859")
+axes[2, 2].set_title("slope - tangent")
+# add colorbar at the bottom
+cax = fig.add_axes([0.15, 0.01, 0.7, 0.02])
+plt.colorbar(slope, ax=axes, orientation='horizontal', label='slope / tangent', cax=cax)
+
 plt.tight_layout()
 
+plt.savefig("/work/mh0033/m300883/High_frequecy_flow/docs/plots/moisture/slope_tangent_diff_map.png")
 # %%
