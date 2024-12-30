a4eaccbcc61dec62b5a0f4e0700fcda3793801a6
Quan <quan.liu@mpimet.mpg.de>
2024-12-30 10:52:37 +0100

    line to ratio

--- script/17moisture/CC_tangent_regression_line.py
+++ script/17moisture/CC_tangent_regression_line.py
@@ -6,55 +6,8 @@ from src.moisture.longitudinal_contrast import read_data
 from sklearn.linear_model import LinearRegression
 
 # %%
-first_tas = read_data("tas", 1850, (0, 60), meridional_mean=False)
-first_hus = read_data("hus", 1850, (0, 60), meridional_mean=False)
-first_data = xr.Dataset({"tas": first_tas, "hus": first_hus * 1000})
-first_df = first_data.to_dataframe()[["tas", "hus"]]
-
-# %%
-
-last_tas = read_data("tas", 2090, (0, 60), meridional_mean=False)
-last_hus = read_data("hus", 2090, (0, 60), meridional_mean=False)
-last_data = xr.Dataset({"tas": last_tas, "hus": last_hus * 1000})
-last_df = last_data.to_dataframe()[["tas", "hus"]]
-
-
-# %%
-# regression column 'hus' on 'tas'
-def regression(df):
-    X = df["tas"].values.reshape(-1, 1)
-    y = df["hus"].values.reshape(-1, 1)
-    reg = LinearRegression().fit(X, y)
-    return reg
-
-# %%
-
-
-def calculate_saturation_specific_humidity(T):
-    """
-    Calculate saturation specific humidity using Clausius-Clapeyron relation
-
-    Parameters:
-    T (float or array): Temperature in Kelvin
-
-    Returns:
-    float or array: Saturation specific humidity (kg/kg)
-    """
-    # Constants
-    Rv = 461.5  # Gas constant for water vapor (J/(kg·K))
-    Rd = 287.0  # Gas constant for dry air (J/(kg·K))
-    L = 2.5e6  # Latent heat of vaporization (J/kg)
-    es0 = 611.0  # Reference saturation vapor pressure at T0 (Pa)
-    T0 = 273.15  # Reference temperature (K)
-    P0 = 101325  # Standard atmospheric pressure (Pa)
-
-    # Calculate saturation vapor pressure using Clausius-Clapeyron equation
-    es = es0 * np.exp((L / Rv) * (1 / T0 - 1 / T))
-
-    # Calculate saturation specific humidity
-    qs = (Rd / Rv) * es / (P0 - (1 - Rd / Rv) * es)
-
-    return qs
+first_ratio = xr.open_dataset("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/tas_moisture_variability/first_ratio.nc").__xarray_dataarray_variable__
+last_ratio = xr.open_dataset("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/tas_moisture_variability/last_ratio.nc").__xarray_dataarray_variable__
 
 
 # %%
@@ -84,18 +37,12 @@ def calculate_dqs_dT(T):
     dqs_dT = (Rd / Rv) * des_dT * P0 / (P0 - (1 - Rd / Rv) * es) ** 2
 
     return dqs_dT
-
-
-# %%
-plot_tas_std = np.arange(0, 12, 0.1).reshape(-1, 1)
-# %%
-first_reg = regression(first_df)
-first_y = first_reg.predict(plot_tas_std)
+#%%
+first_ratio_2060 = first_ratio.sel(lat = slice (20,60)).mean(dim = ('lat', 'lon'))
+last_ratio_2060 = last_ratio.sel(lat = slice (20,60)).mean(dim = ('lat', 'lon'))
 
 # %%
-last_hus_pred = regression(last_df)
-last_y = last_hus_pred.predict(plot_tas_std)
-
+plot_tas_std = np.arange(0, 10, 0.1)
 # %%
 # tangent line
 # Calculate tangent lines at T=250K and T=290K
@@ -109,13 +56,17 @@ dqs_first = dqs_dT_tangent[0] * delta_tas
 # %%
 dqs_last = dqs_dT_tangent[1] * delta_tas
 # %%
+hus_first = first_ratio_2060.values * delta_tas
+hus_last = last_ratio_2060.values * delta_tas
+
+
 
 # %%
 fig, ax = plt.subplots(1, 1, figsize=(10, 5))
 # sns.scatterplot(data = df, x = 'tas', y = 'hus', hue = 'decade', size='vt_extreme_freq',sizes=(50,500), ax = ax)
 
-ax.plot(plot_tas_std, first_y, label="first10")
-ax.plot(plot_tas_std, last_y, label="last10")
+ax.plot(plot_tas_std, hus_first, label="first10")
+ax.plot(plot_tas_std, hus_last, label="last10")
 
 
 ax.plot(
