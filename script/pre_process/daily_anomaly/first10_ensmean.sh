2b57e7ccc3a929a13f4c603e8134f4fe797985b5
Quan <quan.liu@mpimet.mpg.de>
2024-12-31 12:09:26 +0100

    get the data

--- script/pre_process/daily_anomaly/first10_ensmean.sh
+++ script/pre_process/daily_anomaly/first10_ensmean.sh
@@ -3,11 +3,16 @@
 module load cdo
 module load parallel
 
-base_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/vt_daily/
-file_name=vt_day_MPI-ESM1-2-LR_r*_gn_18500501-18590930.nc
+var='vt'
+
+base_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/${var}_daily/
+to_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/${var}_monthly_ensmean/
+mkdir -p $to_path
+
+file_name=${var}_day_MPI-ESM1-2-LR_r*_gn_18500501-18590930.nc
 
 # Save the find command output to first_ens_list
 first_ens_list=($(find $base_path -name $file_name -print))
 
 
-cdo -ensmean -apply,ymonmean [ ${first_ens_list[@]} ] /work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/vt_monthly_ensmean/first10_vt_monthly_ymonmean_ensmean.nc
+cdo -P 16 -ensmean -apply,ymonmean [ ${first_ens_list[@]} ] ${to_path}first10_${var}_monthly_ymonmean_ensmean.nc
