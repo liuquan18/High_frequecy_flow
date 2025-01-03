6fe0d6cae7bd6093e05c2829babb928cde2bdb76
Quan <quan.liu@mpimet.mpg.de>
2025-01-03 16:22:01 +0100

    override

--- script/pre_process/daily_anomaly/monmean_ensmean.sh
+++ script/pre_process/daily_anomaly/monmean_ensmean.sh
@@ -29,4 +29,4 @@ out_name=${var}_monmean_ensmean_${decade}05_$((${decade}+9))09.nc
 first_ens_list=($(find $base_path -name $file_name -print))
 
 
-cdo -P 5 -ensmean -apply,ymonmean [ ${first_ens_list[@]} ] $to_path${out_name}
+cdo -O -P 5 -ensmean -apply,ymonmean [ ${first_ens_list[@]} ] $to_path${out_name}
