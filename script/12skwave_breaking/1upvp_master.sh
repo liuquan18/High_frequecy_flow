40b4d4da66d602edf34ce7570168168dc2cfd62b
Quan <quan.liu@mpimet.mpg.de>
2025-01-05 17:41:26 +0100

    back

--- script/12skwave_breaking/1upvp_master.sh
+++ script/12skwave_breaking/1upvp_master.sh
@@ -5,6 +5,6 @@ for ens in {1..50}
 do
     echo "Ensemble member ${ens}"
     # run the python script
-    # sbatch 1upvp.sh ${ens}
-    ./1upvp.sh ${ens}
+    sbatch 1upvp.sh ${ens}
+    # ./1upvp.sh ${ens}
 done
