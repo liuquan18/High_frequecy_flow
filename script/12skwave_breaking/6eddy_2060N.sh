4aace9451476f75beaf0a8a3a46d7abb5597331b
Quan <quan.liu@mpimet.mpg.de>
2025-01-07 16:49:11 +0100

    ntask

--- script/12skwave_breaking/6eddy_2060N.sh
+++ script/12skwave_breaking/6eddy_2060N.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=00:30:00
 #SBATCH --partition=compute
 #SBATCH --nodes=1
-#SBATCH --ntasks=5
+#SBATCH --ntasks=25
 #SBATCH --mem=200G
 #SBATCH --mail-type=FAIL
 #SBATCH --account=mh0033
@@ -21,4 +21,4 @@ eke_2060_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/eke_dail
 
 mkdir -p ${eke_2060_path}
 
-find ${eke_path} -name "eke*_r${member}i1p1f1_gn_*.nc" | parallel --jobs 5 cdo -P 5 -fldmean -sellonlatbox,0,360,20,60 -sellevel,25000 {} ${eke_2060_path}{/}
+find ${eke_path} -name "eke*_r${member}i1p1f1_gn_*.nc" | parallel --jobs 25 cdo -fldmean -sellonlatbox,0,360,20,60 -sellevel,25000 {} ${eke_2060_path}{/}
