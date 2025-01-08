b2207e607c18a58c8757230f3a8ecfd7b451e6a2
Quan <quan.liu@mpimet.mpg.de>
2025-01-08 19:47:33 +0100

    name

--- script/17moisture/4tas_hus_hussat_spatial_map.py
+++ script/17moisture/4tas_hus_hussat_spatial_map.py
@@ -112,7 +112,7 @@ first_mean.tas.plot(
     ax=axes[0, 0],
     transform=ccrs.PlateCarree(),
     cmap=temp_cmap_seq,
-    cbar_kwargs={"label": "tas (K)", "shrink": 0.6},
+    cbar_kwargs={"label": "T (K)", "shrink": 0.6},
     levels=temp_levels,
     extend='max',
 )
@@ -124,7 +124,7 @@ first_mean.hussat.plot(
     ax=axes[0, 1],
     transform=ccrs.PlateCarree(),
     cmap=prec_cmap_seq,
-    cbar_kwargs={"label": "hussat (g/kg)", "shrink": 0.6},
+    cbar_kwargs={"label": r"$q^*$ (g/kg)", "shrink": 0.6},
     levels=hus_levels*2,
     extend='max',
 )
@@ -136,7 +136,7 @@ first_mean.hus.plot(
     ax=axes[0, 2],
     transform=ccrs.PlateCarree(),
     cmap=prec_cmap_seq,
-    cbar_kwargs={"label": "hus (g/kg)", "shrink": 0.6},
+    cbar_kwargs={"label": r"$q$ (g/kg)", "shrink": 0.6},
     levels=hus_levels,
     extend='max',
 )
@@ -148,7 +148,7 @@ last_mean.tas.plot(
     ax=axes[1, 0],
     transform=ccrs.PlateCarree(),
     cmap=temp_cmap_seq,
-    cbar_kwargs={"label": "tas (K)", "shrink": 0.6},
+    cbar_kwargs={"label": "T (K)", "shrink": 0.6},
     levels=temp_levels,
     extend='max',
 )
@@ -160,7 +160,7 @@ last_mean.hussat.plot(
     ax=axes[1, 1],
     transform=ccrs.PlateCarree(),
     cmap=prec_cmap_seq,
-    cbar_kwargs={"label": "hussat (g/kg)", "shrink": 0.6},
+    cbar_kwargs={"label": r"$q^*$ (g/kg)", "shrink": 0.6},
     levels=hus_levels*2,
     extend='max',
 )
@@ -172,7 +172,7 @@ last_mean.hus.plot(
     ax=axes[1, 2],
     transform=ccrs.PlateCarree(),
     cmap=prec_cmap_seq,
-    cbar_kwargs={"label": "hus (g/kg)", "shrink": 0.6},
+    cbar_kwargs={"label": r"$q$ (g/kg)", "shrink": 0.6},
     levels=hus_levels,
     extend='max',
 )
@@ -185,7 +185,7 @@ diff_mean.tas.plot(
     ax=axes[2, 0],
     transform=ccrs.PlateCarree(),
     cmap=temp_cmap_div,
-    cbar_kwargs={"label": "tas (K)", "shrink": 0.6},
+    cbar_kwargs={"label": "T (K)", "shrink": 0.6},
     levels=temp_levels_div,
     extend='both',
 )
@@ -197,7 +197,7 @@ diff_mean.hussat.plot(
     ax=axes[2, 1],
     transform=ccrs.PlateCarree(),
     cmap=prec_cmap_div,
-    cbar_kwargs={"label": "hussat (g/kg)", "shrink": 0.6},
+    cbar_kwargs={"label": r"$q^*$ (g/kg)", "shrink": 0.6},
     levels=hus_levels_div*2,
     extend='both',
 )
@@ -209,7 +209,7 @@ diff_mean.hus.plot(
     ax=axes[2, 2],
     transform=ccrs.PlateCarree(),
     cmap=prec_cmap_div,
-    cbar_kwargs={"label": "hus (g/kg)", "shrink": 0.6},
+    cbar_kwargs={"label": r"$q$ (g/kg)", "shrink": 0.6},
     levels=hus_levels_div,
     extend='both',
 )
