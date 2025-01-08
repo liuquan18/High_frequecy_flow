1cdb9311a7fc9720e26d55b6a9584c6af2c16f16
Quan <quan.liu@mpimet.mpg.de>
2025-01-08 09:50:21 +0100

    base func

---
+++ script/pre_process/daily_anomaly/ensmean.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+decade=$1 # 1850, node
+
+echo "Decade ${decade} for variable ${var}"
+
+
+base_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/${var}_daily/
+to_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/${var}_monthly_ensmean/
+mkdir -p $to_path
+
+file_name=${var}_day_MPI-ESM1-2-LR_r*_gn_${decade}*.nc
+out_name=${var}_monmean_ensmean_${decade}05_$((${decade}+9))09.nc
+
+
+# Save the find command output to first_ens_list
+first_ens_list=($(find $base_path -name $file_name -print))
+
+
+cdo -O -P 5 -ensmean -apply,ymonmean [ ${first_ens_list[@]} ] $to_path${out_name}
