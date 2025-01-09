108941d5f16a5e6718cd0c7e8d4058e6070a10f3
Quan <quan.liu@mpimet.mpg.de>
2025-01-09 16:45:37 +0100

    back to 50000

--- script/0index_generate_alldecades/project_daily_index.py
+++ script/0index_generate_alldecades/project_daily_index.py
@@ -27,7 +27,7 @@ logging.info(f"This node is working on ensemble member {member}")
 
 #%%
 start_years_single = np.array_split(start_years, size)[rank]  # years on this core
-daily_zg_path=f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/daily/zg_NA_daily_ano/r{member}i1p1f1/"
+daily_zg_path=f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/zg_NA_daily_ano/r{member}i1p1f1/"
 eof_path = "/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/season/EOF_result/plev_50000_decade_mpi_first_JJA_eof_result.nc"
 pc_path=f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/NAO_pc_decade_nonstd/r{member}i1p1f1/"
 
@@ -44,6 +44,6 @@ for i, start_year in enumerate(start_years_single):
     daily_field = xr.open_dataset(daily_file).zg
     eof = xr.open_dataset(eof_path).eof.sel(mode = 'NAO', decade = str(start_year)).squeeze()
 
-    projected_pcs = project_field_to_pattern(daily_field, eof, standard=False, plev = 25000)
+    projected_pcs = project_field_to_pattern(daily_field, eof, standard=False, plev = 50000)
     projected_pcs.name = "pc"
     projected_pcs.to_netcdf(f"{pc_path}NAO_{str(start_year)}-{str(start_year+9)}_r{member}i1p1f1_nonstd.nc")
