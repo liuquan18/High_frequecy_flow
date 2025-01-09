10908de73c1f0c91336f543ecba67a4629b52d03
Quan <quan.liu@mpimet.mpg.de>
2025-01-09 16:42:24 +0100

    50000

--- script/0index_generate_alldecades/Select_NA.sh
+++ script/0index_generate_alldecades/Select_NA.sh
@@ -5,4 +5,4 @@ echo $SLURM_PROCID is Processing ${infile}
 # replace 'zg_daily_ano' to 'zg_NA_daily_ano'
 outfile=${infile/zg_daily_ano/zg_NA_daily_ano}
 
-cdo -P 10 -sellevel,25000 -sellonlatbox,-90,40,20,80 ${infile} ${outfile}
+cdo -P 10 -sellevel,50000 -sellonlatbox,-90,40,20,80 ${infile} ${outfile}
