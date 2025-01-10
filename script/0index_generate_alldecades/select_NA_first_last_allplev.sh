58b246c6a4ea75caa5bc79de5b8a8034ce9f7581
Quan <quan.liu@mpimet.mpg.de>
2025-01-10 09:24:39 +0100

    save path

--- script/0index_generate_alldecades/select_NA_first_last_allplev.sh
+++ script/0index_generate_alldecades/select_NA_first_last_allplev.sh
@@ -32,7 +32,7 @@ selectNA() {
     echo $SLURM_PROCID is Processing ${infile}
 
     # replace 'zg_daily_ano' to 'zg_NA_daily_ano'
-    outfile=${infile/zg_daily_ano/zg_NA_daily_ano}
+    outfile=${infile/zg_daily_ano/zg_NA_allplev_daily_ano}
 
     cdo -P 10 -sellonlatbox,-90,40,20,80 ${infile} ${outfile}
 }
