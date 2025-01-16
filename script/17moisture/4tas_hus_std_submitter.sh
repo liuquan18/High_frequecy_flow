2c73159c2051a66ef241872ec56d3024a5f40a01
Quan <quan.liu@mpimet.mpg.de>
2025-01-16 13:59:09 +0100

    task

--- script/17moisture/4tas_hus_std_submitter.sh
+++ script/17moisture/4tas_hus_std_submitter.sh
@@ -3,11 +3,11 @@
 #SBATCH --time=05:00:00
 #SBATCH --partition=compute
 #SBATCH --nodes=1
-#SBATCH --ntasks=5
+#SBATCH --ntasks=1
 #SBATCH --mem=200G
 #SBATCH --mail-type=FAIL
 #SBATCH --account=mh0033
 #SBATCH --output=std.%j.out
 
 
-mpirun -n 5 python -u 4tas_hus_hussat_spatial_map_std.py $1
+mpirun -n 1 python -u 4tas_hus_hussat_spatial_map_std.py $1
