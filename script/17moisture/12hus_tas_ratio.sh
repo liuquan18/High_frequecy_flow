94c1628659fdc465bd593e4a24f03650e6612f70
Quan <quan.liu@mpimet.mpg.de>
2025-01-16 12:11:54 +0100

    cdo func

--- script/17moisture/12hus_tas_ratio.sh
+++ script/17moisture/12hus_tas_ratio.sh
@@ -10,4 +10,4 @@ echo Processing $(basename $infile)
 
 # replace _ano with _ano_lowlevel
 outfile=${husfile//hus/hus_tas}
-cdo -P 8 -vertmean -sellevel,100000,85000 $infile $outfile
+cdo -P 8 div $husfile $tasfile $outfile
