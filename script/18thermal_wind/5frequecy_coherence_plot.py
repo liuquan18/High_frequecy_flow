b810a6b062a1c3907c7a7ac955b902f4ef2adb63
Quan <quan.liu@mpimet.mpg.de>
2025-01-16 10:23:07 +0100

    format

--- script/18thermal_wind/5frequecy_coherence_plot.py
+++ script/18thermal_wind/5frequecy_coherence_plot.py
@@ -24,26 +24,33 @@ f = Cxy['frequency']
 #%%
 
 fig, ax1 = plt.subplots()
-
-ax1.plot(f, Cxy_mean, label = 'mean')
-ax1.set_xlabel('frequency [Hz]')
+ax1.plot(f, Cxy_mean, label='mean', color='k')
+ax1.set_xlabel('frequency [cycles per day]')
 ax1.set_ylabel('Coherence')
+ax1.set_xlim(0, 0.53)
 
 # fill between
-ax1.fill_between(f, Cxy_5, Cxy_95, color = 'gray', alpha = 0.5)
-ax1.set_xlim(0, 0.55)
+ax1.fill_between(f, Cxy_5, Cxy_95, color='gray', alpha=0.5)
+
+# ax1.set_xscale('log')  # Set x-axis to log scale
+
 # Create a secondary x-axis on top
 ax2 = ax1.twiny()
 ax2.set_xlim(ax1.get_xlim())
-ax2.set_xticks(ax1.get_xticks())
-ax2.set_xticklabels([f'{1/x:.1f}' if x != 0 else 'inf' for x in ax1.get_xticks()])
+# ax2.set_xscale('log')  # Match the log scale of the primary x-axis
+
+# ticks for ax2
+ticks = [1/2, 1/5, 1/12, 1/30]
+ax2.set_xticks(ticks)
+ax2.set_xticklabels([f'{int(1/t)}' for t in ticks])
 ax2.set_xlabel('Period [days]')
 
+
 # Add vertical lines at days = 2 and days = 12
-days = [2, 12]
-for day in days:
-    freq = 1 / day
-    ax2.axvline(freq, color='r', linestyle='--')
+x2 = 1/2
+x12 = 1/12
+ax1.axvline(x=x2, color='r', linestyle='--')
+ax1.axvline(x=x12, color='r', linestyle='--')
 
 plt.savefig("/work/mh0033/m300883/High_frequecy_flow/docs/plots/mositure_paper_v1/vt_va_coherence.png", dpi = 300)
 
