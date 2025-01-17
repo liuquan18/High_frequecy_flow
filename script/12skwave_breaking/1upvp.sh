37a4297efb488ee4d801a53090f24d0964e7bde1
Quan <quan.liu@mpimet.mpg.de>
2025-01-17 16:05:38 +0100

    frequency

--- script/12skwave_breaking/1upvp.sh
+++ script/12skwave_breaking/1upvp.sh
@@ -16,20 +16,29 @@ node=$1
 member=$node
 echo "Ensemble member ${member}"
 
+frequency=${2:-prime} # prime (2-12 days) or high (2-6 days), default prime
+
 u_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/ua_daily/r${member}i1p1f1/
 v_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/va_daily/r${member}i1p1f1/
 
-up_path=/scratch/m/m300883/up/r${member}i1p1f1/
-vp_path=/scratch/m/m300883/vp/r${member}i1p1f1/
+if [ "$frequency" == "prime" ]; then
+    up_path=/scratch/m/m300883/up/r${member}i1p1f1/
+    vp_path=/scratch/m/m300883/vp/r${member}i1p1f1/
+    upvp_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/upvp_daily/r${member}i1p1f1/
+else
+    up_path=/scratch/m/m300883/upp/r${member}i1p1f1/
+    vp_path=/scratch/m/m300883/vpp/r${member}i1p1f1/
+    upvp_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/upvp_high_daily/r${member}i1p1f1/
+fi
 
 tmp_dir=/scratch/m/m300883/upvp/r${member}i1p1f1/
-upvp_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/upvp_daily/r${member}i1p1f1/
 
 mkdir -p ${upvp_path} ${up_path} ${vp_path} ${tmp_dir}
 
 export u_path up_path 
 export v_path vp_path 
 export upvp_path member tmp_dir
+export frequency
 
 # function to band filter
 band_filter(){
@@ -39,10 +48,16 @@ band_filter(){
     fname=$(basename ${infile%.nc})
 
     # split years
-    cdo -splityear ${infile} ${tmp_dir}${fname}_year
-    # band filter, keep 2-12 days 
+    cdo -O -splityear ${infile} ${tmp_dir}${fname}_year
+    # band filter
     year_files=$(ls ${tmp_dir}${fname}_year*)
-    cdo -O -mergetime -apply,bandpass,30.5,182.5 [ ${year_files} ] ${outfile}
+    if [ "$frequency" == "prime" ]; then
+        echo "Filtering 2-12 days"
+        cdo -O -mergetime -apply,bandpass,30.5,182.5 [ ${year_files} ] ${outfile}
+    elif [ "$frequency" == "high" ]; then
+        echo "Filtering 2-6 days"
+        cdo -O -mergetime -apply,bandpass,60.8,182.5 [ ${year_files} ] ${outfile}
+    fi
     # remove temporary files
     rm ${tmp_dir}${fname}_year*
 }
