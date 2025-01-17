4da8625dac3f583b8eea3cab613b8867fa48fb57
Quan <quan.liu@mpimet.mpg.de>
2025-01-17 16:31:45 +0100

    var

--- script/12skwave_breaking/6eddy_2060N.sh
+++ script/12skwave_breaking/6eddy_2060N.sh
@@ -14,10 +14,11 @@ module load parallel
 
 node=$1
 member=$node
+var=$2 # eke, eke_high
 echo "Ensemble member ${member}"
 
-eke_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/eke_daily_ano/r${member}i1p1f1/
-eke_2060_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/eke_daily_ano_2060N/r${member}i1p1f1/
+eke_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/${var}_daily_ano/r${member}i1p1f1/
+eke_2060_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/${var}_daily_ano_2060N/r${member}i1p1f1/
 
 mkdir -p ${eke_2060_path}
 
