9025701d63b9031a5e61e22d59edbbcd38bcafd3
Quan <quan.liu@mpimet.mpg.de>
2025-01-17 15:07:23 +0100

    info

--- script/12skwave_breaking/1eddy_kinetic_energy_master.sh
+++ script/12skwave_breaking/1eddy_kinetic_energy_master.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 frequency=${1:-prime} # prime (2-12 days) or high (2-6 days), default prime
+# print info, if frequency is not prime or high
+if [ "$frequency" == "prime" ]; then
+    echo frequency is set to prime
+    echo "Filtering 2-12 days"
+
+elif [ "$frequency" == "high" ]; then
+    echo frequency is set to high
+    echo "Filtering 2-6 days"
+fi
 
 #for loop 1-50
 for ens in {1..50}
