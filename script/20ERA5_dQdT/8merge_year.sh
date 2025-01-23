927ab7682d0b67a0c2cdab05094e363955d0cad8
Quan <quan.liu@mpimet.mpg.de>
2025-01-23 14:24:09 +0100

    no delete

--- script/20ERA5_dQdT/8merge_year.sh
+++ script/20ERA5_dQdT/8merge_year.sh
@@ -27,8 +27,6 @@ merge_year(){
     year_files=$(find ${daily_dir} -name "*.nc" | grep ${year})
     cdo -r -O -P 8 mergetime ${year_files} ${to_dir}E5pl00_1D_${var}_250hPa_daily_${year}-05-01_${year}-09-31.nc
 
-    # rm tmp files
-    rm ${year_files}
 }
 
 export -f merge_year
