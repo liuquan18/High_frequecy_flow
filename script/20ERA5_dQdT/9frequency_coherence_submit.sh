786c33978c2f803021983131dae1a7412282c558
Quan <quan.liu@mpimet.mpg.de>
2025-01-23 13:44:18 +0100

    submit

---
+++ script/20ERA5_dQdT/9frequency_coherence_submit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=coherence
+#SBATCH --time=03:00:00
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --mem=0
+#SBATCH --mail-type=FAIL
+#SBATCH --account=mh0033
+#SBATCH --output=coherence.%j.out
+
+module load parallel
+module load cdo
+
+var1=$1
+var2=$2
+split_basin=$3
+pixel_wise=$4
+
+python /work/mh0033/m300883/High_frequecy_flow/script/20ERA5_dQdT/9frequency_coherence.py \
+    ${var1} ${var2} ${split_basin} ${pixel_wise}
