9c08c7a0e51bd3d10cc8cef7fcec6e9f8be9f2db
Quan <quan.liu@mpimet.mpg.de>
2025-01-23 10:56:12 +0100

    format

--- script/20ERA5_dQdT/10DtDq_map.py
+++ script/20ERA5_dQdT/10DtDq_map.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 import xarray as xr
 import numpy as np
 import sys
@@ -15,7 +15,9 @@ from src.moisture.plot_utils import draw_box
 from src.moisture.longitudinal_contrast import read_data
 
 # %%
-tas_mean = xr.open_dataset("/work/mh0033/m300883/High_frequecy_flow/data/ERA5/moisture_variability_stat/tas_daily_std_mergeyear_timmean.nc")
+tas_mean = xr.open_dataset(
+    "/work/mh0033/m300883/High_frequecy_flow/data/ERA5/moisture_variability_stat/tas_daily_std_mergeyear_timmean.nc"
+)
 tas_mean = tas_mean.var130.squeeze()
 # %%
 temp_cmap_seq = np.loadtxt(
@@ -37,20 +39,23 @@ prec_cmap_div = np.loadtxt(
     "/work/mh0033/m300883/High_frequecy_flow/data/colormaps-master/continuous_colormaps_rgb_0-1/prec_div.txt"
 )
 prec_cmap_div = mcolors.ListedColormap(prec_cmap_div, name="prec_div")
-#%%
-temp_levels = np.arange(0,16,1)
-hus_levels = np.arange(0,5,0.5)
+# %%
+temp_levels = np.arange(0, 16, 1)
+hus_levels = np.arange(0, 5, 0.5)
 
-temp_levels_div = np.arange(-5,5.5,0.5)
-hus_levels_div = np.arange(-1.5,1.6,0.1)
+temp_levels_div = np.arange(-5, 5.5, 0.5)
+hus_levels_div = np.arange(-1.5, 1.6, 0.1)
 
 # %%
 fig, axes = plt.subplots(
-    1, 2, figsize=(12, 6), subplot_kw={"projection": ccrs.PlateCarree(-90)}
+    1, 2, figsize=(12, 5), subplot_kw={"projection": ccrs.PlateCarree(-90)}
 )
 
-tas_mean.plot(ax=axes[0], transform=ccrs.PlateCarree(), cmap=temp_cmap_seq, levels=temp_levels)
+tas_mean.plot(
+    ax=axes[0], transform=ccrs.PlateCarree(), cmap=temp_cmap_seq, levels=temp_levels,
+    cbar_kwargs={"label": r"$\Delta T$ (K)", "shrink": 0.6, 'ticks': np.arange(0, 16, 5)},
 
+)
 
 
 axes[0].set_title(r"$\Delta_{T}$")
