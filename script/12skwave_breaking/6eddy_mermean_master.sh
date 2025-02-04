bfe2502ea3adea47fb11fb9a6c7c8258bd26f368
Quan <quan.liu@mpimet.mpg.de>
2025-02-04 17:11:13 +0100

    try 30

--- script/12skwave_breaking/6eddy_mermean_master.sh
+++ script/12skwave_breaking/6eddy_mermean_master.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 var=$1 # eke, eke_high
+lat_min=${2:-20}  # Default to 20 if $2 is not provided
+lat_max=${3:-60}  # Default to 60 if $3 is not provided
 #for loop 1-50
 for ens in {1..50}
 do
     echo "Ensemble member ${ens}"
     # run the python script
-    sbatch 6eddy_mermean.sh ${ens} ${var} 40 60
+    sbatch 6eddy_mermean.sh ${ens} ${var} ${lat_min} ${lat_max}
     # ./1upvp.sh ${ens}
 done
