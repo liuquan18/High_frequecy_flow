94d3695425820c9f298ce8448ec221bd79eefd03
Quan <quan.liu@mpimet.mpg.de>
2025-02-04 16:07:29 +0100

    rename

---
+++ script/12skwave_breaking/6eddy_mermean.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=eddy_sel
+#SBATCH --time=00:30:00
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks=25
+#SBATCH --mem=200G
+#SBATCH --mail-type=FAIL
+#SBATCH --account=mh0033
+#SBATCH --output=eddy_sel.%j.out
+
+module load cdo
+module load parallel
+
+node=$1
+member=$node
+var=$2 # eke, eke_high
+echo "Ensemble member ${member}"
+
+eke_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/${var}_daily_ano/r${member}i1p1f1/
+eke_2060_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/${var}_daily_ano_2060N/r${member}i1p1f1/
+
+mkdir -p ${eke_2060_path}
+
+# find ${eke_path} -name "eke*_r${member}i1p1f1_gn_*.nc" | parallel --jobs 25 cdo -mermean -sellonlatbox,0,360,20,60 -sellevel,25000 {} ${eke_2060_path}{/}
+find ${eke_path} -name "eke*_r${member}i1p1f1_gn_*.nc" | parallel --jobs 25 cdo -mermean -sellonlatbox,0,360,20,60 -vertmean -sellevel,100000,85000 {} ${eke_2060_path}{/}
