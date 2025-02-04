5ff60b3e76c4a8b04f211dfedc8166dbe4b30bf5
Quan <quan.liu@mpimet.mpg.de>
2025-02-04 15:31:24 +0100

    low-level

--- script/12skwave_breaking/6eddy_2060N.sh
+++ script/12skwave_breaking/6eddy_2060N.sh
@@ -22,4 +22,5 @@ eke_2060_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/${var}_d
 
 mkdir -p ${eke_2060_path}
 
-find ${eke_path} -name "eke*_r${member}i1p1f1_gn_*.nc" | parallel --jobs 25 cdo -mermean -sellonlatbox,0,360,20,60 -sellevel,25000 {} ${eke_2060_path}{/}
+# find ${eke_path} -name "eke*_r${member}i1p1f1_gn_*.nc" | parallel --jobs 25 cdo -mermean -sellonlatbox,0,360,20,60 -sellevel,25000 {} ${eke_2060_path}{/}
+find ${eke_path} -name "eke*_r${member}i1p1f1_gn_*.nc" | parallel --jobs 25 cdo -mermean -sellonlatbox,0,360,20,60 -vertmean -sellevel,100000,85000 {} ${eke_2060_path}{/}
