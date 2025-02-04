a183827ff4e5e4a03905370d43fa3964bc7195fa
Quan <quan.liu@mpimet.mpg.de>
2025-02-04 16:14:15 +0100

    new region

--- script/12skwave_breaking/6eddy_mermean_master.sh
+++ script/12skwave_breaking/6eddy_mermean_master.sh
@@ -6,6 +6,6 @@ for ens in {1..50}
 do
     echo "Ensemble member ${ens}"
     # run the python script
-    sbatch 6eddy_mermean.sh ${ens} ${var}
+    sbatch 6eddy_mermean.sh ${ens} ${var} 40 60
     # ./1upvp.sh ${ens}
 done
