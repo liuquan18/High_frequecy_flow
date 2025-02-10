9d6d1bd71b664594ecb1f7c82a9ee030a8a76c0f
Quan <quan.liu@mpimet.mpg.de>
2025-02-10 20:38:35 +0100

    all plevs

---
+++ script/17moisture_plevs/0var_spatial_std_submitter.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=std
+#SBATCH --time=02:00:00
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks=5
+#SBATCH --mem=200G
+#SBATCH --mail-type=FAIL
+#SBATCH --account=mh0033
+#SBATCH --output=std.%j.out
+
+
+mpirun -n 5 python -u 0var_spatial_std.py $1 $2 $3 # node, var, name
