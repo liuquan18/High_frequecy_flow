18a376a0dbf5be9418e04e2fb895fe8de60bef49
Quan <quan.liu@mpimet.mpg.de>
2025-02-17 11:58:44 +0100

    parallel

--- script/12skwave_breaking/13eke_before_NAO_15_5_mean.py
+++ script/12skwave_breaking/13eke_before_NAO_15_5_mean.py
@@ -13,22 +13,6 @@ import os
 from src.extremes.before_extreme import read_NAO_extremes, sel_before_NAO
 logging.basicConfig(level=logging.INFO)
 #%%
-# nodes for different decades
-try:
-    from mpi4py import MPI
-
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()  # [0,1,2,3,4,5,6,7,8,9]
-    size = comm.Get_size()  # 10
-
-except:
-    logging.warning("::: Warning: Proceeding without mpi4py! :::")
-    rank = 0
-    size = 1
-
-if rank == 0:
-    logging.info(f"::: Running on {size} cores :::")
-
 
 
 #%%
@@ -92,11 +76,11 @@ def process_data(decade, var):
     NAO_pos, NAO_neg, data = read_all_data(decade, var = var)
 
     # select data before NAO events, here 'var' is only for column name
-    logging.info (f"rank {rank} is selecting data for {decade} \n")
+    logging.info (f"selecting data for {decade} \n")
     eke_NAO_pos = before_NAO_mean(NAO_pos, data)
     eke_NAO_neg = before_NAO_mean(NAO_neg, data)
 
-    logging.info(f"rank {rank} is saving data for decade {decade} \n")
+    logging.info(f"saving data for decade {decade} \n")
     save_dir_pos=f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0stat_results/eke_NAO_pos_15_5_mean_{decade}.nc'
     save_dir_neg=f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0stat_results/eke_NAO_neg_15_5_mean_{decade}.nc'
 
@@ -107,9 +91,10 @@ def process_data(decade, var):
 
 #%%
 
-decades = [1850, 2090]
-decade = decades[rank]
-logging.info(f"rank {rank} is processing decade {decade} \n")
+# nodes for different decades
+decade = int(sys.argv[1]) if len(sys.argv) > 1 else 1850
+
+logging.info(f"processing decade {decade} \n")
 process_data(decade, 'eke')
 
 
