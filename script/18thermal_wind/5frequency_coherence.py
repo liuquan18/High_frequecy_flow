32253d92ca240a8fb4e5f9a25d7ed710689dfec1
Quan <quan.liu@mpimet.mpg.de>
2025-02-18 11:20:05 +0100

    also vtm

--- script/18thermal_wind/5frequency_coherence.py
+++ script/18thermal_wind/5frequency_coherence.py
@@ -52,6 +52,8 @@ elif var1 == 'tas_std':
 elif var1 == 'hus_tas':
     var1_path = f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/hus_tas_daily_std/r{member}i1p1f1/'
 
+elif var1 == 'vtm':
+    var1_path = f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/vtm_daily_ano/r{member}i1p1f1/'
 
 if var2 == 'va':
     var2_path = f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/va_daily_ano_lowlevel/r{member}i1p1f1/'
