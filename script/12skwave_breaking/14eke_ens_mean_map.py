2860b278fa766ab3b5ec0b7fdb81aead74ce311a
Quan <quan.liu@mpimet.mpg.de>
2025-02-19 14:42:56 +0100

    remove zonal mean

--- script/12skwave_breaking/14eke_ens_mean_map.py
+++ script/12skwave_breaking/14eke_ens_mean_map.py
@@ -6,6 +6,11 @@ import cartopy.crs as ccrs
 from src.plotting.util import erase_white_line
 import matplotlib.colors as mcolors
 import cartopy
+#%%
+import src.plotting.util as util
+import importlib
+importlib.reload(util)
+from src.plotting.util import lon2x
 
 # %%
 ensmean_first = xr.open_dataset("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0stat_results/eke_ensmean_50000_1850.nc")
@@ -54,7 +59,21 @@ NAO_neg_first = erase_white_line(NAO_neg_first)
 
 NAO_pos_last = erase_white_line(NAO_pos_last)
 NAO_neg_last = erase_white_line(NAO_neg_last)
+#%%
+def remove_zonalmean(arr):
+    arr = arr - arr.mean(dim = 'lon')
+    return arr
+#%%
+# ensmean_first = remove_zonalmean(ensmean_first)
+# ensmean_last = remove_zonalmean(ensmean_last)
 
+NAO_pos_first = remove_zonalmean(NAO_pos_first)
+NAO_neg_first = remove_zonalmean(NAO_neg_first)
+
+NAO_pos_last = remove_zonalmean(NAO_pos_last)
+NAO_neg_last = remove_zonalmean(NAO_neg_last)
+
+#%%
 # %%
 fig, axes = plt.subplots(3, 3, figsize=(10, 6), subplot_kw={'projection': ccrs.PlateCarree(-90)})
 
@@ -115,7 +134,7 @@ plt.figtext(0.68, 0.63, "h", fontsize=12, fontweight='bold')
 plt.figtext(0.68, 0.31, "i", fontsize=12, fontweight='bold')
 
 
-plt.savefig("/work/mh0033/m300883/High_frequecy_flow/docs/plots/mositure_paper_v1/eke_maps.pdf", dpi = 300)
+# plt.savefig("/work/mh0033/m300883/High_frequecy_flow/docs/plots/mositure_paper_v1/eke_maps.pdf", dpi = 300)
 # %%
 fig, axes = plt.subplots(3, 2, figsize=(10, 6), subplot_kw={'projection': ccrs.PlateCarree(-90)})
 
@@ -136,6 +155,11 @@ for ax in axes.flatten():
     ax.set_extent([-180, 180, 0, 90], crs=ccrs.PlateCarree())
     ax.axhline(20, color='k', lw=0.5, ls='dotted')
     ax.axhline(60, color='k', lw=0.5, ls='dotted')
+
+axes[2,1].axvline(x=lon2x(-145, axes[2,1]), ymin=0.22, ymax=0.65, color='k', lw=0.5, ls='dotted')
+axes[2,1].axvline(x=lon2x(140, axes[2,1]), ymin=0.22, ymax=0.65, color='k', lw=0.5, ls='dotted')
+axes[2,1].axvline(x=lon2x(-35, axes[2,1]), ymin=0.22, ymax=0.65, color='k', lw=0.5, ls='dotted')
+axes[2,1].axvline(x=lon2x(-70, axes[2,1]), ymin=0.22, ymax=0.65, color='k', lw=0.5, ls='dotted')
 
 # latitude ticks
 for i, ax in enumerate(axes.flatten()):
@@ -160,9 +184,9 @@ plt.figtext(0.04, 0.95, "a", fontsize=12, fontweight='bold')
 plt.figtext(0.04, 0.63, "b", fontsize=12, fontweight='bold')
 plt.figtext(0.04, 0.31, "c", fontsize=12, fontweight='bold')
 
-plt.figtext(0.36, 0.95, "d", fontsize=12, fontweight='bold')
-plt.figtext(0.36, 0.63, "e", fontsize=12, fontweight='bold')
-plt.figtext(0.36, 0.31, "f", fontsize=12, fontweight='bold')
+plt.figtext(0.52, 0.95, "d", fontsize=12, fontweight='bold')
+plt.figtext(0.52, 0.63, "e", fontsize=12, fontweight='bold')
+plt.figtext(0.52, 0.31, "f", fontsize=12, fontweight='bold')
 
 plt.tight_layout(w_pad = 0.5, h_pad = 1.1)
 plt.savefig("/work/mh0033/m300883/High_frequecy_flow/docs/plots/mositure_paper_v1/eke_NAO_diff_maps.pdf", dpi = 300)
