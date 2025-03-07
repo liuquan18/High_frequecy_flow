a615e0bada3c4f0f240a56ae54032192013c6504
Quan <quan.liu@mpimet.mpg.de>
2025-03-07 14:41:54 +0100

    to func

---
+++ src/prime/prime_data.py
@@ -0,0 +1,30 @@
+import xarray as xr
+import numpy as np
+import glob
+
+def read_prime( decade, suffix = '_ano', var='eke', name = None, **kwargs):
+    """
+    read high frequency data
+    """
+    if name is None:
+        name = kwargs.get('name', var) # default name is the same as var
+    plev = kwargs.get('plev', 50000)
+
+    time_tag = f"{decade}0501-{decade+9}0930"
+    data_path = (
+        f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/{var}_daily{suffix}/"
+    )
+    files = glob.glob(data_path + "r*i1p1f1/" + f"{var}*{time_tag}*.nc")
+    # sort files
+    files.sort(key=lambda x: int(x.split('/')[-2][1:].split('i')[0]))
+
+    data = xr.open_mfdataset(
+        files, combine="nested", concat_dim="ens",
+        chunks = {"ens": 1, "time": -1, "lat": -1, "lon": -1, "plev": 1}
+    )
+    data = data[name]
+    data = data.sel(plev = plev)
+
+    data['ens'] = range(1, 51)
+
+    return data
