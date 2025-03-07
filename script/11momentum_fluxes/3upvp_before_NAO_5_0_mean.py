1a0cccf17cd4bfde8462b4f2c59b52d94c27934c
Quan <quan.liu@mpimet.mpg.de>
2025-03-07 14:53:59 +0100

    for upvp

--- script/11momentum_fluxes/3upvp_before_NAO_5_0_mean.py
+++ script/11momentum_fluxes/3upvp_before_NAO_5_0_mean.py
@@ -11,36 +11,10 @@ import re
 import os
 
 from src.extremes.before_extreme import read_NAO_extremes
+from src.composite.composite import before_NAO_mean
+from src.prime.prime_data import read_prime
 logging.basicConfig(level=logging.INFO)
 
-
-#%%
-def read_hf( decade, suffix = '_ano', var='eke', name = None, **kwargs):
-    """
-    read high frequency data
-    """
-    if name is None:
-        name = kwargs.get('name', var) # default name is the same as var
-    plev = kwargs.get('plev', 50000)
-
-    time_tag = f"{decade}0501-{decade+9}0930"
-    data_path = (
-        f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/{var}_daily{suffix}/"
-    )
-    files = glob.glob(data_path + "r*i1p1f1/" + f"{var}*{time_tag}*.nc")
-    # sort files
-    files.sort(key=lambda x: int(x.split('/')[-2][1:].split('i')[0]))
-
-    data = xr.open_mfdataset(
-        files, combine="nested", concat_dim="ens",
-        chunks = {"ens": 1, "time": -1, "lat": -1, "lon": -1, "plev": 1}
-    )
-    data = data[name]
-    data = data.sel(plev = plev)
-
-    data['ens'] = range(1, 51)
-
-    return data
 
     
 #%%
@@ -51,7 +25,7 @@ def read_all_data(decade, var, **kwargs):
     NAO_neg = read_NAO_extremes(decade, 'negative')
 
     logging.info("reading upvp")
-    hf_data = read_hf( decade, var = var, suffix='_ano', **kwargs)  # change the suffix to read different data
+    hf_data = read_prime( decade, var = var, suffix='_ano', **kwargs)  # change the suffix to read different data
     
 
     return NAO_pos, NAO_neg, hf_data
@@ -60,7 +34,7 @@ def read_all_data(decade, var, **kwargs):
 #%%
 def process_data(decade, var):
     # read data
-    NAO_pos, NAO_neg, data = read_all_data(decade, var = var, kwargs={'name': 'upvp', 'plev': 25000})
+    NAO_pos, NAO_neg, data = read_all_data(decade, var = var, kwargs={'name': 'ua', 'plev': 25000})
 
     # select data before NAO events, here 'var' is only for column name
     logging.info (f"selecting data for {decade} \n")
@@ -68,8 +42,8 @@ def process_data(decade, var):
     eke_NAO_neg = before_NAO_mean(NAO_neg, data)
 
     logging.info(f"saving data for decade {decade} \n")
-    save_dir_pos=f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0stat_results/eke_NAO_pos_15_5_mean_{decade}.nc'
-    save_dir_neg=f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0stat_results/eke_NAO_neg_15_5_mean_{decade}.nc'
+    save_dir_pos=f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0stat_results/upvp_NAO_pos_5_0_mean_{decade}.nc'
+    save_dir_neg=f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0stat_results/upvp_NAO_neg_5_0_mean_{decade}.nc'
 
 
     eke_NAO_pos.to_netcdf(save_dir_pos)
@@ -84,6 +58,3 @@ decade = int(sys.argv[1]) if len(sys.argv) > 1 else 1850
 logging.info(f"processing decade {decade} \n")
 process_data(decade, 'eke')
 
-
-
-# %%
