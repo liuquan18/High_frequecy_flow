5e6e25e7a1cbd842ec3e2458c1dc602e3cb8b97f
Quan <quan.liu@mpimet.mpg.de>
2025-03-10 18:40:58 +0100

    submit

---
+++ script/22VKE/4qprime_master.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+frequency=${1:-prime} # prime (2-12 days) or high (2-6 days), default prime
+# print info, if frequency is not prime or high
+if [ "$frequency" == "prime" ]; then
+    echo frequency is set to prime
+    echo "Filtering 2-12 days"
+
+elif [ "$frequency" == "high" ]; then
+    echo frequency is set to high
+    echo "Filtering 2-6 days"
+fi
+
+#for loop 1-50
+for ens in {1..50}
+do
+    echo "Ensemble member ${ens}"
+    # run the python script
+    sbatch 4qprime.sh ${ens} ${frequency}
+    # ./1upvp.sh ${ens}
+done
