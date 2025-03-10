d6bc2e5d36ef2c69f298959e206594bdb01cc2c5
Quan <quan.liu@mpimet.mpg.de>
2025-03-10 16:50:12 +0100

    projection

--- script/19plots_main/before_NAO_pos_m_neg.py
+++ script/19plots_main/before_NAO_pos_m_neg.py
@@ -27,7 +27,7 @@ def remove_zonalmean(arr):
     arr = arr - arr.mean(dim = 'lon')
     return arr
 #%%
-def postprocess(ds, do_smooth = True, remove_zonal = True):
+def postprocess(ds, do_smooth = False, remove_zonal = False):
     if do_smooth:
         ds = smooth(ds)
     if remove_zonal:
@@ -63,6 +63,9 @@ uhat_neg_last10 = xr.open_dataarray(f"{uhat_composiste}jetstream_MJJAS_last10_ne
 #%%
 uhat_NAO_first = uhat_pos_first10 - uhat_neg_first10
 uhat_NAO_last = uhat_pos_last10 - uhat_neg_last10
+#%%
+uhat_NAO_first = postprocess(uhat_NAO_first)
+uhat_NAO_last = postprocess(uhat_NAO_last)
 
 # %%
 eke_NAO_pos_first, eke_NAO_neg_first = read_composite('eke', 'eke', 1850)
@@ -110,7 +113,7 @@ uhat_levels_div = np.arange(-12, 13, 2)
 
 
 # %%
-fig, axes = plt.subplots(3, 3, figsize=(10, 8), subplot_kw={'projection': ccrs.PlateCarree(-90)})
+fig, axes = plt.subplots(3, 3, figsize=(10, 11), subplot_kw={'projection': ccrs.Orthographic(-20, 90)})
 uhat_NAO_first.plot.contourf(ax=axes[0, 0], transform=ccrs.PlateCarree(), cmap=temp_cmap_div, levels=uhat_levels_div, extend='both', add_colorbar=False)
 uhat_NAO_last.plot.contourf(ax=axes[1, 0], transform=ccrs.PlateCarree(), cmap=temp_cmap_div, levels=uhat_levels_div, extend='both', add_colorbar=False)
 uhat_diff = (uhat_NAO_last - uhat_NAO_first).plot.contourf(ax=axes[2, 0], transform=ccrs.PlateCarree(), cmap=temp_cmap_div, levels=uhat_levels_div, extend='both', add_colorbar=False)
@@ -134,33 +137,34 @@ cbar_eke.set_label(r'$m^2/s^2$')
 
 for ax in axes.flatten():
     ax.coastlines()
-    ax.set_extent([-180, 180, 0, 90], crs=ccrs.PlateCarree())
+    # ax.set_extent([-180, 180, 0, 90], crs=ccrs.PlateCarree())
     ax.axhline(20, color='k', lw=0.5, ls='dotted')
     ax.axhline(60, color='k', lw=0.5, ls='dotted')
 
-axes[2,2].axvline(x=lon2x(-145, axes[2,2]), ymin=0.22, ymax=0.65, color='k', lw=0.5, ls='dotted')
-axes[2,2].axvline(x=lon2x(140, axes[2,2]), ymin=0.22, ymax=0.65, color='k', lw=0.5, ls='dotted')
-axes[2,2].axvline(x=lon2x(-30, axes[2,2]), ymin=0.22, ymax=0.65, color='k', lw=0.5, ls='dotted')
-axes[2,2].axvline(x=lon2x(-70, axes[2,2]), ymin=0.22, ymax=0.65, color='k', lw=0.5, ls='dotted')
+# axes[2,2].axvline(x=lon2x(-145, axes[2,2]), ymin=0.22, ymax=0.65, color='k', lw=0.5, ls='dotted')
+# axes[2,2].axvline(x=lon2x(140, axes[2,2]), ymin=0.22, ymax=0.65, color='k', lw=0.5, ls='dotted')
+# axes[2,2].axvline(x=lon2x(-30, axes[2,2]), ymin=0.22, ymax=0.65, color='k', lw=0.5, ls='dotted')
+# axes[2,2].axvline(x=lon2x(-70, axes[2,2]), ymin=0.22, ymax=0.65, color='k', lw=0.5, ls='dotted')
 
 # latitude ticks
 for i, ax in enumerate(axes.flatten()):
-    ax.set_xticks(np.arange(-180, 180, 60), crs=ccrs.PlateCarree())
+    # ax.set_xticks(np.arange(-180, 180, 60), crs=ccrs.PlateCarree())
 
-    if i % 3 == 0:
-        ax.set_yticks([20, 60], crs=ccrs.PlateCarree())
-        ax.set_ylabel("Latitude")
-        ax.yaxis.set_major_formatter(cartopy.mpl.ticker.LatitudeFormatter())
-    else:
-        ax.set_yticks([])
+    # if i % 3 == 0:
+    #     ax.set_yticks([20, 60], crs=ccrs.PlateCarree())
+    #     ax.set_ylabel("Latitude")
+    #     ax.yaxis.set_major_formatter(cartopy.mpl.ticker.LatitudeFormatter())
+    # else:
+    #     ax.set_yticks([])
     
-    if i // 3 == 3:
-        ax.set_xticklabels(['180°', '120°W', '60°W', '0°', '60°E', '120°E'])
-    else:
-        ax.set_xticks([])
+    # if i // 3 == 3:
+    #     ax.set_xticklabels(['180°', '120°W', '60°W', '0°', '60°E', '120°E'])
+    # else:
+    #     ax.set_xticks([])
 
     ax.set_xlabel("")
     ax.set_ylabel("")
+
 axes[0, 0].set_title(r"1850 $\bar{u}$")
 axes[0, 1].set_title(r"1850 $u'v'$")
 axes[0, 2].set_title(r"1850 EKE")
