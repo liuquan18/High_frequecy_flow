03acff8c62df67c2b47424dd1c3aee07105e3782
Quan <quan.liu@mpimet.mpg.de>
2025-03-14 14:44:19 +0100

    work

---
+++ script/21ERA5_allplev/5IVKE.py
@@ -0,0 +1,57 @@
+# %%
+import xarray as xr
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+import os
+import sys
+import logging
+
+logging.basicConfig(level=logging.INFO)
+# %%
+try:
+    from mpi4py import MPI
+
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()  # [0,1,2,3,4,5,6,7,8,9]
+    size = comm.Get_size()  # 10
+except:
+    logging.warning("::: Warning: Proceeding without mpi4py! :::")
+    rank = 0
+    size = 1
+# %%
+from_path = "/work/mh0033/m300883/High_frequecy_flow/data/ERA5_allplev/vke_daily/"
+to_path = "/work/mh0033/m300883/High_frequecy_flow/data/ERA5_allplev/ivke_daily/"
+
+if rank == 0:
+    if not os.path.exists(to_path):
+        os.makedirs(to_path)
+
+
+# %%
+all_files = glob.glob(from_path + "*.nc")
+files_core = np.array_split(all_files, size)[rank]
+
+
+# %%
+def ivke(vke):
+    vke = vke.sortby(
+        "plev", ascending=False
+    )  # make sure plev is in descending order, p_B is larger than p_T
+    d_vke_dp = vke.differentiate("plev")
+    ivke = d_vke_dp.integrate("plev")
+    ivke = -1 * ivke / 9.81
+    ivke.name = "ivke"
+    return ivke
+
+
+# %%
+for i, file in enumerate(files_core):
+    logging.info(f"rank {rank} Processing {i+1}/{len(files_core)}")
+    ds = xr.open_dataset(file, chunks={"time": 1})
+
+    ds = ivke(ds["vke"])
+
+    ds.to_netcdf(file.replace("vke", "ivke"))
+    ds.close()
+# %%
