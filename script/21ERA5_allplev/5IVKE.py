correct sign

script/21ERA5_allplev/5IVKE.py
@@ -35,12 +35,9 @@ files_core = np.array_split(all_files, size)[rank]
 
 # %%
 def ivke(vke):
-    vke = vke.sortby(
-        "plev", ascending=False
-    )  # make sure plev is in descending order, p_B is larger than p_T
     d_vke_dp = vke.differentiate("plev")
     ivke = d_vke_dp.integrate("plev")
-    ivke = -1 * ivke / 9.81
+    ivke =  ivke / 9.81
     ivke.name = "ivke"
     return ivke
 
@@ -49,6 +46,7 @@ def ivke(vke):
 for i, file in enumerate(files_core):
     logging.info(f"rank {rank} Processing {i+1}/{len(files_core)}")
     ds = xr.open_dataset(file, chunks={"time": 1})
+    ds = ds.sortby("plev", ascending=True)
 
     ds = ivke(ds["vke"])
 