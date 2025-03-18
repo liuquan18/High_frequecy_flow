d665c2d0a9a6aa2bee31a57adc93459f6695e1ce
Quan <quan.liu@mpimet.mpg.de>
2025-03-18 11:15:49 +0100

    with var

--- script/22VKE/5ivke_ensmean_master.sh
+++ script/22VKE/5ivke_ensmean_master.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+var=$1
 for decade in {1850..2090..10}
 do
     echo "This node is calculating upvp for ${decade} to $((decade+9))"
-    sbatch 5var_ensmean.sh $decade ivke
+    sbatch 5var_ensmean.sh $decade $var
 
 done
