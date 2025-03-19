d036b524c25bd0e2fbbf09ba0b95518b7b424335
Quan <quan.liu@mpimet.mpg.de>
2025-03-19 15:09:42 +0100

    remove trend from ERA5

--- script/pre_process/ERA5/4season_trend_from_daily.sh
+++ script/pre_process/ERA5/4season_trend_from_daily.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
-#SBATCH --job-name=trend
-#SBATCH --time=01:30:00
+#SBATCH --job-name=trend_day
+#SBATCH --time=03:30:00
 #SBATCH --partition=compute
 #SBATCH --nodes=1
-#SBATCH --ntasks=10
-#SBATCH --cpus-per-task=10
+#SBATCH --ntasks=5
+#SBATCH --cpus-per-task=5
 #SBATCH --mem=0
 #SBATCH --mail-type=FAIL
 #SBATCH --account=mh0033
-#SBATCH --output=trend.%j.out
+#SBATCH --output=trend_day.%j.out
 
 # module load cdo 
 module load parallel
 
 var=$1
 
-var_daily_dir=/work/mh0033/m300883/High_frequecy_flow/data/ERA5_allplev/${var}_daily/
-
-daily_files=($(find $var_daily_dir -name "*.nc" -print))
 to_dir=/work/mh0033/m300883/High_frequecy_flow/data/ERA5_allplev/${var}_monthly_stat/
 
 mkdir -p $to_dir
 
 export var_daily_dir daily_files to_dir var
 
-
-
 trend(){
+    local mon=$1
+    shift
 
-    mon=$1
+    local var_daily_dir=/work/mh0033/m300883/High_frequecy_flow/data/ERA5_allplev/${var}_daily/
+
+    local daily_files=($(find $var_daily_dir -name "*.nc" -print))
+
     echo "month averaging $mon"
     cdo -r -f nc -O -P 10 -monmean -mergetime -apply,selmon,$mon [ ${daily_files[@]} ] ${to_dir}${var}_monthly_${mon}.nc
 
@@ -39,12 +39,10 @@ trend(){
 
     # monthly data pre-process
     # trend
-    cdo -P 10 -trend -selmon,$mon $month_pre_data $afile $bfile
-
-
+    cdo -P 2 -trend $month_pre_data $afile $bfile
 }
 
 
 export -f trend
 
-parallel --jobs 5 trend ::: {05..09}
+parallel --jobs 5 trend ::: {5..9}
