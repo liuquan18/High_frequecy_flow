ba7aab8dcf9192977466219a856733b2990e69b5
Quan <quan.liu@mpimet.mpg.de>
2025-03-19 13:52:40 +0100

    option of ieke

--- script/12eke/13eke_before_NAO_15_5_mean.py
+++ script/12eke/13eke_before_NAO_15_5_mean.py
@@ -14,27 +14,25 @@ from src.extremes.before_extreme import read_NAO_extremes
 from src.composite.composite import before_NAO_mean
 from src.prime.prime_data import read_prime
 logging.basicConfig(level=logging.INFO)
-#%%
-
 
     
 #%%
-def read_all_data(decade, var, name = 'eke'):
+def read_all_data(decade, var, **kwargs):
     logging.info("reading NAO extremes")
     # wave breaking
     NAO_pos = read_NAO_extremes(decade, 'positive')
     NAO_neg = read_NAO_extremes(decade, 'negative')
 
     logging.info(f"reading {var}")
-    eke = read_prime( decade, var = var, suffix='_ano', name = name)  # change the suffix to read different data
+    eke = read_prime( decade, var = var, **kwargs)  # change the suffix to read different data
     
 
     return NAO_pos, NAO_neg, eke
 
 #%%
-def process_data(decade, var, name = 'eke'):
+def process_data(decade, var, **kwargs):
     # read data
-    NAO_pos, NAO_neg, data = read_all_data(decade, var = var, name = name)
+    NAO_pos, NAO_neg, data = read_all_data(decade, var = var, **kwargs)
 
     # select data before NAO events, here 'var' is only for column name
     logging.info (f"selecting data for {decade} \n")
@@ -42,8 +40,8 @@ def process_data(decade, var, name = 'eke'):
     eke_NAO_neg = before_NAO_mean(NAO_neg, data)
 
     logging.info(f"saving data for decade {decade} \n")
-    save_dir_pos=f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0stat_results/eke_NAO_pos_15_5_mean_{decade}.nc'
-    save_dir_neg=f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0stat_results/eke_NAO_neg_15_5_mean_{decade}.nc'
+    save_dir_pos=f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0stat_results/{var}_NAO_pos_15_5_mean_{decade}.nc'
+    save_dir_neg=f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0stat_results/{var}_NAO_neg_15_5_mean_{decade}.nc'
 
 
     eke_NAO_pos.to_netcdf(save_dir_pos)
@@ -61,4 +59,5 @@ process_data(decade, 'eke')
 
 
 # %%
-process_data(1850, 'ieke', 'ieke')
+process_data(1850, 'ieke', name = 'ieke', suffix = '', plev = None)
+# %%
