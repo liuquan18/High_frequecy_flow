1f48bb10652f03b6cc3af917e86ce6a3767a1e1b
Quan <quan.liu@mpimet.mpg.de>
2025-03-28 18:32:02 +0100

    move

--- script/pre_process/prime/3eddy_fluxes.sh
+++ script/pre_process/prime/3eddy_fluxes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=upvp
+#SBATCH --job-name=mul
 #SBATCH --time=01:30:00
 #SBATCH --partition=compute
 #SBATCH --nodes=1
@@ -7,7 +7,7 @@
 #SBATCH --mem=200G
 #SBATCH --mail-type=FAIL
 #SBATCH --account=mh0033
-#SBATCH --output=upvp.%j.out
+#SBATCH --output=mul.%j.out
 
 module load cdo/2.5.0-gcc-11.2.0
 module load parallel
