f225df9da72f8e32f6569a2c0f1c1e685bce280a
Quan <quan.liu@mpimet.mpg.de>
2025-03-28 15:39:50 +0100

    prime

---
+++ script/23EPflux/0var_prime_master.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+var=$1 # variable to run
+frequency=${2:-prime} # prime (2-12 days) or high (2-6 days), default prime
+# print info, if frequency is not prime or high
+if [ "$frequency" == "prime" ]; then
+    echo frequency is set to prime
+    echo "Filtering 2-12 days"
+
+elif [ "$frequency" == "high" ]; then
+    echo frequency is set to high
+    echo "Filtering 2-6 days"
+fi
+
+#for loop 1-50
+for ens in {1..50}
+do
+    echo "Ensemble member ${ens}"
+    # run the python script
+    sbatch 0var_prime.sh ${ens} ${var} ${frequency}
+    # ./1upvp.sh ${ens}
+done
