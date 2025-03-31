a1b87216b32aabb6c6658d511eeb5b1bef8ad2bc
Quan <quan.liu@mpimet.mpg.de>
2025-03-31 18:39:13 +0200

    integrate option

--- script/23EPflux/3prime_before_NAO_15_5_mean.py
+++ script/23EPflux/3prime_before_NAO_15_5_mean.py
@@ -13,6 +13,7 @@ import os
 from src.extremes.before_extreme import read_NAO_extremes
 from src.composite.composite import before_NAO_mean
 from src.prime.prime_data import read_prime
+from src.prime.prime_data import vert_integrate
 logging.basicConfig(level=logging.INFO)
 
 
@@ -32,7 +33,7 @@ def read_all_data(decade, var, **kwargs):
 
 
 #%%
-def process_data(decade, var, **kwargs):
+def process_data(decade, var, integrate=False, **kwargs):
     # read data
     NAO_pos, NAO_neg, data = read_all_data(decade, var = var, **kwargs)
 
@@ -40,6 +41,10 @@ def process_data(decade, var, **kwargs):
     logging.info (f"selecting data for {decade} \n")
     ivke_NAO_pos = before_NAO_mean(NAO_pos, data, (-15, -5))
     ivke_NAO_neg = before_NAO_mean(NAO_neg, data, (-15, -5))
+
+    if integrate:
+        ivke_NAO_pos = vert_integrate(ivke_NAO_pos)
+        ivke_NAO_neg = vert_integrate(ivke_NAO_neg)
 
     logging.info(f"saving data for decade {decade} \n")
     save_dir_pos=f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0stat_results/{var}_NAO_pos_15_5_mean_{decade}.nc'
@@ -56,7 +61,8 @@ if __name__ == "__main__":
     # nodes for different decades
     decade = int(sys.argv[1]) if len(sys.argv) > 1 else 1850
     var = str(sys.argv[2]) if len(sys.argv) > 2 else 'vptp'
+    integrate = bool(sys.argv[3]) if len(sys.argv) > 3 else False
 
     logging.info(f"processing decade {decade} of {var} \n")
-    process_data(decade, var, kwargs = {'name': var, 'plev': 50000})
+    process_data(decade, var, integrate, kwargs = {'name': var, 'plev': None})
 
