35cc68e634bd2046fbf448d10dc79e9ad62242a7
Quan <quan.liu@mpimet.mpg.de>
2025-04-02 18:15:59 +0200

    no ano

--- script/23EPflux/3prime_before_NAO_15_5_mean.py
+++ script/23EPflux/3prime_before_NAO_15_5_mean.py
@@ -47,9 +47,13 @@ def process_data(decade, var, integrate=False, **kwargs):
         ivke_NAO_neg = vert_integrate(ivke_NAO_neg)
 
     logging.info(f"saving data for decade {decade} \n")
-    save_dir_pos=f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0stat_results/{var}_NAO_pos_15_5_mean_{decade}.nc'
-    save_dir_neg=f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0stat_results/{var}_NAO_neg_15_5_mean_{decade}.nc'
-
+    suffix = kwargs.get('suffix', '_ano')
+    if suffix == '_ano':
+        save_dir_pos=f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0stat_results/{var}_NAO_pos_15_5_mean_{decade}.nc'
+        save_dir_neg=f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0stat_results/{var}_NAO_neg_15_5_mean_{decade}.nc'
+    elif suffix == '':
+        save_dir_pos=f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0stat_results_without_ano/{var}_NAO_pos_15_5_mean_{decade}.nc'
+        save_dir_neg=f'/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0stat_results_without_ano/{var}_NAO_neg_15_5_mean_{decade}.nc'
 
     ivke_NAO_pos.to_netcdf(save_dir_pos)
     ivke_NAO_neg.to_netcdf(save_dir_neg)
