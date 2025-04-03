858e1d1c509830d640d5f1d24df219ff526dd608
Quan <quan.liu@mpimet.mpg.de>
2025-04-03 14:01:24 +0200

    plot

--- script/19plots_main/eddy_heat_before_NAO_pos_or_neg.py
+++ script/19plots_main/eddy_heat_before_NAO_pos_or_neg.py
@@ -36,7 +36,7 @@ def remove_zonalmean(arr):
 
 
 # %%
-def postprocess(ds, do_smooth=False, remove_zonal=False):
+def postprocess(ds, do_smooth=True, remove_zonal=False):
     if do_smooth:
         ds = smooth(ds)
     if remove_zonal:
@@ -46,13 +46,21 @@ def postprocess(ds, do_smooth=False, remove_zonal=False):
 
 
 # %%
-def read_composite_MPI(var, name, decade):
-    pos_file = glob.glob(
-        f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0stat_results/{var}_NAO_pos_15_5_mean_{decade}.nc"
-    )
-    neg_file = glob.glob(
-        f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0stat_results/{var}_NAO_neg_15_5_mean_{decade}.nc"
-    )
+def read_composite_MPI(var, name, decade, ano = False):
+    if ano:
+        pos_file = glob.glob(
+            f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0stat_results/{var}_NAO_pos_15_5_mean_{decade}.nc"
+        )
+        neg_file = glob.glob(
+            f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0stat_results/{var}_NAO_neg_15_5_mean_{decade}.nc"
+        )
+    else:
+        pos_file = glob.glob(
+            f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0stat_results_without_ano/{var}_NAO_pos_15_5_mean_{decade}.nc"
+        )
+        neg_file = glob.glob(
+            f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0stat_results_without_ano/{var}_NAO_neg_15_5_mean_{decade}.nc"
+        )
     if len(pos_file) == 0 or len(neg_file) == 0:
         raise ValueError(f"no file found for {var} in {decade}")
     NAO_pos = xr.open_dataset(pos_file[0])
@@ -101,27 +109,27 @@ vpqp_neg_last = vert_integrate(vpqp_neg_last)
 # %%
 qflux_pos_first = xr.Dataset(
     {
-        "u": upqp_pos_first,
-        "v": vpqp_pos_first,
+        "u": upqp_pos_first * 1e3,
+        "v": vpqp_pos_first * 1e3,
     }
 )
 
 qflux_pos_last = xr.Dataset(
     {
-        "u": upqp_pos_last,
-        "v": vpqp_pos_last,
+        "u": upqp_pos_last * 1e3,
+        "v": vpqp_pos_last * 1e3,
     }
 )
 qflux_neg_first = xr.Dataset(
     {
-        "u": upqp_neg_first,
-        "v": vpqp_neg_first,
+        "u": upqp_neg_first * 1e3,
+        "v": vpqp_neg_first * 1e3,
     }
 )
 qflux_neg_last = xr.Dataset(
     {
-        "u": upqp_neg_last,
-        "v": vpqp_neg_last,
+        "u": upqp_neg_last * 1e3,
+        "v": vpqp_neg_last * 1e3,
     }
 )
 # %%
@@ -146,11 +154,15 @@ prec_cmap_div = np.loadtxt(
 prec_cmap_div = mcolors.ListedColormap(prec_cmap_div, name="prec_div")
 
 # %%
-zg_levels = np.arange(-30, 31, 5)
-uhat_levels_div = np.arange(-12, 13, 2)
-upvp_levels_div = np.arange(-2, 2.1, 0.5)
+# for anomaly
+# upvp_levels_div = np.arange(-2, 2.1, 0.5)
 vptp_levels_div = np.arange(-1, 1.1, 0.2)
-scale = 0.0005
+
+vptp_levels = np.arange(-10, 11, 2)
+
+scale = 5
+
+scale_div = 0.5
 
 
 # %%
@@ -162,7 +174,7 @@ vptp_pos_first.plot.contourf(
     ax=axes[0, 0],
     transform=ccrs.PlateCarree(),
     cmap=temp_cmap_div,
-    levels=vptp_levels_div,
+    levels=vptp_levels,
     extend="both",
     add_colorbar=False,
 )
@@ -171,7 +183,7 @@ vptp_pos_map = vptp_pos_last.plot.contourf(
     ax=axes[0, 1],
     transform=ccrs.PlateCarree(),
     cmap=temp_cmap_div,
-    levels=vptp_levels_div,
+    levels=vptp_levels,
     extend="both",
     add_colorbar=False,
 )
@@ -181,7 +193,7 @@ vptp_diff_map = (vptp_pos_last - vptp_pos_first).plot.contourf(
     ax=axes[0, 2],
     transform=ccrs.PlateCarree(),
     cmap=temp_cmap_div,
-    levels=vptp_levels_div,
+    levels=vptp_levels,
     extend="both",
     add_colorbar=False,
 )
@@ -191,7 +203,7 @@ vptp_neg_first.plot.contourf(
     ax=axes[1, 0],
     transform=ccrs.PlateCarree(),
     cmap=temp_cmap_div,
-    levels=vptp_levels_div,
+    levels=vptp_levels,
     extend="both",
     add_colorbar=False,
 )
@@ -199,7 +211,7 @@ vptp_neg_map = vptp_neg_last.plot.contourf(
     ax=axes[1, 1],
     transform=ccrs.PlateCarree(),
     cmap=temp_cmap_div,
-    levels=vptp_levels_div,
+    levels=vptp_levels,
     extend="both",
     add_colorbar=False,
 )
@@ -271,7 +283,7 @@ qflux_pos_last.isel(lon=slice(None, None, 3), lat=slice(None, None, 3)).plot.qui
     v="v",
     ax=axes[0, 2],
     transform=ccrs.PlateCarree(),
-    scale=scale,
+    scale=scale_div,
     color="black",
 )
 qflux_neg_first.isel(lon=slice(None, None, 3), lat=slice(None, None, 3)).plot.quiver(
@@ -304,7 +316,7 @@ qflux_neg_last.isel(lon=slice(None, None, 3), lat=slice(None, None, 3)).plot.qui
     v="v",
     ax=axes[1, 2],
     transform=ccrs.PlateCarree(),
-    scale=scale,
+    scale=scale_div,
     color="black",
 )
 qflux_phase_map = (qflux_pos_first - qflux_neg_first).isel(
@@ -316,7 +328,7 @@ qflux_phase_map = (qflux_pos_first - qflux_neg_first).isel(
     v="v",
     ax=axes[2, 0],
     transform=ccrs.PlateCarree(),
-    scale=scale,
+    scale=scale_div,
     color="black",
 )
 qflux_phase_map = (qflux_pos_last - qflux_neg_last).isel(
@@ -328,7 +340,7 @@ qflux_phase_map = (qflux_pos_last - qflux_neg_last).isel(
     v="v",
     ax=axes[2, 1],
     transform=ccrs.PlateCarree(),
-    scale=scale,
+    scale=scale_div,
     color="black",
 )
 (qflux_pos_last - qflux_pos_first - (qflux_neg_last - qflux_neg_first)).isel(
@@ -340,7 +352,7 @@ qflux_phase_map = (qflux_pos_last - qflux_neg_last).isel(
     v="v",
     ax=axes[2, 2],
     transform=ccrs.PlateCarree(),
-    scale=scale,
+    scale=scale_div,
     color="black",
 )
 
@@ -388,5 +400,236 @@ for i, ax in enumerate(axes.flatten()):
 
 plt.tight_layout(w_pad=-9, h_pad=1)
 
+
+# %%
+upqp_first_clim = xr.open_dataset("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/upqp_monthly_ensmean/upqp_monmean_ensmean_185005_185909.nc")
+upqp_last_clim = xr.open_dataset("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/upqp_monthly_ensmean/upqp_monmean_ensmean_209005_209909.nc")
+# %%
+vpqp_first_clim = xr.open_dataset("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/vpqp_monthly_ensmean/vpqp_monmean_ensmean_185005_185909.nc")
+vpqp_last_clim = xr.open_dataset("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/vpqp_monthly_ensmean/vpqp_monmean_ensmean_209005_209909.nc")
+# %%
+upqp_first_clim = upqp_first_clim["upqp"].mean(dim = "time")
+upqp_last_clim = upqp_last_clim["upqp"].mean(dim = "time")
+vpqp_first_clim = vpqp_first_clim["vptp"].mean(dim = "time") # name not updated
+vpqp_last_clim = vpqp_last_clim["vptp"].mean(dim = "time")
+# %%
+# integrate
+upqp_first_clim = vert_integrate(upqp_first_clim)
+upqp_last_clim = vert_integrate(upqp_last_clim)
+vpqp_first_clim = vert_integrate(vpqp_first_clim)
+vpqp_last_clim = vert_integrate(vpqp_last_clim)
+# %%
+# to flux
+qflux_first_clim = xr.Dataset(
+    {
+        "u": upqp_first_clim * 1e3,
+        "v": vpqp_first_clim * 1e3,
+    }
+)
+
+qflux_last_clim = xr.Dataset(
+    {
+        "u": upqp_last_clim * 1e3,
+        "v": vpqp_last_clim * 1e3,
+    }
+)
+#%%
+vptp_first_clim = xr.open_dataset("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/vptp_monthly_ensmean/vptp_monmean_ensmean_185005_185909.nc")
+vptp_last_clim = xr.open_dataset("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/vptp_monthly_ensmean/vptp_monmean_ensmean_209005_209909.nc")
+#%%
+vptp_first_clim = vptp_first_clim["vptp"].sel(plev=85000).mean(dim = "time")
+vptp_last_clim = vptp_last_clim["vptp"].sel(plev=85000).mean(dim = "time")
+# %%
+fig, axes = plt.subplots(
+    2, 3, figsize=(12, 10), subplot_kw={"projection": ccrs.Orthographic(-90, 80)}
+)
+
+vptp_first_clim.plot.contourf(
+    ax=axes[0, 0],
+    transform=ccrs.PlateCarree(),
+    cmap=temp_cmap_div,
+    levels=vptp_levels,
+    extend="both",
+    add_colorbar=True,
+    cbar_kwargs={
+        "label": r"$v'\theta '\, / \, K \, m \, s^{-1}$",
+        "shrink": 0.8,
+        "orientation": "horizontal",}
+)
+
+vptp_map = vptp_last_clim.plot.contourf(
+    ax=axes[0, 1],
+    transform=ccrs.PlateCarree(),
+    cmap=temp_cmap_div,
+    levels=vptp_levels,
+    extend="both",
+    add_colorbar=True,cbar_kwargs={
+        "label": r"$v'\theta '\, / \, K \, m \, s^{-1}$",
+        "shrink": 0.8,
+        "orientation": "horizontal",}
+,
+)
+
+# diff
+vptp_diff_map = (vptp_last_clim - vptp_first_clim).plot.contourf(
+    ax=axes[0, 2],
+    transform=ccrs.PlateCarree(),
+    cmap=temp_cmap_div,
+    levels=vptp_levels_div,
+    extend="both",
+    add_colorbar=True,cbar_kwargs={
+        "label": r"$v'\theta '\, / \, K \, m \, s^{-1}$",
+        "shrink": 0.8,
+        "orientation": "horizontal",}
+,
+)
+
+# quiver
+ens_quiver = qflux_first_clim.isel(lon=slice(None, None, 3), lat=slice(None, None, 3)).plot.quiver(
+    x="lon",
+    y="lat",
+    u="u",
+    v="v",
+    ax=axes[0, 0],
+    transform=ccrs.PlateCarree(),
+    scale=scale,
+    color="black",
+)
+qflux_last_clim.isel(lon=slice(None, None, 3), lat=slice(None, None, 3)).plot.quiver(
+    x="lon",
+    y="lat",
+    u="u",
+    v="v",
+    ax=axes[0, 1],
+    transform=ccrs.PlateCarree(),
+    scale=scale,
+    color="black",
+)
+(qflux_last_clim - qflux_first_clim).isel(
+    lon=slice(None, None, 3), lat=slice(None, None, 3)
+).plot.quiver(
+    x="lon",
+    y="lat",
+    u="u",
+    v="v",
+    ax=axes[0, 2],
+    transform=ccrs.PlateCarree(),
+    scale=scale_div,
+    color="black",
+)
+
+
+# pos neg diff
+vptp_phase_map = (vptp_pos_first - vptp_neg_first).plot.contourf(
+    ax=axes[1, 0],
+    transform=ccrs.PlateCarree(),
+    cmap=temp_cmap_div,
+    levels=vptp_levels_div,
+    extend="both",
+    add_colorbar=True,cbar_kwargs={
+        "label": r"$v'\theta '\, / \, K \, m \, s^{-1}$",
+        "shrink": 0.8,
+        "orientation": "horizontal",}
+,
+)
+vptp_phase_map = (vptp_pos_last - vptp_neg_last).plot.contourf(
+    ax=axes[1, 1],
+    transform=ccrs.PlateCarree(),
+    cmap=temp_cmap_div,
+    levels=vptp_levels_div,
+    extend="both",
+    add_colorbar=True,cbar_kwargs={
+        "label": r"$v'\theta '\, / \, K \, m \, s^{-1}$",
+        "shrink": 0.8,
+        "orientation": "horizontal",}
+,
+)
+# diff
+((vptp_pos_last - vptp_neg_last) - (vptp_pos_first - vptp_neg_first)).plot.contourf(
+    ax=axes[1, 2],
+    transform=ccrs.PlateCarree(),
+    cmap=temp_cmap_div,
+    levels=vptp_levels_div,
+    extend="both",
+    add_colorbar=True,cbar_kwargs={
+        "label": r"$v'\theta '\, / \, K \, m \, s^{-1}$",
+        "shrink": 0.8,
+        "orientation": "horizontal",}
+,
+)
+# quiver
+qflux_phase_map = (qflux_pos_first - qflux_neg_first).isel(
+    lon=slice(None, None, 3), lat=slice(None, None, 3)
+).plot.quiver(
+    x="lon",
+    y="lat",
+    u="u",
+    v="v",
+    ax=axes[1, 0],
+    transform=ccrs.PlateCarree(),
+    scale=scale_div,
+    color="black",
+)
+qflux_phase_map = (qflux_pos_last - qflux_neg_last).isel(
+    lon=slice(None, None, 3), lat=slice(None, None, 3)
+).plot.quiver(
+    x="lon",
+    y="lat",
+    u="u",
+    v="v",
+    ax=axes[1, 1],
+    transform=ccrs.PlateCarree(),
+    scale=scale_div,
+    color="black",
+)
+(qflux_pos_last - qflux_pos_first - (qflux_neg_last - qflux_neg_first)).isel(
+    lon=slice(None, None, 3), lat=slice(None, None, 3)
+).plot.quiver(
+    x="lon",
+    y="lat",
+    u="u",
+    v="v",
+    ax=axes[1, 2],
+    transform=ccrs.PlateCarree(),
+    scale=scale_div,
+    color="black",
+)
+
+# add legend for the quiver
+axes[0, 0].quiverkey(
+    ens_quiver,
+    0.9,
+    0.9,
+    scale,
+    label="5 m/s",
+    labelpos="E",
+    coordinates="figure",
+    color="black",
+)
+
+axes[1, 0].quiverkey(
+    qflux_phase_map,
+    0.9,
+    0.9,
+    scale_div,
+    label="0.5 m/s",
+    labelpos="E",
+    coordinates="figure",
+    color="black",
+)
+
+
+
+for ax in axes.flatten():
+    ax.coastlines()
+    # add gidlines at lat every 20 degree, and lon every 60 degree
+    ax.gridlines(draw_labels=False, linewidth=0.5, linestyle="dotted")
+    ax.set_global()
+
+    ax.set_xlabel("")
+    ax.set_ylabel("")
+    ax.set_title("")
+
+# plt.savefig("/work/mh0033/m300883/High_frequecy_flow/docs/plots/eddy_flux/eddy_flux_before_NAO_pos_or_neg.png", dpi=300, bbox_inches="tight")
 
 # %%
