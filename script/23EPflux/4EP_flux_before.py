af9689205276b8969c1805037c4a0144a86cc267
Quan <quan.liu@mpimet.mpg.de>
2025-04-08 18:10:51 +0200

    get the plot

--- script/23EPflux/4EP_flux_before.py
+++ script/23EPflux/4EP_flux_before.py
@@ -55,18 +55,77 @@ theta_first_ensmean = theta_first_ensmean.theta
 theta_last_ensmean = theta_last_ensmean.theta
 # %%
 F_phi_pos_first, F_p_pos_first, div1_pos_first, div2_pos_first = EP_flux(vptp_pos_first, upvp_pos_first, theta_first_ensmean)
+F_phi_neg_first, F_p_neg_first, div1_neg_first, div2_neg_first = EP_flux(vptp_neg_first, upvp_neg_first, theta_first_ensmean)
 
 #%%
-F_phi_pos_first['plev'] = F_phi_pos_first['plev'] / 100
-F_p_pos_first['plev'] = F_p_pos_first['plev'] / 100
-div1_pos_first['plev'] = div1_pos_first['plev'] / 100
-div2_pos_first['plev'] = div2_pos_first['plev'] / 100
+div1 = div1_pos_first
+div2 = div2_pos_first
+Div = div1 + div2
+
+div1_neg = div1_neg_first
+div2_neg = div2_neg_first
+Div_neg = div1_neg + div2_neg
 # %%
-fig, axes = plt.subplots(2,2, figsize = (12, 8))
+fig, axes = plt.subplots(2, 2, figsize=(12, 8))
 lat = F_phi_pos_first.lat
 plev = F_phi_pos_first.plev
-PlotEPfluxArrows(x = lat, y = plev,
-                 ep1=F_phi_pos_first.mean(dim = 'lon'),
-                    ep2=F_p_pos_first.mean(dim = 'lon'),
-                    fig=fig, ax=axes[0,0],)
+
+# Positive case
+Div.mean(dim='lon').plot.contourf(ax=axes[0, 0], levels=np.arange(-2, 2.1, 0.1), cmap='RdBu_r', add_colorbar=False)
+PlotEPfluxArrows(
+    x=lat[::3], y=plev,
+    ep1=F_phi_pos_first.mean(dim='lon')[:, ::3],
+    ep2=F_p_pos_first.mean(dim='lon')[:, ::3],
+    xscale='linear',
+    yscale='linear',
+    scale=5e15,
+    xlim=[0, 90],
+    fig=fig, ax=axes[0, 0],
+)
+
+# Negative case
+Div_neg.mean(dim='lon').plot.contourf(ax=axes[0, 1], levels=np.arange(-2, 2.1, 0.1), cmap='RdBu_r', add_colorbar=False)
+PlotEPfluxArrows(
+    x=lat[::3], y=plev,
+    ep1=F_phi_neg_first.mean(dim='lon')[:, ::3],
+    ep2=F_p_neg_first.mean(dim='lon')[:, ::3],
+    xscale='linear',
+    yscale='linear',
+    scale=5e15,
+    xlim=[0, 90],
+    fig=fig, ax=axes[0, 1],
+)
+
+# Difference between positive and negative
+Div_diff = Div - Div_neg
+Div_diff.mean(dim='lon').plot.contourf(ax=axes[1, 0], levels=np.arange(-0.5, 0.6, 0.05), cmap='RdBu_r', add_colorbar=False)
+PlotEPfluxArrows(
+    x=lat[::3], y=plev,
+    ep1=(F_phi_pos_first - F_phi_neg_first).mean(dim='lon')[:, ::3],
+    ep2=(F_p_pos_first - F_p_neg_first).mean(dim='lon')[:, ::3],
+    xscale='linear',
+    yscale='linear',
+    scale=1e14,
+    xlim=[0, 90],
+    fig=fig, ax=axes[1, 0],
+)
+# %%
+F_phi = F_phi_pos_first
+F_p = F_p_pos_first
+
+#%%
+F = xr.Dataset({'F_phi': F_phi, 'F_p': F_p})
+F = F.mean(dim = 'lon')
+#%%
+
+#%%
+fig, ax = plt.subplots(figsize = (12, 8))
+
+ax.quiver(
+    F.lat,
+    F.plev,
+    F.F_phi,
+    F.F_p,
+)
+ax.set_ylim(1000, 200)
 # %%
