5606cbc2a3305d9e9132dc21980aa3cd24e150e9
Quan <quan.liu@mpimet.mpg.de>
2025-04-08 17:03:59 +0200

    add plot func

--- src/EP_flux/EP_flux.py
+++ src/EP_flux/EP_flux.py
@@ -102,3 +102,123 @@ def EP_flux(vptp, upvp, Th_bar):
 	div2.name = 'div2'
 
 	return ep1_cart, ep2_cart, div1, div2
+
+## helper function: Get actual width and height of axes
+def GetAxSize(fig,ax,dpi=False):
+	"""get width and height of a given axis.
+	   output is in inches if dpi=False, in dpi if dpi=True
+	"""
+	bbox = ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+	width, height = bbox.width, bbox.height
+	if dpi:
+		width *= fig.dpi
+		height *= fig.dpi
+	return width, height
+#%%
+def PlotEPfluxArrows(x,y,ep1,ep2,fig,ax,xlim=None,ylim=None,xscale='linear',yscale='linear',invert_y=True, newax=False, pivot='tail',scale=None,quiv_args=None):
+	"""Correctly scales the Eliassen-Palm flux vectors for plotting on a latitude-pressure or latitude-height axis.
+		x,y,ep1,ep2 assumed to be xarray.DataArrays.
+
+	INPUTS:
+		x	: horizontal coordinate, assumed in degrees (latitude) [degrees]
+		y	: vertical coordinate, any units, but usually this is pressure or height
+		ep1	: horizontal Eliassen-Palm flux component, in [m2/s2]. Typically, this is ep1_cart from
+				   ComputeEPfluxDiv()
+		ep2	: vertical Eliassen-Palm flux component, in [U.m/s2], where U is the unit of y.
+				   Typically, this is ep2_cart from ComputeEPfluxDiv(), in [hPa.m/s2] and y is pressure [hPa].
+		fig	: a matplotlib figure object. This figure contains the axes ax.
+		ax	: a matplotlib axes object. This is where the arrows will be plotted onto.
+		xlim	: axes limits in x-direction. If None, use [min(x),max(x)]. [None]
+		ylim	: axes limits in y-direction. If None, use [min(y),max(y)]. [None]
+		xscale	: x-axis scaling. currently only 'linear' is supported. ['linear']
+		yscale	: y-axis scaling. 'linear' or 'log' ['linear']
+		invert_y: invert y-axis (for pressure coordinates). [True]
+		newax	: plot on second y-axis. [False]
+		pivot	: keyword argument for quiver() ['tail']
+		scale	: keyword argument for quiver(). Smaller is longer [None]
+				  besides fixing the length, it is also usefull when calling this function inside a
+				   script without display as the only way to have a quiverkey on the plot.
+               quiv_args: further arguments passed to quiver plot.
+
+	OUTPUTS:
+	   Fphi*dx : x-component of properly scaled arrows. Units of [m3.inches]
+	   Fp*dy   : y-component of properly scaled arrows. Units of [m3.inches]
+	   ax	: secondary y-axis if newax == True
+	"""
+	import numpy as np
+	import matplotlib.pyplot as plt
+	#
+	def Deltas(z,zlim):
+		# if zlim is None:
+		return np.max(z)-np.min(z)
+		# else:
+			# return zlim[1]-zlim[0]
+	# Scale EP vector components as in Edmon, Hoskins & McIntyre JAS 1980:
+	cosphi = np.cos(np.deg2rad(x))
+	a0 = 6376000.0 # Earth radius [m]
+	grav = 9.81
+	# first scaling: Edmon et al (1980), Eqs. 3.1 & 3.13
+	Fphi = 2*np.pi/grav*cosphi**2*a0**2*ep1 # [m3.rad]
+	Fp   = 2*np.pi/grav*cosphi**2*a0**3*ep2 # [m3.hPa]
+	#
+	# Now comes what Edmon et al call "distances occupied by 1 radian of
+	#  latitude and 1 [hecto]pascal of pressure on the diagram."
+	# These distances depend on figure aspect ratio and axis scale
+	#
+	# first, get the axis width and height for
+	#  correct aspect ratio
+	width,height = GetAxSize(fig,ax)
+	# we use min(),max(), but note that if the actual axis limits
+	#  are different, this will be slightly wrong.
+	delta_x = Deltas(x,xlim)
+	delta_y = Deltas(y,ylim)
+	#
+	#scale the x-axis:
+	if xscale == 'linear':
+		dx = width/delta_x/np.pi*180
+	else:
+		raise ValueError('ONLY LINEAR X-AXIS IS SUPPORTED AT THE MOMENT')
+	#scale the y-axis:
+	if invert_y:
+		y_sign = -1
+	else:
+		y_sign = 1
+	if yscale == 'linear':
+		dy = y_sign*height/delta_y
+	elif yscale == 'log':
+		dy = y_sign*height/y/np.log(np.max(y)/np.min(y))
+	#
+	# plot the arrows onto axis
+	quivArgs = {'angles':'uv','scale_units':'inches','pivot':pivot}
+	if quiv_args is not None:
+		for key in quiv_args.keys():
+			quivArgs[key] = quiv_args[key]
+	if scale is not None:
+		quivArgs['scale'] = scale
+	if newax:
+		ax = ax.twinx()
+		ax.set_ylabel('pressure [hPa]')
+	try:
+		Q = ax.quiver(x,y,Fphi*dx,Fp*dy,**quivArgs)
+	except:
+		Q = ax.quiver(x,y,dx*Fphi.transpose(),dy*Fp.transpose(),**quivArgs)
+	if scale is None:
+		fig.canvas.draw() # need to update the plot to get the Q.scale
+		U = Q.scale
+	else:
+		U = scale
+	if U is not None: # when running inside a script, the figure might not exist and therefore U is None
+		ax.quiverkey(Q,0.9,1.02,U/width,label=r'{0:.1e}$\,m^3$'.format(U),labelpos='E',coordinates='axes')
+	if invert_y:
+		ax.invert_yaxis()
+	if xlim is not None:
+		ax.set_xlim(xlim)
+	if ylim is not None:
+		ax.set_ylim(ylim)
+	ax.set_yscale(yscale)
+	ax.set_xscale(xscale)
+	#
+	if newax:
+		return Fphi*dx,Fp*dy,ax
+	else:
+		return Fphi*dx,Fp*dy
