8dc9f4a0eb151dc4f1361f4b242146a30265e915
Quan <quan.liu@mpimet.mpg.de>
2025-04-11 16:48:55 +0200

    ano

--- script/23EPflux/4EP_flux_isentropes_submit.sh
+++ script/23EPflux/4EP_flux_isentropes_submit.sh
@@ -12,7 +12,8 @@
 phase=$1 # phase
 decade=$2 # decade
 isentrope=$3 # isentrope
+ano=$4 # anomaly
 
 echo run $phase for $decade
 
-python 4EP_flux_isentropes.py $phase $decade $isentrope
+python 4EP_flux_isentropes.py $phase $decade $isentrope $ano
