b83d33bd9a94478508995aeee20c35daf72bb031
Quan <quan.liu@mpimet.mpg.de>
2025-05-06 18:17:37 +0200

    steady eddy master

---
+++ script/pre_process/steady/0steady_eddy_master.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+var=$1
+
+
+#for loop 1-50
+for ens in {1..50}
+do
+    echo "Ensemble member ${ens}"
+    # run the python script
+    sbatch 0steady_eddy.sh ${ens} ${var}
+    # ./1upvp.sh ${ens}
+done
