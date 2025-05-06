69689077a5c0fb3a0c5ecb11b399fcf7b4716970
Quan <quan.liu@mpimet.mpg.de>
2025-05-06 18:14:47 +0200

    steady eddies

---
+++ script/pre_process/steady/zonal_ano.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#SBATCH --job-name=prime
+#SBATCH --time=01:30:00
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks=5
+#SBATCH --mem=200G
+#SBATCH --mail-type=FAIL
+#SBATCH --account=mh0033
+#SBATCH --output=prime.%j.out
+
+module load cdo/2.5.0-gcc-11.2.0
+module load parallel
+
+node=$1
+member=$node
+var=$2
+echo "Ensemble member ${member} of variable ${var}"
+
+
+T_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/${var}_hat_daily/r${member}i1p1f1/
+
+
+Ts_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/${var}_steady_daily/r${member}i1p1f1/
+
+
+tmp_dir=/scratch/m/m300883/${var}_steady/r${member}i1p1f1/
+
+mkdir -p ${Ts_path} ${tmp_dir}
+
+
+export T_path Ts_path member tmp_dir var
+
+
+# function to band filter
+sub_zonmean(){
+    infile=$1
+    outfile=$2  
+    # basename without .nc
+    fname=$(basename ${infile%.nc})
+
+    # zonal anomaly
+    cdo -O sub ${infile} -enlarge,${infile} -zonmean ${infile} ${outfile}
+}
+
+steady_eddy(){
+    dec=$1
+    Tfile=$(find ${T_path} -name "*${dec}*.nc")
+    Tfile=$(echo $Tfile | tr -d '\n')
+    
+    Tfile_name=$(basename "${Tfile}")
+    Tsfile_name="${Ts_path}${Tfile_name//${var}/${var}_steady}"
+
+    echo "Input file: ${Tfile}"
+    echo "Output file: ${Tsfile_name}"
+    sub_zonmean ${Tfile} ${Tsfile_name}
+}
+
+export -f sub_zonmean
+export -f steady_eddy
+
+parallel -j 5 steady_eddy ::: {1850..2090..10}
+
+# check completion
+for dec in {1850..2090..10}; do
+    if [ ! -f ${Ts_path}/*${dec}*.nc ]; then
+        echo "Decade ${dec} is missing"
+        echo "Recalculate decade ${dec}"
+        steady_eddy ${dec}
+    fi
+done
