205490682134953c4a1411654eb6ef378da3eef1
Quan <quan.liu@mpimet.mpg.de>
2025-05-07 12:09:55 +0200

    with window

--- script/23EPflux/3prime_before_NAO_15_5_mean.py
+++ script/23EPflux/3prime_before_NAO_15_5_mean.py
@@ -39,8 +39,16 @@ def process_data(decade, var, integrate=False, **kwargs):
 
     # select data before NAO events, here 'var' is only for column name
     logging.info (f"selecting data for {decade} \n")
-    ivke_NAO_pos = before_NAO_mean(NAO_pos, data, (-15, -5))
-    ivke_NAO_neg = before_NAO_mean(NAO_neg, data, (-15, -5))
+
+    # determin the time window
+    if kwargs.get('window', '15_5') == '15_5':
+        time_window = (-15, -5)
+    elif kwargs.get('window', '5_0') == '5_0':
+        time_window = (-5, 0)
+    else:
+        raise ValueError("window must be either '15_5' or '5_0'")
+    ivke_NAO_pos = before_NAO_mean(NAO_pos, data, time_window)
+    ivke_NAO_neg = before_NAO_mean(NAO_neg, data, time_window)
 
     if integrate:
         ivke_NAO_pos = vert_integrate(ivke_NAO_pos)
@@ -66,7 +74,8 @@ if __name__ == "__main__":
     decade = int(sys.argv[1]) if len(sys.argv) > 1 else 1850
     var = str(sys.argv[2]) if len(sys.argv) > 2 else 'vptp'
     name = str(sys.argv[3]) if len(sys.argv) > 3 else 'vptp'
-    suffix = str(sys.argv[4]) if len(sys.argv) > 4 else '' # '_ano' or ''
+    window= str(sys.argv[4]) if len(sys.argv) > 4 else '15_5' # '15_5' or '5_0'
+    suffix = str(sys.argv[5]) if len(sys.argv) > 4 else '' # '_ano' or ''
     integrate = False
 
     logging.info(f"processing decade {decade} of {var} \n")
