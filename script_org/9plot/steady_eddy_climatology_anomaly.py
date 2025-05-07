82eacd80c226292001d0e6198559d46f634a9ec3
Quan <quan.liu@mpimet.mpg.de>
2025-05-07 15:17:18 +0200

    read data

--- script_org/9plot/steady_eddy_climatology_anomaly.py
+++ script_org/9plot/steady_eddy_climatology_anomaly.py
@@ -47,6 +47,8 @@ def read_climatology(var, decade, **kwargs):
 def read_composite_ano(var, decade, phase, **kwargs):
 
     name = kwargs.get("name", var)  # default name is the same as var
+    before = kwargs.get("before", "5_0")  # default is 5-0 days before
+    # u hat is in different path
     if var == 'uhat':
         composite_path = "/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/NA_jet_stream/composite/"
         if decade == 1850:
@@ -62,34 +64,19 @@ def read_composite_ano(var, decade, phase, **kwargs):
 
         composite_ano = xr.open_dataarray(composite_path)
 
-    # 5-0 days before
-    elif var == "upvp":
-        upvp = read_composite_MPI("upvp", name=name, decade = decade, before = "5_0", return_as = phase)
-        try:
-            composite_ano = upvp.sel(plev = 25000)
-        except KeyError:
-            composite_ano = upvp
-    elif var == "usvs":
-        usvs = read_composite_MPI("usvs", name=name, decade = decade, before = "5_0", return_as = phase)
-        try:
-            composite_ano = usvs.sel(plev = 25000)
-        except KeyError:
-            composite_ano = usvs
-    
-    # -15 - 5 days before
-    elif var == "vpetp":
-        vpetp = read_composite_MPI("vpetp", "vpetp",decade = decade, before = "15_5", return_as = phase)
-        composite_ano = vpetp.sel(plev = 85000)
-    elif var == "vptp":
-        vptp = read_composite_MPI("vptp", "vptp", decade = decade, before = "15_5", return_as = phase)
-        composite_ano = vptp.sel(plev = 85000)
-
-    elif var == "vsets":
-        vsets = read_composite_MPI("vsets", "vsets", decade = decade, before = "15_5", return_as = phase)
-        composite_ano = vsets.sel(plev = 85000)
-    elif var == "vsts":
-        vsts = read_composite_MPI("vsts", "vsts", decade = decade, before = "15_5", return_as = phase)
-        composite_ano = vsts.sel(plev = 85000)
+    # 5-0 days 
+    else:
+        composite_ano = read_composite_MPI(var, name, decade = decade, before = before, return_as = phase, ano = True)
+        if var == "upvp" or var == "usvs": # upper level
+            try:
+                composite_ano = composite_ano.sel(plev = 25000)
+            except KeyError:
+                pass
+        elif var in ["vpetp", "vptp", "vpqp", "vsets", "vsts"]: # lower level
+            try:
+                composite_ano = composite_ano.sel(plev = 85000)
+            except KeyError:
+                pass
 
     return erase_white_line(composite_ano)
 
@@ -121,6 +108,9 @@ vpetp_last = read_climatology("vpetp", 2090, name="vpetp")
 vpetp_first = vpetp_first.sel(plev = 85000)
 vpetp_last = vpetp_last.sel(plev = 85000)
 #%%
+vsets_first = read_climatology("vsets", 1850, name="vsets")
+vsets_last = read_climatology("vsets", 2090, name="vsets")
+#%%
 ###### pos anomaly ######
 uhat_first_pos = read_composite_ano("uhat", 1850, "pos", name="ua")
 uhat_last_pos = read_composite_ano("uhat", 2090, "pos", name="ua")
@@ -130,6 +120,14 @@ upvp_last_pos = read_composite_ano("upvp", 2090, "pos", name="ua")
 
 vpetp_first_pos = read_composite_ano("vpetp", 1850, "pos", name="vpetp")
 vpetp_last_pos = read_composite_ano("vpetp", 2090, "pos", name="vpetp")
+
+usvs_first_pos = read_composite_ano("usvs", 1850, "pos", name="usvs")
+usvs_last_pos = read_composite_ano("usvs", 2090, "pos", name="usvs")
+
+vsets_first_pos = read_composite_ano("vsets", 1850, "pos", name="vsets")
+vsets_last_pos = read_composite_ano("vsets", 2090, "pos", name="vsets")
+#%%
+
 ####### neg anomaly ######
 uhat_first_neg = read_composite_ano("uhat", 1850, "neg", name="ua")
 uhat_last_neg = read_composite_ano("uhat", 2090, "neg", name="ua")
