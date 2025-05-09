f74b5405339bdd15ecd367116b6f8c68d2416f2d
Quan <quan.liu@mpimet.mpg.de>
2025-05-09 12:07:07 +0200

    steady eddies

--- src/EP_flux/EP_flux.py
+++ src/EP_flux/EP_flux.py
@@ -230,19 +230,27 @@ def read_data_all(decade, phase, ano = False, before = '15_5', equiv_theta = Fal
 	"""
 	if eddy == 'transient':
 		upvp = read_composite_MPI("upvp", "ua", decade = decade, before = before, return_as=phase, ano=ano, smooth_value=None, remove_zonal=False)
+		if equiv_theta:
+			vptp = read_composite_MPI("vpetp", "vpetp", decade = decade, before = before, return_as=phase, ano=ano)
+			theta = read_composite_MPI("equiv_theta", "etheta", decade = decade, before = before, return_as=phase, ano=ano)
+
+		else:
+			vptp = read_composite_MPI("vptp", "vptp", decade = decade, before = before, return_as=phase, ano=ano)
+			theta = read_composite_MPI("theta", "theta", decade = decade, before = before, return_as=phase, ano=ano)			
+
+
 	elif eddy == 'steady':
 		upvp = read_composite_MPI("usvs", "usvs", decade = decade, before = before, return_as=phase, ano=ano, smooth_value=None, remove_zonal=False)
+		if equiv_theta:
+			vptp = read_composite_MPI("vsets", "vsets", decade = decade, before = before, return_as=phase, ano=ano)
+			theta = read_composite_MPI("equiv_theta", "etheta", decade = decade, before = before, return_as=phase, ano=ano)
+
+		else:
+			vptp = read_composite_MPI("vsts", "vsts", decade = decade, before = before, return_as=phase, ano=ano)
+			theta = read_composite_MPI("theta", "theta", decade = decade, before = before, return_as=phase, ano=ano)
 
 	else:
 		raise ValueError("eddy must be either 'transient' or 'steady'", f"but got {eddy}")
-	if equiv_theta:
-		vptp = read_composite_MPI("vpetp", "vpetp", decade = decade, before = before, return_as=phase, ano=ano)
-		theta = read_composite_MPI("equiv_theta", "etheta", decade = decade, before = before, return_as=phase, ano=ano)
-
-
-	else:
-		vptp = read_composite_MPI("vptp", "vptp", decade = decade, before = before, return_as=phase, ano=ano)
-		theta = read_composite_MPI("theta", "theta", decade = decade, before = before, return_as=phase, ano=ano)			
 
 
 	return upvp, vptp, theta
