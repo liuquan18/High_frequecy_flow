28202c2c01f72c18c0cf951e2b723d1b99abb636
Quan <quan.liu@mpimet.mpg.de>
2025-05-09 09:02:47 +0200

    use monmean

--- script_org/preprocess/eddies/1steady_eddy.sh
+++ script_org/preprocess/eddies/1steady_eddy.sh
@@ -20,6 +20,8 @@ echo "Ensemble member ${member} of variable ${var}"
 
 T_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/${var}_hat_daily/r${member}i1p1f1/
 
+T_mmean_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/${var}_monthly_ensmean/
+
 
 Ts_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/${var}_steady_daily/r${member}i1p1f1/
 
@@ -35,25 +37,28 @@ export T_path Ts_path member tmp_dir var
 # function to band filter
 sub_zonmean(){
     infile=$1
-    outfile=$2  
-    # basename without .nc
-    fname=$(basename ${infile%.nc})
+    mmean_file=$2
+    outfile=$3
+
 
     # zonal anomaly
-    cdo -O sub ${infile} -enlarge,${infile} -zonmean ${infile} ${outfile}
+    cdo -O ymonsub ${infile} -enlarge,${mmean_file} -zonmean ${mmean_file} ${outfile}
 }
 
 steady_eddy(){
     dec=$1
     Tfile=$(find ${T_path} -name "*${dec}*.nc")
     Tfile=$(echo $Tfile | tr -d '\n')
+
+    # monthly mean
+    Tmmean_file=$(find ${T_mmean_path} -name "*${dec}*.nc")
     
     Tfile_name=$(basename "${Tfile}")
-    Tsfile_name="${Ts_path}${Tfile_name//${var}/${var}_steady}"
+    Tsfile="${Ts_path}${Tfile_name//${var}/${var}_steady}"
 
     echo "Input file: ${Tfile}"
-    echo "Output file: ${Tsfile_name}"
-    sub_zonmean ${Tfile} ${Tsfile_name}
+    echo "Output file: ${Tsfile}"
+    sub_zonmean ${Tfile} ${Tmmean_file} ${Tsfile}
 }
 
 export -f sub_zonmean
