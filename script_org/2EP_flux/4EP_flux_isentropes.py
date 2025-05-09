687f9c1663f3be1794b551277fbf99f2c64af6dc
Quan <quan.liu@mpimet.mpg.de>
2025-05-09 11:12:47 +0200

    eddy input wrong

--- script_org/2EP_flux/4EP_flux_isentropes.py
+++ script_org/2EP_flux/4EP_flux_isentropes.py
@@ -89,7 +89,7 @@ def calculate_EP_flux(decade, phase, ano=False, equiv_theta=True, isentrope=True
 phase = sys.argv[1] # 'pos' or 'neg'
 decade = sys.argv[2] # '1850' or '2090'
 isentrope = sys.argv[3].lower() == 'true' # Convert string to boolean
-eddy = sys.argv[4] == 'transient' if len(sys.argv) > 4 else 'steady' # Convert string to boolean
+eddy = sys.argv[4]
 ano = sys.argv[5].lower() == 'true' if len(sys.argv) > 5 else False  # Convert string to boolean, default is False
 
 #%%
