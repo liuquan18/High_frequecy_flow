9e57440358e49c188e6031b559f46a922e335c94
Quan <quan.liu@mpimet.mpg.de>
2025-05-09 11:24:33 +0200

    get the plot

--- script_org/9plot/lon_pressure_eddy_heat.py
+++ script_org/9plot/lon_pressure_eddy_heat.py
@@ -56,11 +56,11 @@ vsets_first_low = vsets_first.sel(plev=85000)
 vsets_last_low = vsets_last.sel(plev=85000)
 #%%
 # meridional mean between 30-50N
-vpetp_first_profile = vpetp_first.sel(lat=slice(30, 50)).mean(dim="lat")
-vpetp_last_profile = vpetp_last.sel(lat=slice(30, 50)).mean(dim="lat")
+vpetp_first_profile = vpetp_first.sel(lat=slice(20, 50)).mean(dim="lat")
+vpetp_last_profile = vpetp_last.sel(lat=slice(20, 50)).mean(dim="lat")
 
-vsets_first_profile = vsets_first.sel(lat=slice(30, 50)).mean(dim="lat")
-vsets_last_profile = vsets_last.sel(lat=slice(30, 50)).mean(dim="lat")
+vsets_first_profile = vsets_first.sel(lat=slice(20, 50)).mean(dim="lat")
+vsets_last_profile = vsets_last.sel(lat=slice(20, 50)).mean(dim="lat")
 
 #%%
 # fake data to plot
@@ -101,12 +101,9 @@ usqs_last = vert_integrate(usqs_last)
 vsqs_first = vert_integrate(vsqs_first)
 vsqs_last = vert_integrate(vsqs_last)
 # to flux
-qflux_first = xr.Dataset({'u': usqs_first*1e3, 'v': vsqs_first*1e3}) #g/kg m/s
-qflux_last = xr.Dataset({'u': usqs_last*1e3, 'v': vsqs_last*1e3})
-#%%
+sflux_first = xr.Dataset({'u': usqs_first*1e3, 'v': vsqs_first*1e3}) #g/kg m/s
+sflux_last = xr.Dataset({'u': usqs_last*1e3, 'v': vsqs_last*1e3})
 
-
-# %%
 # %%
 temp_cmap_seq = np.loadtxt(
     "/work/mh0033/m300883/High_frequecy_flow/data/colormaps-master/continuous_colormaps_rgb_0-1/temp_seq.txt"
@@ -135,6 +132,7 @@ vsts_levels_div = np.arange(-3, 3.1, 0.5)
 vptp_levels_div = np.arange(-1.2, 1.3, 0.2)
 
 scale_div = 1
+scale_steady_div = 5
 #%%
 fig = plt.figure(figsize=(10, 10))
 grid = plt.GridSpec(
@@ -197,6 +195,70 @@ vpetp_last_plot.plot.contourf(
 
 ###### maps
 ### steady eddies
+vsets_first_low.plot.contourf(
+    ax=map_ax_steady_first,
+    transform=ccrs.PlateCarree(),
+    cmap=temp_cmap_div,
+    levels=vsts_levels_div,
+    add_colorbar=False,
+    extend="both",
+    add_label=False,
+)
+# quiver
+steady_first_flux_arrow = map_ax_steady_first.quiver(
+    sflux_first["lon"].values[::4],
+    sflux_first["lat"].values[::3],
+    sflux_first["u"].values[::3, ::4],
+    sflux_first["v"].values[::3, ::4],
+    transform=ccrs.PlateCarree(),
+    scale=scale_steady_div,
+    color="black",
+    pivot="middle",
+)
+# last
+vsets_last_low.plot.contourf(
+    ax=map_ax_steady_last,
+    transform=ccrs.PlateCarree(),
+    cmap=temp_cmap_div,
+    levels=vsts_levels_div,
+    add_colorbar=True,      
+    extend="both",
+    cbar_kwargs={
+        "label": r"$ v' \theta'_e/ K m s^{-1}$",
+        "orientation": "horizontal",
+        "pad": 0.2,
+        "shrink": 0.8,
+        "aspect": 20,
+    },
+    add_label=False,
+)
+# quiver
+steady_last_flux_arrow = map_ax_steady_last.quiver(
+    sflux_last["lon"].values[::4],
+    sflux_last["lat"].values[::3],
+    sflux_last["u"].values[::3, ::4],
+    sflux_last["v"].values[::3, ::4],
+    transform=ccrs.PlateCarree(),
+    scale=scale_steady_div,
+    color="black",
+    pivot="middle",
+)
+
+# add quiver key
+quiver_key = map_ax_steady_last.quiverkey(
+    steady_last_flux_arrow,
+    0.65,
+    -2.5,
+    0.1,
+    r"$0.05 g kg^{-1} m s^{-1}$",
+    transform=ccrs.PlateCarree(),
+    labelpos="E",
+    coordinates="axes",
+    fontproperties={"size": 12},
+)
+steady_last_flux_arrow.set_color("yellow")
+steady_first_flux_arrow.set_color("yellow")
+
 
 
 ### transient eddies
@@ -213,9 +275,9 @@ vpetp_first_low.plot.contourf(
 # quiver
 transient_first_flux_arrow = map_ax_transient_first.quiver(
     qflux_first["lon"].values[::4],
-    qflux_first["lat"].values[::4],
-    qflux_first["u"].values[::4, ::4],
-    qflux_first["v"].values[::4, ::4],
+    qflux_first["lat"].values[::3],
+    qflux_first["u"].values[::3, ::4],
+    qflux_first["v"].values[::3, ::4],
     transform=ccrs.PlateCarree(),
     scale=scale_div,
     color="black",
@@ -255,8 +317,8 @@ transient_last_flux_arrow = map_ax_transient_last.quiver(
 quiver_key = map_ax_transient_last.quiverkey(
     transient_last_flux_arrow,
     0.65,
-    -2,
-    0.05,
+    -2.5,
+    0.5,
     r"$0.05 g kg^{-1} m s^{-1}$",
     transform=ccrs.PlateCarree(),
     labelpos="E",
@@ -266,6 +328,7 @@ quiver_key = map_ax_transient_last.quiverkey(
 
 
 transient_last_flux_arrow.set_color("yellow")
+transient_first_flux_arrow.set_color("yellow")
 
 
 
@@ -307,8 +370,12 @@ for ax in [map_ax_steady_first, map_ax_transient_first,
     # continents light gray
     # add gidlines at lat every 20 degree, and lon every 60 degree
     ax.set_xlim(-180, 180)  # 
-    ax.set_ylim(20, 60)  # 
+    ax.set_ylim(10, 70)  # 
     ax.set_title("")
+
+    # hline at y = 30 and y = 50
+    ax.axhline(20, color="gray", linewidth=0.5, linestyle="--")
+    ax.axhline(50, color="gray", linewidth=0.5, linestyle="--")
 
 for ax in [map_ax_steady_last, map_ax_transient_last]:
     gl = ax.gridlines(
@@ -326,10 +393,15 @@ for ax in [map_ax_steady_last, map_ax_transient_last]:
 
     gl.ylabels_right = False
     gl.ylines = False
-    gl.ylocator = mticker.FixedLocator([30, 50])
+    gl.ylocator = mticker.FixedLocator([20, 50])
     gl.yformatter = LATITUDE_FORMATTER
 
-
+plt.savefig(
+    "/work/mh0033/m300883/High_frequecy_flow/docs/plots/eddy_flux/lon_pressure_eddy_heat.pdf",
+    bbox_inches="tight",
+    dpi=300,
+    transparent=True,
+)
 
 
 # %%
