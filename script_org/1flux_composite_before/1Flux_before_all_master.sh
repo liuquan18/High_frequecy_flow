f1ef48620bc0702aba620ee7e35b8ca8b10ac55d
Quan <quan.liu@mpimet.mpg.de>
2025-05-19 11:30:58 +0200

    for all

--- script_org/1flux_composite_before/1Flux_before_all_master.sh
+++ script_org/1flux_composite_before/1Flux_before_all_master.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
 window=$1
 
+suffix=$2
+
 
 # ua hat
-echo "ua hat before $window"
-sbatch 1Flux_before_NAO_submit.sh 1850 ua_hat ua $window
-sbatch 1Flux_before_NAO_submit.sh 2090 ua_hat ua $window
+echo "ua hat before $window $suffix"
+sbatch 1Flux_before_NAO_submit.sh 1850 ua_hat ua $window $suffix
+sbatch 1Flux_before_NAO_submit.sh 2090 ua_hat ua $window $suffix
 
 # va hat
-echo "va hat before $window"
-sbatch 1Flux_before_NAO_submit.sh 1850 va_hat va $window
-sbatch 1Flux_before_NAO_submit.sh 2090 va_hat va $window
+echo "va hat before $window $suffix"
+sbatch 1Flux_before_NAO_submit.sh 1850 va_hat va $window $suffix
+sbatch 1Flux_before_NAO_submit.sh 2090 va_hat va $window $suffix
 
 # upvp
-echo "upvp before $window"
-sbatch 1Flux_before_NAO_submit.sh 1850 upvp upvp $window
-sbatch 1Flux_before_NAO_submit.sh 2090 upvp upvp $window
+echo "upvp before $window $suffix"
+sbatch 1Flux_before_NAO_submit.sh 1850 upvp upvp $window $suffix
+sbatch 1Flux_before_NAO_submit.sh 2090 upvp upvp $window $suffix
 
 # vpetp
-echo "vpetp before $window"
-sbatch 1Flux_before_NAO_submit.sh 1850 vpetp vpetp $window
-sbatch 1Flux_before_NAO_submit.sh 2090 vpetp vpetp $window
+echo "vpetp before $window $suffix"
+sbatch 1Flux_before_NAO_submit.sh 1850 vpetp vpetp $window $suffix
+sbatch 1Flux_before_NAO_submit.sh 2090 vpetp vpetp $window $suffix
 
 # upqp
-echo "upqp before $window"
-sbatch 1Flux_before_NAO_submit.sh 1850 upqp upqp $window
-sbatch 1Flux_before_NAO_submit.sh 2090 upqp upqp $window
+echo "upqp before $window $suffix"
+sbatch 1Flux_before_NAO_submit.sh 1850 upqp upqp $window $suffix
+sbatch 1Flux_before_NAO_submit.sh 2090 upqp upqp $window $suffix
 
 # vpqp
-echo "vpqp before $window"
-sbatch 1Flux_before_NAO_submit.sh 1850 vpqp vpqp $window
-sbatch 1Flux_before_NAO_submit.sh 2090 vpqp vpqp $window
+echo "vpqp before $window $suffix"
+sbatch 1Flux_before_NAO_submit.sh 1850 vpqp vpqp $window $suffix
+sbatch 1Flux_before_NAO_submit.sh 2090 vpqp vpqp $window $suffix
