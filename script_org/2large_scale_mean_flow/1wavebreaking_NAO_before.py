8d40c439542cbd4b3608aafb7c6559e7d01fbd69
Quan <quan.liu@mpimet.mpg.de>
2025-05-20 16:49:19 +0200

    wip

---
+++ script_org/2large_scale_mean_flow/1wavebreaking_NAO_before.py
@@ -0,0 +1,29 @@
+#%%
+# import xarray as xr
+import numpy as np
+import pandas as pd
+import sys
+import matplotlib.pyplot as plt
+import seaborn as sns
+import glob
+import logging
+import re
+import os
+
+from src.extremes.before_extreme import read_NAO_extremes
+from src.composite.composite_NAO_WB import read_wb
+# %%
+def read_all_data(decade, **kwargs):
+    logging.info("reading NAO extremes")
+    # wave breaking
+    NAO_pos = read_NAO_extremes(decade, 'positive')
+    NAO_neg = read_NAO_extremes(decade, 'negative')
+
+    logging.info("reading wave breaking data")
+
+    AWB = read_wb(decade, 'cwb', NAO_region=False)
+    CWB = read_wb(decade, 'cwb', NAO_region=False)
+    
+
+    return NAO_pos, NAO_neg, AWB, CWB
+# %%
