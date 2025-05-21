4974803d0f1f65b8ef38360d37c45f08376fe838
Quan <quan.liu@mpimet.mpg.de>
2025-05-21 18:06:49 +0200

    wip

--- script_org/0large_scale_mean_flow/3theta_2UPV_NAO_range.py
+++ script_org/0large_scale_mean_flow/3theta_2UPV_NAO_range.py
@@ -1,58 +1,29 @@
 #%%
 import xarray as xr
 import numpy as np
-from src.data_helper.before_extreme import read_NAO_extremes
-from src.composite.composite import range_NAO_composite
-from src.data_helper.prime_data import read_prime
+from src.data_helper.before_extreme import read_NAO_extremes_single_ens
+from src.composite import composite
+from src.data_helper import prime_data
 
 import logging
 logging.basicConfig(level=logging.INFO)
-# %%
-def read_all_data(decade, **kwargs):
-    
-    logging.info("reading NAO extremes")
-
-    NAO_pos = read_NAO_extremes(decade, "positive")
-    NAO_neg = read_NAO_extremes(decade, "negative")
-
-    logging.info("reading ua, va, and theta data")
-
-    ua_data = read_prime(
-        decade, var = "ua", name = 'ua', suffix = ""
-    )
-
-    va_data = read_prime(
-        decade, var = "va", name = 'va', suffix = ""
-    )
-    theta_data = read_prime(
-        decade, var = "theta", name = 'theta', suffix = ""
-    )
-    return NAO_pos, NAO_neg, ua_data, va_data, theta_data
-
+import importlib
+importlib.reload(composite)
+importlib.reload(prime_data)
+read_prime_single_ens = prime_data.read_prime_single_ens
+range_NAO_composite = composite.range_NAO_composite
 #%%
-def composite_single_ens(zg, pos_extreme, neg_extreme, base_plev=25000, cross_plev=1):
-    pos_date_range = zg_comp.find_lead_lag_30days(
-        pos_extreme, base_plev=base_plev, cross_plev=cross_plev
-    )
+def composite_single_ens(var, decade, ens, plev, freq_label=None, **kwargs):
+    
+    # read NAO extremes
+    pos_extreme = read_NAO_extremes_single_ens('pos', decade, ens)
+    neg_extreme = read_NAO_extremes_single_ens('neg', decade, ens)
 
-    pos_composite = None
-    neg_composite = None
+    # read variable
+    name = kwargs.get('name', var)  # default name is the same as var')
+    var_field = read_prime_single_ens(var, decade, ens, name = name, plev = plev)
 
-    if not pos_date_range.empty:
-        pos_composite = zg_comp.date_range_composite(zg, pos_date_range)
+    if not pos_extreme.empty and not neg_extreme.empty:
 
-    neg_date_range = zg_comp.find_lead_lag_30days(
-        neg_extreme, base_plev=25000, cross_plev=cross_plev
-    )
-
-    if not neg_date_range.empty:
-        neg_composite = zg_comp.date_range_composite(zg, neg_date_range)
-
-    return pos_composite, neg_composite
-
-# %%
-def process_data(decade, **kwargs):
-    # read data
-    NAO_pos, NAO_neg, ua_data, va_data, theta_data = read_all_data(decade, **kwargs)
-
+        var_pos, var_neg = range_NAO_composite(var_field, pos_extreme, neg_extreme)
 
