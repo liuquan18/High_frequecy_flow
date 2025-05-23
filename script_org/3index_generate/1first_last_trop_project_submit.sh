26d1410b8bf32c5bb0c80c5abd17f2e18d792c2c
Quan <quan.liu@mpimet.mpg.de>
2025-05-23 13:33:26 +0200

    copy

---
+++ script_org/3index_generate/1first_last_trop_project_submit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=trop
+#SBATCH --time=01:00:00
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks=10
+#SBATCH --mem=0
+#SBATCH --mail-type=FAIL
+#SBATCH --account=mh0033
+#SBATCH --output=trop.%j.out
+
+mpirun -n 10 python -u 1first_last_trop_project_index.py $1
