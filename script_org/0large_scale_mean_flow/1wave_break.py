8f79878b0653df08513f33ddf18e80b25c4c0304
Quan <quan.liu@mpimet.mpg.de>
2025-05-23 20:44:14 +0200

    with pv

--- script_org/0large_scale_mean_flow/1wave_break.py
+++ script_org/0large_scale_mean_flow/1wave_break.py
@@ -15,35 +15,67 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 # %%
-def remap(ifile, var="ua", plev=25000):
+def remap(ifile, var="ua", plev=None):
     cdo = Cdo()
 
     ofile = cdo.remapnn("r192x96", input=ifile, options="-f nc", returnXArray=var)
-
-    ofile = ofile.sel(plev=plev)
+    if plev is not None:
+        ofile = ofile.sel(plev=plev)
 
     return ofile
 
 # %%
-def wavebreaking(avor, mflux):
+def wavebreaking(pv, mflux):
+    pv = pv * 1e6
+    pv = remap(pv, var = 'pv')
+    mflux = remap(mflux, var = 'usvs', plev = 25000)
+    # contour_levels = [-2*1e-6, 2*1e-6]
+    contour_levels = [-2, 2]
+    
+    smoothed = wb.calculate_smoothed_field(data=pv.sel(isentropic_level = 330),
+                                        passes=5,
+                                        weights=np.array([[0, 1, 0], [1, 2, 1], [0, 1, 0]]), # optional
+                                        mode="wrap") # optional
 
-    # calculate overturnings index
-    overturnings = wb.calculate_overturnings(
-        data=avor,
-        contour_levels=[3 * 1e-5], # 90th percentile of [20, 60]
-        range_group=5,  # optional
-        min_exp=5,  # optional
-        intensity=mflux,  # optional
-        periodic_add=120,
-    )  # optional
+    contours = wb.calculate_contours(data=smoothed,
+                                    contour_levels=contour_levels,
+                                    periodic_add=120, # optional
+                                    original_coordinates=False) # optional
+
+    # calculate streamers
+    streamers = wb.calculate_streamers(data=smoothed,
+                                    contour_levels=contour_levels,
+                                    contours=contours, #optional
+                                    geo_dis=800, # optional
+                                    cont_dis=1200, # optional
+                                    intensity=mflux, # optional
+                                    periodic_add=120) # optional
+
 
     # classify
-    events = overturnings
+    events = streamers
+    # stratospheric and tropospheric (only for streamers and cutoffs)
+    stratospheric = events[events.mean_var >= contour_levels[1]]
+    tropospheric = events[events.mean_var < contour_levels[1]]
 
-    # positive (anticyclonic) as 2, negative (cyclonic) as 1
-    events = events.assign(mean_var=(events.intensity > 0).astype(int) + 1)
 
-    return events
+    # anticyclonic and cyclonic by intensity for the Northern Hemisphere
+    anticyclonic = events[events.intensity >= 0]
+    cyclonic = events[events.intensity < 0]
+
+
+
+    # transform to xarray.DataArray
+    stratospheric_array = wb.to_xarray(data=smoothed,
+                            events=stratospheric)
+    tropospheric_array = wb.to_xarray(data=smoothed,
+                            events=tropospheric)
+    anticyclonic_array = wb.to_xarray(data=smoothed,
+                            events=anticyclonic)
+    cyclonic_array = wb.to_xarray(data=smoothed,
+                            events=cyclonic)
+
+    return stratospheric_array, tropospheric_array, anticyclonic_array, cyclonic_array
 # %%
 def event_classify(events):
 
@@ -84,11 +116,16 @@ except:
     rank = 0
     size = 1
 # %%
-av_path = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/av_daily_ano/r{ens}i1p1f1/"
-mf_path = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/usvs_daily_ano/r{ens}i1p1f1/"
+pv_path = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/pv_daily/r{ens}i1p1f1/"
+mf_path = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/usvs_daily/r{ens}i1p1f1/"
 
-awb_path = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/WB_awb_th3_daily/r{ens}i1p1f1/"
-cwb_path = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/WB_cwb_th3_daily/r{ens}i1p1f1/"
+awb_path = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/wb_anticyclonic_daily/r{ens}i1p1f1/"
+cwb_path = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/wb_cyclonic_daily/r{ens}i1p1f1/"
+
+
+stratospheric_path = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/wb_stratospheric_daily/r{ens}i1p1f1/"
+tropospheric_path = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/wb_tropospheric_daily/r{ens}i1p1f1/"
+
 
 if rank == 0:
     if not os.path.exists(awb_path):
@@ -96,6 +133,11 @@ if rank == 0:
 
     if not os.path.exists(cwb_path):
         os.makedirs(cwb_path)
+
+    if not os.path.exists(stratospheric_path):
+        os.makedirs(stratospheric_path)
+    if not os.path.exists(tropospheric_path):
+        os.makedirs(tropospheric_path)
 # %%
 all_decades = np.arange(1850, 2100, 10)
 single_decades = np.array_split(all_decades, size)[rank]
@@ -104,26 +146,19 @@ for i, dec in enumerate(single_decades):
     logging.info(f"rank {rank} Processing {i+1}/{len(single_decades)}")
 
 
-    av_file = glob.glob(av_path + f"av_day_MPI-ESM1-2-LR_r{ens}i1p1f1_gn_{dec}*.nc")
-    av = xr.open_dataset(av_file[0])
-    av = av.AV
+    pv_file = glob.glob(pv_path + f"*{dec}*.nc")
+    pv = xr.open_dataset(pv_file[0])
+    pv = pv.pv
 
     mf_file = glob.glob(mf_path + f"*{dec}*.nc")
-    # remap mf, while av is remaped during its calculation
-    mf = remap(mf_file[0], var = 'usvs')
+    mf = xr.open_dataset(mf_file[0]).usvs
 
-    events = wavebreaking(av, mf)
+    stratospheric_array, tropospheric_array, anticyclonic_array, cyclonic_array = wavebreaking(pv, mf)
 
-    if not events.empty:
 
-        anti_tracked, cyc_tracked = event_classify(events)
-
-        anti_tracked.to_csv(
-            awb_path + f"AWB_r{ens}_{dec}0502-{dec+9}0930.csv", index=False
-        )
-
-        cyc_tracked.to_csv(
-            cwb_path + f"CWB_r{ens}_{dec}0502-{dec+9}0930.csv", index=False
-        )
-
+    # save the data
+    stratospheric_array.to_netcdf(stratospheric_path + f"wb_stratospheric_{ens}_{dec}.nc")
+    tropospheric_array.to_netcdf(tropospheric_path + f"wb_tropospheric_{ens}_{dec}.nc")
+    anticyclonic_array.to_netcdf(awb_path + f"wb_anticyclonic_{ens}_{dec}.nc")
+    cyclonic_array.to_netcdf(cwb_path + f"wb_cyclonic_{ens}_{dec}.nc")
 # %%
