b3b5da049c6a07871a277e00bead91b819379324
Quan <quan.liu@mpimet.mpg.de>
2025-05-23 17:17:36 +0200

    name

--- script_org/0large_scale_mean_flow/0pv_isent_submit.sh
+++ script_org/0large_scale_mean_flow/0pv_isent_submit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=2pv
+#SBATCH --job-name=isent
 #SBATCH --time=03:00:00
 #SBATCH --partition=compute
 #SBATCH --nodes=1
@@ -7,7 +7,7 @@
 #SBATCH --mem=200G
 #SBATCH --mail-type=FAIL
 #SBATCH --account=mh0033
-#SBATCH --output=2pv.%j.out
+#SBATCH --output=isent.%j.out
 
 
 
