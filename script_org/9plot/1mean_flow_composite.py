da7531e4889bbc25bfd65fab0bdcf882882589b9
Quan <quan.liu@mpimet.mpg.de>
2025-05-23 08:47:23 +0200

    submit

--- script_org/9plot/1mean_flow_composite.py
+++ script_org/9plot/1mean_flow_composite.py
@@ -10,17 +10,18 @@ import cartopy.feature as cfeature
 from src.data_helper.read_variable import read_MPI_GE_uhat
 
 # %%
-def read_theta_2pvu(decade, phase, time_window = (-10, 5)):
+def read_theta_2pvu(decade, phase, time_window = (-5, 5)):
     basedir = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0composite_range/theta_2pvu_NAO_{phase}_{decade}.nc"
     ds = xr.open_dataset(basedir).__xarray_dataarray_variable__
     ds = ds.sel(time=slice(*time_window))
     ds = ds.mean(dim=("time", "ens"))
     return ds
 #%%
-def read_wb(decade, wb_type, phase):
+def read_wb(decade, wb_type, phase, time_window = (-5, 5)):
     basedir = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0composite_range/{wb_type}_{phase}_{decade}.nc"
-    ds = xr.open_dataset(basedir)
-    ds = ds.mean(dim=( "ens"))
+    ds = xr.open_dataset(basedir).flag
+    ds = ds.sel(time=slice(*time_window))
+    ds = ds.mean(dim=("time", "ens"))
     return ds
 # %%
 # u hat
@@ -34,4 +35,9 @@ theta_neg_first = read_theta_2pvu(1850, "neg")
 theta_neg_last = read_theta_2pvu(2090, "neg")
 # %%
 AWB_pos_first = read_wb(1850, "AWB", "pos")
+CWB_neg_first = read_wb(1850, "CWB", "neg")
+
+AWB_pos_last = read_wb(2090, "AWB", "pos")
+CWB_neg_last = read_wb(2090, "CWB", "neg")
+
 # %%
