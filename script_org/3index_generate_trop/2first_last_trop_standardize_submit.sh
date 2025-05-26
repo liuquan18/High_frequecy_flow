70544a6e3bb663e91f11aaca8db5ded414240919
Quan <quan.liu@mpimet.mpg.de>
2025-05-26 18:42:19 +0200

    single core

--- script_org/3index_generate_trop/2first_last_trop_standardize_submit.sh
+++ script_org/3index_generate_trop/2first_last_trop_standardize_submit.sh
@@ -3,10 +3,10 @@
 #SBATCH --time=01:00:00
 #SBATCH --partition=compute
 #SBATCH --nodes=1
-#SBATCH --ntasks=10
+#SBATCH --ntasks=1
 #SBATCH --mem=0
 #SBATCH --mail-type=FAIL
 #SBATCH --account=mh0033
 #SBATCH --output=trop.%j.out
 
-mpirun -n 10 python -u 2first_last_trop_standardize.py $1
+python -u 2first_last_trop_standardize.py $1
