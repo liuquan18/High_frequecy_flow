39336cf6918fe7fc767ca36eb708ccf47323978c
Quan <quan.liu@mpimet.mpg.de>
2025-05-26 14:55:02 +0200

    remove zonal mean

--- src/data_helper/read_composite.py
+++ src/data_helper/read_composite.py
@@ -8,6 +8,7 @@ def read_comp_var(var, phase, decade, time_window=(-5, 5), **kwargs):
     name = kwargs.get("name", var)
     method = kwargs.get("method", "mean")
     suffix = kwargs.get("suffix", "")
+    remove_zonmean = kwargs.get("remove_zonmean", False)
     basedir = (
         "/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/0composite_range/"
     )
@@ -16,6 +17,8 @@ def read_comp_var(var, phase, decade, time_window=(-5, 5), **kwargs):
     ds = ds.sel(time=slice(*time_window))
     if method == "mean":
         ds = ds.mean(dim=("time", "ens"))
+        if remove_zonmean:
+            ds = ds - ds.mean(dim="lat", keep_attrs=True)
     elif method == "sum":
         ds = ds.sum(dim=("time", "ens"))
     return ds
