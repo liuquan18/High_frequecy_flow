8e4494390f6947968c02781d74b5188397b51847
Quan <quan.liu@mpimet.mpg.de>
2025-05-27 15:39:49 +0200

    copy

---
+++ script_org/4composite_distribution_change/4var_NAO_range_submit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=NAO_range
+#SBATCH --time=01:00:00
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=5
+#SBATCH --mem=200G
+#SBATCH --mail-type=FAIL
+#SBATCH --account=mh0033
+#SBATCH --output=NAO_range.%j.out
+
+
+decade=$1
+var=$2
+name=$3
+suffix=$4
+mpirun -n 5 python 4var_NAO_range.py $decade $var $name $suffix
