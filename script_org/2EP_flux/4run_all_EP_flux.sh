0057ffcbb48a80a6f1b50fb30d612eba0d8d1fc0
Quan <quan.liu@mpimet.mpg.de>
2025-05-28 16:31:59 +0200

    new name

--- script_org/2EP_flux/4run_all_EP_flux.sh
+++ script_org/2EP_flux/4run_all_EP_flux.sh
@@ -2,18 +2,18 @@
 
 # transient
 ## pos
-sbatch ./4EP_flux_isentropes_submit.sh pos 1850 False transient False
-sbatch ./4EP_flux_isentropes_submit.sh pos 2090 False transient False
+sbatch ./4EP_flux_submit.sh pos 1850 False transient False
+sbatch ./4EP_flux_submit.sh pos 2090 False transient False
 
 ## neg
-sbatch ./4EP_flux_isentropes_submit.sh neg 1850 False transient False
-sbatch ./4EP_flux_isentropes_submit.sh neg 2090 False transient False
+sbatch ./4EP_flux_submit.sh neg 1850 False transient False
+sbatch ./4EP_flux_submit.sh neg 2090 False transient False
 
 # steady
 ## pos
-sbatch ./4EP_flux_isentropes_submit.sh pos 1850 False steady False
-sbatch ./4EP_flux_isentropes_submit.sh pos 2090 False steady False
+sbatch ./4EP_flux_submit.sh pos 1850 False steady False
+sbatch ./4EP_flux_submit.sh pos 2090 False steady False
 
 ## neg
-sbatch ./4EP_flux_isentropes_submit.sh neg 1850 False steady False
-sbatch ./4EP_flux_isentropes_submit.sh neg 2090 False steady False
+sbatch ./4EP_flux_submit.sh neg 1850 False steady False
+sbatch ./4EP_flux_submit.sh neg 2090 False steady False
