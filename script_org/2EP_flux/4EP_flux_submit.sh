391765145c55d58a1f232825ff355ee77f8495a0
Quan <quan.liu@mpimet.mpg.de>
2025-05-28 19:07:23 +0200

    rename

--- script_org/2EP_flux/4EP_flux_submit.sh
+++ script_org/2EP_flux/4EP_flux_submit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=EP_isen
+#SBATCH --job-name=EP
 #SBATCH --time=01:00:00
 #SBATCH --partition=compute
 #SBATCH --nodes=1
@@ -7,7 +7,7 @@
 #SBATCH --mem=200G
 #SBATCH --mail-type=FAIL
 #SBATCH --account=mh0033
-#SBATCH --output=EP_isen.%j.out
+#SBATCH --output=EP.%j.out
 
 phase=$1 # phase
 decade=$2 # decade
