5002ed34a08782f7fe02d5b849ba4ca803ec00bf
Quan <quan.liu@mpimet.mpg.de>
2025-05-29 11:04:36 +0200

    allow ano

--- script_org/1flow_NAO_composite_range/4var_NAO_range_allplev_all.sh
+++ script_org/1flow_NAO_composite_range/4var_NAO_range_allplev_all.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # decade
 decade=$1
+suffix=$2
 # sbatch 4var_NAO_range_submit.sh $decade wb_anticyclonic flag MPI_GE_CMIP6_allplev
 # sbatch 4var_NAO_range_submit.sh $decade wb_cyclonic flag MPI_GE_CMIP6_allplev
 
-sbatch 4var_NAO_range_submit.sh $decade upvp upvp MPI_GE_CMIP6_allplev
-sbatch 4var_NAO_range_submit.sh $decade vpetp vpetp MPI_GE_CMIP6_allplev
+sbatch 4var_NAO_range_submit.sh $decade upvp upvp MPI_GE_CMIP6_allplev $suffix
+sbatch 4var_NAO_range_submit.sh $decade vpetp vpetp MPI_GE_CMIP6_allplev $suffix
 # sbatch 4var_NAO_range_submit.sh $decade upqp upqp MPI_GE_CMIP6_allplev
 # sbatch 4var_NAO_range_submit.sh $decade vpqp vpqp MPI_GE_CMIP6_allplev
 
-sbatch 4var_NAO_range_submit.sh $decade usvs usvs MPI_GE_CMIP6_allplev
-sbatch 4var_NAO_range_submit.sh $decade vsets vsets MPI_GE_CMIP6_allplev
+sbatch 4var_NAO_range_submit.sh $decade usvs usvs MPI_GE_CMIP6_allplev $suffix
+sbatch 4var_NAO_range_submit.sh $decade vsets vsets MPI_GE_CMIP6_allplev $suffix
