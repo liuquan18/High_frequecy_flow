02ba8364eaa1401cc5da988a2a5ceafef226ccb2
Quan <quan.liu@mpimet.mpg.de>
2025-06-01 16:49:56 +0200

    reorder

--- script_org/9plot/6distribution_change.py
+++ script_org/9plot/6distribution_change.py
@@ -577,24 +577,54 @@ vsets_neg_last = vsets_neg_last.to_dataframe().reset_index()
 #%%
 fig, ax = plt.subplots(1,2,figsize = (10, 8), sharex=True, sharey=True)
 
-# negative
-## transient eddy heat flux
+# positive (was axes[1], now axes[0])
+sns.lineplot(
+    x = 'time',
+    y = 'vpetp',
+    data=vpetp_pos_first,
+    color="black",
+    ax=ax[0],
+)
+sns.lineplot(
+    x = 'time',
+    y = 'vsets',
+    data=vsets_pos_first,
+    color="black",
+    ax=ax[0],
+    linestyle="--",
+)
+
+sns.lineplot(
+    x = 'time',
+    y = 'vpetp',
+    data=vpetp_pos_last,
+    color="red",
+    ax=ax[0],
+)
+sns.lineplot(
+    x = 'time',
+    y = 'vsets',
+    data=vsets_pos_last,
+    color="red",
+    ax=ax[0],
+    linestyle="--",
+)
+
+# transient eddy heat flux
 sns.lineplot(
     x = 'time',
     y = 'vpetp',
     data=vpetp_neg_first,
-    # label="transient",
     color="black",
-    ax=ax[0],
+    ax=ax[1],
 )
 ## steady eddy heat flux
 sns.lineplot(
     x = 'time',
     y = 'vsets',
     data=vsets_neg_first,
-    # label="steady",
     color="black",
-    ax=ax[0],
+    ax=ax[1],
     linestyle="--",
 )
 
@@ -604,59 +634,21 @@ sns.lineplot(
     y = 'vpetp',
     data=vpetp_neg_last,
     color="red",
-    ax=ax[0],
+    ax=ax[1],
 )
 sns.lineplot(
     x = 'time',
     y = 'vsets',
     data=vsets_neg_last,
     color="red",
-    ax=ax[0],
-    linestyle="--",
-)
-
-
-
-
-# positive
-sns.lineplot(
-    x = 'time',
-    y = 'vpetp',
-    data=vpetp_pos_first,
-    color="black",
-    ax=ax[1],
-)
-sns.lineplot(
-    x = 'time',
-    y = 'vsets',
-    data=vsets_pos_first,
-    color="black",
-    ax=ax[1],
-    linestyle="--",
-)
-
-sns.lineplot(
-    x = 'time',
-    y = 'vpetp',
-    data=vpetp_pos_last,
-    color="red",
-    ax=ax[1],
-)
-sns.lineplot(
-    x = 'time',
-    y = 'vsets',
-    data=vsets_pos_last,
-    color="red",
     ax=ax[1],
     linestyle="--",
 )
 
 # add customed legend, solid line for transient, dashed line for steady, black for first10, red for last10
-handles, labels = ax[0].get_legend_handles_labels()
 handles = [
     plt.Line2D([0], [0], color="black", linestyle="-", label="transient"),  
     plt.Line2D([0], [0], color="black", linestyle="--", label="steady"),
-
     plt.Line2D([0], [0], color="black", linestyle="-", label="first10"),
     plt.Line2D([0], [0], color="red", linestyle="-", label="last10"),
 ]
@@ -669,11 +661,15 @@ ax[0].legend(
 ax[0].set_xlabel("")
 ax[0].set_ylabel(r"eddy heat flux anomaly (K $m s^{-1}$)", fontsize=20)
 ax[1].set_xlabel("")
+
+# vline at x=0
+ax[0].axvline(x=0, color="k", linestyle="dotted", linewidth=1)
+ax[1].axvline(x=0, color="k", linestyle="dotted", linewidth=1)
 fig.supxlabel("days relative to extreme NAO onset", fontsize=20)
 
 plt.tight_layout()
 plt.savefig(
-    "/work/mh0033/m300883/High_frequecy_flow/docs/plots/0distribution_change/eddy_heat_flux_timewindow.pdf",
+    "/work/mh0033/m300883/High_frequecy_flow/docs/plots/0distribution_change/eddy_heat_flux_timewindow_last.pdf",
     dpi = 500,
     transparent=True,
 )
