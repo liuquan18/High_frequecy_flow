67a2dbb66d82401c901889e8b96beb625af95e6d
Quan <quan.liu@mpimet.mpg.de>
2025-06-02 11:52:38 +0200

    try upvp

--- script_org/0large_scale_flow/1wave_break.py
+++ script_org/0large_scale_flow/1wave_break.py
@@ -95,7 +95,7 @@ except:
     size = 1
 # %%
 pv_path = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/pv_daily/r{ens}i1p1f1/"
-mf_path = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/usvs_daily/r{ens}i1p1f1/"
+mf_path = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/upvp_daily/r{ens}i1p1f1/"  # change to transient flux
 
 awb_path = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/wb_anticyclonic_daily/r{ens}i1p1f1/"
 cwb_path = f"/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/wb_cyclonic_daily/r{ens}i1p1f1/"
