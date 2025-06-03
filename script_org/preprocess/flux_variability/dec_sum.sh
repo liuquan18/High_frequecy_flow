b20524f14a99f7728f76279efd08ad851d1d0d42
Quan <quan.liu@mpimet.mpg.de>
2025-06-03 13:41:46 +0200

    use cdo

---
+++ script_org/preprocess/flux_variability/dec_sum.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+decade=$1 # 1850, node
+var=$2
+
+echo "Node number: $SLURM_NODEID"
+echo "Decade ${decade} for variable ${var}"
+
+
+base_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6_allplev/${var}_daily/
+to_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6_allplev/${var}_decmean/
+mkdir -p $to_path
+
+file_name=*${decade}*.nc
+out_name=${var}_monmean_ensmean_${decade}05_$((${decade}+9))09.nc
+
+
+# Save the find command output to first_ens_list
+first_ens_list=($(find -L $base_path -name $file_name -print))
+
+
+cdo -O -P 5 -enssum -apply,ymonsum [ ${first_ens_list[@]} ] $to_path${out_name}
+# cdo -O -P 5 -ensmean -apply,yearmean [ ${first_ens_list[@]} ] $to_path${out_name}
