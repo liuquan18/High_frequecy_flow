03406880958d8c38b57c30b5c0a7005812912d57
Quan <quan.liu@mpimet.mpg.de>
2025-06-05 14:31:59 +0200

    no isent

--- script_org/2EP_flux/4run_all_EP_flux.sh
+++ script_org/2EP_flux/4run_all_EP_flux.sh
@@ -2,18 +2,18 @@
 
 # transient
 ## pos
-sbatch ./4EP_flux_submit.sh pos 1850 False transient False
-sbatch ./4EP_flux_submit.sh pos 2090 False transient False
+sbatch ./4EP_flux_submit.sh pos 1850 transient False
+sbatch ./4EP_flux_submit.sh pos 2090 transient False
 
 ## neg
-sbatch ./4EP_flux_submit.sh neg 1850 False transient False
-sbatch ./4EP_flux_submit.sh neg 2090 False transient False
+sbatch ./4EP_flux_submit.sh neg 1850 transient False
+sbatch ./4EP_flux_submit.sh neg 2090 transient False
 
 # steady
 ## pos
-sbatch ./4EP_flux_submit.sh pos 1850 False steady False
-sbatch ./4EP_flux_submit.sh pos 2090 False steady False
+sbatch ./4EP_flux_submit.sh pos 1850 steady False
+sbatch ./4EP_flux_submit.sh pos 2090 steady False
 
 ## neg
-sbatch ./4EP_flux_submit.sh neg 1850 False steady False
-sbatch ./4EP_flux_submit.sh neg 2090 False steady False
+sbatch ./4EP_flux_submit.sh neg 1850 steady False
+sbatch ./4EP_flux_submit.sh neg 2090 steady False
