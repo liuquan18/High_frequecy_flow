6f4f7e982f91bffe8b9abc3a77f26bc5ad76109f
Quan <quan.liu@mpimet.mpg.de>
2025-06-05 14:27:20 +0200

    no isen

--- script_org/2EP_flux/4EP_flux_submit.sh
+++ script_org/2EP_flux/4EP_flux_submit.sh
@@ -11,10 +11,9 @@
 
 phase=$1 # phase
 decade=$2 # decade
-isentrope=$3 # isentrope
-eddy=$4 # steady or transient
-ano=$5 # anomaly
+eddy=$3 # steady or transient
+ano=$4 # anomaly
 
 echo run $phase for $decade
 
-python 4EP_flux.py $phase $decade $isentrope $eddy $ano
+python 4EP_flux.py $phase $decade $eddy $ano
