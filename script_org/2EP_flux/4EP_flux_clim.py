56dbdb52634fbf59b33969ecd461676354088189
Quan <quan.liu@mpimet.mpg.de>
2025-06-05 20:51:40 +0200

    nothing

--- script_org/2EP_flux/4EP_flux_clim.py
+++ script_org/2EP_flux/4EP_flux_clim.py
@@ -9,10 +9,9 @@ importlib.reload(EP_flux_module)
 from src.dynamics.EP_flux import (  # noqa: E402
     EP_flux,
     eff_stat_stab_xr,
-    read_data_all,
 )
 
-
+from src.data_helper.read_variable import read_climatology
 
 logging.basicConfig(
     level=logging.INFO,
@@ -23,15 +22,25 @@ logging.basicConfig(
 #%%
 
 
-def calculate_EP_flux(decade, phase, ano=False, equiv_theta=True, eddy='transient'):
+def calculate_EP_flux(decade, equiv_theta=True, eddy='transient'):
     """
     Calculate EP flux for a given decade and phase
     """
     # Read data
-    logging.info (f"Read data for {phase} phase in {decade}")
-    upvp, vptp, ta = read_data_all(decade, phase, equiv_theta=equiv_theta, eddy=eddy)
+    logging.info (f"Read data for {decade}")
+    upvp = read_climatology(var = 'upvp',decade = decade)
 
-    logging.info (f"Calculate {eddy} EP flux for {phase} phase in {decade}")
+    if equiv_theta:
+        vptp = read_climatology(var = 'vpetp', decade = decade)
+    else:
+        vptp = read_climatology(var = 'vptp', decade = decade)
+    # read temperature to compute static stability
+    if eddy == 'transient':
+        ta = read_climatology(var = 'ta', decade = decade)
+    elif eddy == 'steady':
+        ta = read_climatology(var = 'ta_hat', decade = decade, name = 'ta')
+    
+    logging.info (f"Calculate {eddy} EP flux {decade}")
     save_dir="/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6_allplev/0EP_flux/"
 
     # Calculate EP flux
@@ -39,29 +48,17 @@ def calculate_EP_flux(decade, phase, ano=False, equiv_theta=True, eddy='transien
     F_phi, F_p, div_phi, div_p = EP_flux(vptp, upvp, stat_stab)
 
     # Save data
-    logging.info (f"Save data for {phase} phase in {decade}")
+    logging.info (f"Save data for clima phase in {decade}")
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
-    F_phi.to_netcdf(os.path.join(save_dir, f"{eddy}_F_phi_{phase}_{decade}_ano{ano}.nc"))
-    F_p.to_netcdf(os.path.join(save_dir, f"{eddy}_F_p_{phase}_{decade}_ano{ano}.nc"))
-    div_phi.to_netcdf(os.path.join(save_dir, f"{eddy}_div_phi_{phase}_{decade}_ano{ano}.nc"))
-    div_p.to_netcdf(os.path.join(save_dir, f"{eddy}_div_p_{phase}_{decade}_ano{ano}.nc"))
+    F_phi.to_netcdf(os.path.join(save_dir, f"{eddy}_F_phi_clima_{decade}.nc"))
+    F_p.to_netcdf(os.path.join(save_dir, f"{eddy}_F_p_clima_{decade}.nc"))
+    div_phi.to_netcdf(os.path.join(save_dir, f"{eddy}_div_phi_clima_{decade}.nc"))
+    div_p.to_netcdf(os.path.join(save_dir, f"{eddy}_div_p_clima_{decade}.nc"))
 
 #%%
-phase = sys.argv[1] # 'pos' or 'neg'
-decade = sys.argv[2] # '1850' or '2090'
-eddy = sys.argv[3]
-ano = sys.argv[4].lower() == 'true' if len(sys.argv) > 4 else False  # Convert string to boolean, default is False
-
-#%%
-if __name__ == "__main__":
-    # Summary of the input
-    logging.info(f"Phase: {phase}")
-    logging.info(f"Decade: {decade}")
-    logging.info(f"Eddy: {eddy}")
-    logging.info(f"Anomaly: {ano}")
-    
-    # Calculate EP flux for NAO pos and neg phase
-    calculate_EP_flux(decade, phase, ano=ano, equiv_theta=True, eddy=eddy)
-
+for eddy in ['transient', 'steady']:
+    for dec in [1850, 2090]:
+        calculate_EP_flux(decade=dec, eddy=eddy, equiv_theta=True)
+        logging.info(f"Finished calculating EP flux for {eddy} phase in {dec}")
 # %%
