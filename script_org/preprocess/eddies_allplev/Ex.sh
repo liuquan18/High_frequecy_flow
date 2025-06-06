9986a0446e85768684f3e225747e01ebc0fb40fe
Quan <quan.liu@mpimet.mpg.de>
2025-06-06 17:51:06 +0200

    calculate M2

---
+++ script_org/preprocess/eddies_allplev/Ex.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+module load cdo/2.5.0-gcc-11.2.0
+module load parallel
+
+member=$1
+
+
+up_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6_allplev/ua_prime_daily/r${member}i1p1f1/
+vp_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6_allplev/va_prime_daily/r${member}i1p1f1/
+output_dir=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6_allplev/M2_daily/r${member}i1p1f1/
+
+mkdir -p ${output_dir}
+export up_path
+export vp_path
+export output_dir
+export member
+
+echo "Ensemble member ${member}"
+
+
+Ex_cal(){
+    dec=$1
+    up_file=$(find ${up_path} -name "*${dec}*.nc" | head -n 1)
+    vp_file=$(find ${vp_path} -name "*${dec}*.nc" | head -n 1)
+    
+    if [ -z "$up_file" ] || [ -z "$vp_file" ]; then
+        echo "Files not found for date: $dec"
+        return
+    fi
+    
+    output_file=${output_dir}M2_${dec}.nc
+    echo "Calculating M2 for date: $dec"
+    
+    cdo -r -O -expr,"M2=va*va-ua*ua" -merge ${up_file} ${vp_file} ${output_file}
+}
+
+export -f Ex_cal
+
+parallel -j 5 Ex_cal ::: {1850..2090..10}
