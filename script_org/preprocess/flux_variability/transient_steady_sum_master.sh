a8c172437e3fd0c472e6c8c4f6311539433ff5d0
Quan <quan.liu@mpimet.mpg.de>
2025-06-10 16:28:37 +0200

    submit

---
+++ script_org/preprocess/flux_variability/transient_steady_sum_master.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+var1=Fdiv_p_transient
+var2=Fdiv_p_steady
+
+
+#for loop 1-50
+for ens in {1..50}
+do
+    echo "Ensemble member ${ens}"
+    # run the python script
+    sbatch transient_steady_sum.sh ${ens} ${var1} ${var2} ${suffix}
+    # ./1upvp.sh ${ens}
+done
