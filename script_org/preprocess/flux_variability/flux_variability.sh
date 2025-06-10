ac05297b8fdf86c52d78c818cd5efd53025c7eb4
Quan <quan.liu@mpimet.mpg.de>
2025-06-10 17:42:29 +0200

    name

--- script_org/preprocess/flux_variability/flux_variability.sh
+++ script_org/preprocess/flux_variability/flux_variability.sh
@@ -17,17 +17,17 @@ var1=$2
 var2=$3
 suffix=$4
 
-var1_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6_allplev/${var1}_daily_ano${suffix}/r${member}i1p1f1/
+var1_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6_allplev/${var1}_daily_ano/r${member}i1p1f1/
 
-var2_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6_allplev/${var2}_daily_ano${suffix}/r${member}i1p1f1/
+var2_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6_allplev/${var2}_daily_ano/r${member}i1p1f1/
 
 echo "Ensemble member ${member}"
 
 # create output directory
 if [ "$var1" == "$var2" ]; then
-    output_dir=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6_allplev/${var1}_std${suffix}/r${member}i1p1f1/
+    output_dir=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6_allplev/${var1}_std_daily/r${member}i1p1f1/
 else
-    output_dir=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6_allplev/${var1}_${var2}_cov${suffix}/r${member}i1p1f1/
+    output_dir=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6_allplev/${var1}_${var2}_cov_daily/r${member}i1p1f1/
 fi
 mkdir -p ${output_dir}
 
