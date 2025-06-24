3342987fbfd9df36acbb4f732fb89b1d4dcd20f5
Quan <quan.liu@mpimet.mpg.de>
2025-06-24 16:10:12 +0200

    format

--- script_org/11plot_thesis/rossby_wave_breaking.py
+++ script_org/11plot_thesis/rossby_wave_breaking.py
@@ -4,19 +4,20 @@ import cartopy.crs as ccrs
 from src.plotting.util import clip_map
 import numpy as np
 import matplotlib.ticker as mticker
+import cartopy.feature as cfeature
 
 # %%
 fig, axes = plt.subplots(
-    1,2, figsize=(12, 6), subplot_kw={'projection': ccrs.Orthographic(central_longitude=-30, central_latitude=70)}
+    1, 2, figsize=(10, 5), subplot_kw={'projection': ccrs.Orthographic(central_longitude=-30, central_latitude=70)}
 )
 
 for ax in axes:
     # ax.set_extent([-180, 180, 0, 90], crs=ccrs.PlateCarree())
-    ax.coastlines(linewidth=1)
+    ax.coastlines(linewidth=1.5)
     ax.add_feature(ccrs.cartopy.feature.LAND, facecolor='lightgray', edgecolor='black')
     ax.set_extent([-180, 180, 20, 90], crs=ccrs.PlateCarree())
     # add gridlines
-    gl = ax.gridlines(draw_labels=False, linewidth=1, color='gray', alpha=0.5, linestyle='--')
+    gl = ax.gridlines(draw_labels=False, linewidth=1.5, color='gray', alpha=0.5, linestyle='--')
     gl.top_labels = False
     gl.right_labels = False
     gl.bottom_labels = False
@@ -24,7 +25,9 @@ for ax in axes:
     gl.xlocator = mticker.FixedLocator(np.arange(-180, 181, 30))
     gl.ylocator = mticker.FixedLocator(np.arange(20, 91, 20))
     clip_map(ax)
+    # Set the edge (boundary) linewidth
+    ax.spines['geo'].set_linewidth(1.5)
 
 plt.tight_layout()
-plt.savefig("/work/mh0033/m300883/High_frequecy_flow/docs/plots/0thesis/rossby_wave_breaking_map.svg", dpi=300, bbox_inches="tight", transparent=True)
+plt.savefig("/work/mh0033/m300883/High_frequecy_flow/docs/plots/0thesis/rossby_wave_breaking_map.png", dpi=300, bbox_inches="tight", transparent=True)
 # %%
