63bdc008b1315d1227323dd0ac346c314b25f26f
Quan <quan.liu@mpimet.mpg.de>
2025-07-02 18:00:03 +0200

    calculate seperately

---
+++ script_org/6ERA_composite/5effective_static_stability.py
@@ -0,0 +1,32 @@
+#%%
+import src.dynamics.EP_flux as EP_flux_module
+import importlib
+import sys
+import os
+from src.data_helper.read_composite import read_comp_var_ERA5
+
+import logging
+importlib.reload(EP_flux_module)
+from src.dynamics.EP_flux import (  # noqa: E402
+    EP_flux,
+    eff_stat_stab_xr,
+)
+#%%
+
+# %%
+def process_data(phase, equiv_theta=True):
+    
+    ta = read_comp_var_ERA5('ta_hat', phase, time_window='all', name = 'var130', method='no_stat', equiv_theta=True)
+    
+    # rechunk
+    ta = ta.load()
+    
+    stat_stab = eff_stat_stab_xr(ta)
+
+    #save
+    stat_stab.to_netcdf(f"/work/mh0033/m300883/High_frequecy_flow/data/ERA5_allplev/0EP_flux_distribution/stat_stab_{phase}.nc")
+# %%
+phase = sys.argv[1] if len(sys.argv) > 1 else "pos"
+
+logging.info(f"Processing data for {phase} phase")
+process_data(phase, equiv_theta=True)
