db206320ad16345d91f3f40099b1d4361dc05c2f
Quan <quan.liu@mpimet.mpg.de>
2025-07-03 14:41:11 +0200

    submit

--- script/20ERA5/3standard_index_submit.sh
+++ script/20ERA5/3standard_index_submit.sh
@@ -3,10 +3,10 @@
 #SBATCH --time=01:00:00
 #SBATCH --partition=compute
 #SBATCH --nodes=5
-#SBATCH --ntasks=30
+#SBATCH --ntasks=10
 #SBATCH --mem=0
 #SBATCH --mail-type=FAIL
 #SBATCH --account=mh0033
 #SBATCH --output=std.%j.out
 
-mpirun -n 30 python -u 3standard_index.py
+mpirun -n 50 python -u 3standard_index.py
