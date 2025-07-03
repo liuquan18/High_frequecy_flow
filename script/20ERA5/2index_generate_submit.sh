20de5cc47ffa59684e7a9a116c7d0631a4e32532
Quan <quan.liu@mpimet.mpg.de>
2025-07-03 14:38:28 +0200

    some bugs

--- script/20ERA5/2index_generate_submit.sh
+++ script/20ERA5/2index_generate_submit.sh
@@ -3,10 +3,10 @@
 #SBATCH --time=01:00:00
 #SBATCH --partition=compute
 #SBATCH --nodes=5
-#SBATCH --ntasks=30
+#SBATCH --ntasks=10
 #SBATCH --mem=0
 #SBATCH --mail-type=FAIL
 #SBATCH --account=mh0033
 #SBATCH --output=proj.%j.out
 
-mpirun -n 30 python -u 2index_generate.py
+mpirun -n 50 python -u 2index_generate.py
