7dc63094a3e1cbe51052f18510b8e7cf316c25ea
Quan <quan.liu@mpimet.mpg.de>
2025-07-10 14:18:39 +0200

    change dir

--- script_org/preprocess/flux_variability/flux_variability.sh
+++ script_org/preprocess/flux_variability/flux_variability.sh
@@ -17,12 +17,12 @@ var1=$2
 var2=$3
 suffix=$4
 
-# var1_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6_allplev/${var1}_daily_ano/r${member}i1p1f1/
+var1_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6_allplev/${var1}_daily_ano/r${member}i1p1f1/
 
-# var2_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6_allplev/${var2}_daily_ano/r${member}i1p1f1/
+var2_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6_allplev/${var2}_daily_ano/r${member}i1p1f1/
 
-var1_path=/scratch/m/m300883/${var1}_daily${suffix}/r${member}i1p1f1/
-var2_path=/scratch/m/m300883/${var2}_daily${suffix}/r${member}i1p1f1/
+# var1_path=/scratch/m/m300883/${var1}_daily${suffix}/r${member}i1p1f1/
+# var2_path=/scratch/m/m300883/${var2}_daily${suffix}/r${member}i1p1f1/
 
 echo "Ensemble member ${member}"
 
