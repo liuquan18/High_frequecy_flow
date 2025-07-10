6aab0bc08c0d4cc2719e7e30027144ed98b4c779
Quan <quan.liu@mpimet.mpg.de>
2025-07-10 17:50:14 +0200

    save

--- script_org/10plot_text/10index_pdf_change.py
+++ script_org/10plot_text/10index_pdf_change.py
@@ -10,18 +10,6 @@ import glob
 import logging
 # Set logging level
 logging.basicConfig(level=logging.INFO)
-# %%
-transient_first = read_prime(1850, var = 'Fdiv_phi_transient_index', name = 'phi_index', suffix = '', model_dir = 'MPI_GE_CMIP6_allplev', plev = None)
-transient_last = read_prime(2090, var = 'Fdiv_phi_transient_index', name = 'phi_index', suffix = '', model_dir = 'MPI_GE_CMIP6_allplev', plev = None)
-# %%
-steady_first = read_prime(1850, var = 'Fdiv_phi_steady_index', name = 'phi_index', suffix = '', model_dir = 'MPI_GE_CMIP6_allplev', plev = None)
-steady_last = read_prime(2090, var = 'Fdiv_phi_steady_index', name = 'phi_index', suffix = '', model_dir = 'MPI_GE_CMIP6_allplev', plev = None)
-# %%
-# to dataframe
-transient_first_df = transient_first.to_dataframe('phi_index').reset_index()
-transient_last_df = transient_last.to_dataframe('phi_index').reset_index()
-steady_first_df = steady_first.to_dataframe('phi_index').reset_index()
-steady_last_df = steady_last.to_dataframe('phi_index').reset_index()
 
 #%%
 std_transient = []
@@ -47,29 +35,51 @@ for decade in range(1850, 2091, 10):
     transient_flat = transient.stack(com = ('time', 'ens'))
     steady_flat = steady.stack(com = ('time', 'ens'))
     covariance = xr.cov(transient_flat, steady_flat, dim='com').compute()
+    covariance['decade'] = decade
     covariances.append(covariance)
 
 
 std_transient = xr.concat(std_transient, dim='decade')
 std_steady = xr.concat(std_steady, dim='decade')
 covariances = xr.concat(covariances, dim='decade')
+#%%
+steady.close()
+transient.close()
+# %%
+transient_first = read_prime(1850, var = 'Fdiv_phi_transient_index', name = 'phi_index', suffix = '', model_dir = 'MPI_GE_CMIP6_allplev', plev = None)
+transient_last = read_prime(2090, var = 'Fdiv_phi_transient_index', name = 'phi_index', suffix = '', model_dir = 'MPI_GE_CMIP6_allplev', plev = None)
+# %%
+steady_first = read_prime(1850, var = 'Fdiv_phi_steady_index', name = 'phi_index', suffix = '', model_dir = 'MPI_GE_CMIP6_allplev', plev = None)
+steady_last = read_prime(2090, var = 'Fdiv_phi_steady_index', name = 'phi_index', suffix = '', model_dir = 'MPI_GE_CMIP6_allplev', plev = None)
+# %%
+# to dataframe
+transient_first_df = transient_first.to_dataframe('phi_index').reset_index()
+transient_last_df = transient_last.to_dataframe('phi_index').reset_index()
+steady_first_df = steady_first.to_dataframe('phi_index').reset_index()
+steady_last_df = steady_last.to_dataframe('phi_index').reset_index()
 
+covariances_df = covariances.to_dataframe('covariance').reset_index()
 
 # %%
 fig, axes = plt.subplots(2, 3, figsize=(15, 10), sharey=False, sharex=False)
 
 # Plot PDF for transient
-sns.histplot(transient_first_df['phi_index'], bins = np.arange(-6000, 6000, 200), ax=axes[0,1], color='black', label='1850', stat='density', kde=False, alpha=0.5)
-sns.histplot(transient_last_df['phi_index'],  bins = np.arange(-6000, 6000, 200),ax=axes[0,1], color='red', label='2090', stat='density', kde=False, alpha=0.5)
+sns.histplot(transient_first_df['phi_index'], bins = np.arange(-250, 251, 10), ax=axes[0,1], color='black', label='1850', stat='density', kde=False, alpha=0.5)
+sns.histplot(transient_last_df['phi_index'],  bins = np.arange(-250, 251, 10),ax=axes[0,1], color='red', label='2090', stat='density', kde=False, alpha=0.5)
 axes[0,1].set_title('Transient')
 axes[0,1].legend()
 
 # Plot PDF for steady
-sns.histplot(steady_first_df['phi_index'], bins = np.arange(-6000, 6000, 200), ax=axes[0,2], color='black', label='1850', stat='density', kde=False, alpha=0.5)
-sns.histplot(steady_last_df['phi_index'], bins = np.arange(-6000, 6000, 200), ax=axes[0,2], color='red', label='2090', stat='density', kde=False, alpha=0.5)
+sns.histplot(steady_first_df['phi_index'], bins = np.arange(-250, 251, 10), ax=axes[0,2], color='black', label='1850', stat='density', kde=False, alpha=0.5)
+sns.histplot(steady_last_df['phi_index'], bins = np.arange(-250, 251, 10), ax=axes[0,2], color='red', label='2090', stat='density', kde=False, alpha=0.5)
 axes[0,2].set_title('Steady')
 axes[0,2].legend()
 
+# Plot covariance
+axes[1,0].plot(covariances_df['decade'], covariances_df['covariance'], marker='o', color='black')
+axes[1,0].set_title('Covariance')
+axes[1,0].set_xlabel('Decade')
+axes[1,0].set_ylabel('Covariance')
 
 # Plot standard deviation for transient
 axes[1,1].plot(std_transient['decade'], std_transient, marker='o', color='black')
