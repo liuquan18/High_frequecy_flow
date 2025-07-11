e651bfe5d02ce6f46b6e6deef6977a989320ac81
Quan <quan.liu@mpimet.mpg.de>
2025-07-11 15:45:50 +0200

    rename

--- script_org/0large_scale_flow/4eady_growth_rate_submit.sh
+++ script_org/0large_scale_flow/4eady_growth_rate_submit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=2pvu
+#SBATCH --job-name=eady
 #SBATCH --time=01:00:00
 #SBATCH --partition=compute
 #SBATCH --nodes=1
@@ -7,7 +7,7 @@
 #SBATCH --mem=200G
 #SBATCH --mail-type=FAIL
 #SBATCH --account=mh0033
-#SBATCH --output=2pvu.%j.out
+#SBATCH --output=eady.%j.out
 
 
 
