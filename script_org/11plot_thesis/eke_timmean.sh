46d9a8b1f17e83ce550c675412934c12152b840f
Quan <quan.liu@mpimet.mpg.de>
2025-07-14 16:54:18 +0200

    different nodes

--- script_org/11plot_thesis/eke_timmean.sh
+++ script_org/11plot_thesis/eke_timmean.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=eke_mean
 #SBATCH --time=01:00:00
 #SBATCH --partition=compute
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
+#SBATCH --nodes=46
+#SBATCH --ntasks=46
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=0
 #SBATCH --mail-type=FAIL
@@ -23,4 +23,37 @@ out_file=/work/mh0033/m300883/High_frequecy_flow/data/ERA5/zg_monthly_stat/eke_5
 # find all files in eke_dir
 eke_files=($(find $eke_dir -name "*.nc" -print))
 
-cdo -r -P 8 -timmean -mergetime -apply,sellevel,50000 [ "${eke_files[@]}" ] $out_file
+
+year_mean_500(){
+    infile=$1
+    tmpfile=${tmp_dir}$(basename $infile .nc)_tmp.nc
+
+    # select 50000 hPa level and apply grid type
+    cdo -r -f nc -O -P 8 -timmean -sellevel,50000 -setgridtype,regular $infile $tmpfile
+    echo $tmpfile
+}
+
+
+for file in "${eke_files[@]}"; do
+    echo "Processing $file"
+    while [ "$(jobs -p | wc -l)" -ge "$SLURM_NTASKS" ]; do
+        sleep 2
+    done
+    srun --ntasks=1 --nodes=1 --cpus-per-task=$SLURM_CPUS_PER_TASK bash -c "year_mean_500 '$file'" &
+done
+wait
+# Merge all processed files
+eke_files=($(find $tmp_dir -name "*_tmp.nc" -print))
+echo "Merging files: ${eke_files[@]}"
+# Use cdo to merge and calculate the time mean
+if [ -f "$out_file" ]; then
+    rm -f "$out_file"
+fi
+echo "Output file: $out_file"
+
+
+# Merge and calculate the time mean
+cdo -r -O -P 8 -mergetime ${eke_files[@]} $out_file
+# delete the tmp files
+rm -f ${tmp_dir}*_tmp.nc
+# cdo -r -O -P 8 -timmean -mergetime ${eke_files[@]} $out_file
