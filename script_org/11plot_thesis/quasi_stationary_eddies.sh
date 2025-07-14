c98adb649b64f75fe5de4847d6e3281ff7d16984
Quan <quan.liu@mpimet.mpg.de>
2025-07-14 17:27:05 +0200

    global map

--- script_org/11plot_thesis/quasi_stationary_eddies.sh
+++ script_org/11plot_thesis/quasi_stationary_eddies.sh
@@ -14,13 +14,39 @@
 module load cdo/2.5.0-gcc-11.2.0
 module load parallel
 
-infile=/work/mh0033/m300883/High_frequecy_flow/data/ERA5/zg_monthly_stat/zg_50000_monthly_05_09.nc
+from_dir=/pool/data/ERA5/E5/pl/an/1M/129/
+tmp_dir=/scratch/m/m300883/ERA5/zg_hat/
+to_dir=/work/mh0033/m300883/High_frequecy_flow/data/ERA5/zg_monthly_state/
 
-tmpfile=/scratch/m/m300883/zg_50000_monthly_05_09_detrend.nc
-outfile=/work/mh0033/m300883/High_frequecy_flow/data/ERA5/zg_monthly_stat/zg_50000_monthly_05_09_zonalano.nc
+mkdir -p ${tmp_dir} ${to_dir}
 
+cdo -P 8 -r -timmean -sub ${infile} -enlarge,${infile} -zonmean ${infile} ${outfile}
 
-# detrend data
-cdo -r -O -P 8 -detrend $infile $tmpfile
+export from_dir tmp_dir to_dir
 
-cdo -P 8 -r -sub ${tmpfile} -enlarge,${tmpfile} -zonmean ${tmpfile} ${outfile}
+steady_eddy(){
+    dec=$1
+    Tfile=$(find ${T_path} -name "*${dec}*.nc")
+
+    cdo -P 8 -O -timmean -sub ${Tfile} -enlarge,${Tfile} -zonmean ${Tfile} ${tmp_dir}/zg_hat_${dec}.nc
+
+}
+
+export -f steady_eddy
+
+for dec in {1979..2024}; do
+    echo "Processing $file"
+    while [ "$(jobs -p | wc -l)" -ge "$SLURM_NTASKS" ]; do
+        sleep 2
+    done
+    srun --ntasks=1 --nodes=1 --cpus-per-task=$SLURM_CPUS_PER_TASK bash -c "steady_eddy ${dec}"
+
+done
+
+wait
+
+# Merge all processed files
+zg_files=($(find ${tmp_dir} -name "zg_hat_*.nc" -print))
+echo "Merging files: ${zg_files[@]}"
+# Use cdo to merge and calculate the time mean
+cdo -r -O -P 8 -timmean -mergetime ${zg_files[@]} ${to_dir}/zg_hat_monthly_mean.nc
