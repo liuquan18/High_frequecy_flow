417c2f6af04031245e6a455e598c68f9376ae9d8
Quan <quan.liu@mpimet.mpg.de>
2025-07-14 16:16:59 +0200

    get the zonal anomaly

---
+++ script_org/11plot_thesis/quasi_stationary_eddies.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=zg_hat
+#SBATCH --time=01:00:00
+#SBATCH --partition=compute
+#SBATCH --nodes=46
+#SBATCH --ntasks=46
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=0
+#SBATCH --mail-type=FAIL
+#SBATCH --account=mh0033
+#SBATCH --output=zg_hat.%j.out
+
+
+module load cdo/2.5.0-gcc-11.2.0
+module load parallel
+
+infile=/work/mh0033/m300883/High_frequecy_flow/data/ERA5/zg_monthly_stat/zg_50000_monthly_05_09.nc
+
+outfile=/work/mh0033/m300883/High_frequecy_flow/data/ERA5/zg_monthly_stat/zg_50000_monthly_05_09_zonalano.nc
+
+
+cdo -P 8 -r -sub ${infile} -enlarge,${infile} -zonmean ${infile} ${outfile}
