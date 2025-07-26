1a03d2eb3efc90c047d28ba2deb7adbfc5df6452
Quan <quan.liu@mpimet.mpg.de>
2025-07-26 15:04:28 +0200

    rename

---
+++ script_org/8wavebreaking_stat/cyclonic_fldmean.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --job-name=awb
+#SBATCH --time=01:30:00
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks=10
+#SBATCH --mem=200G
+#SBATCH --mail-type=FAIL
+#SBATCH --account=mh0033
+#SBATCH --output=awb.%j.out
+
+module load cdo/2.5.0-gcc-11.2.0
+module load parallel
+
+fldmean(){
+    member=$1
+    dec=$2
+
+    from_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/wb_cyclonic_daily/r${member}i1p1f1/
+    to_path=/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6/wb_cyclonic_fldmean/r${member}i1p1f1/
+
+    mkdir -p ${to_path}
+
+    file=$(find ${from_path} -name "*${dec}*.nc" | head -n 1)
+    if [ -z "$file" ]; then
+        echo "File not found for member: $member, date: $dec"
+        return
+    fi
+    output_file=${to_path}wb_cyclonic_fldmean_${dec}.nc
+    echo "Calculating field mean for member: $member, date: $dec"
+    cdo -r -O -fldmean -sellonlatbox,-100,-30,45,75 ${file} ${output_file}
+}
+
+export -f fldmean
+
+parallel --jobs 10 --bar fldmean ::: {1..50} ::: {1850..2090..10}
