aece8ca7888356a17937bbb13695da9e9e3c31d0
Quan <quan.liu@mpimet.mpg.de>
2025-11-25 17:00:18 +0100

    slab ocean

---
+++ script_org/preprocess/collect_var/vco2_land_slab_ocean_submit_split_merge_decade_allplev.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+var=$1
+simulation=$2 #'vco2_4xco2_land', 'vco2_4xco2_ocean'
+#for loop 1-50
+for ens in {01..24}
+do
+    echo "Ensemble member ${ens}"
+    # run the python script
+    sbatch vco2_land_slab_ocean_split_merge_decade_allplev.sh ${ens} ${var} ${simulation}
+done
