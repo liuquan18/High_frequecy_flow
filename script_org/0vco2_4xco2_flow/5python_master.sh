ae1309090e13339d58e7ceb49b8eea254c377edd
Quan <quan.liu@mpimet.mpg.de>
2025-12-01 19:01:15 +0100

    hat

--- script_org/0vco2_4xco2_flow/5python_master.sh
+++ script_org/0vco2_4xco2_flow/5python_master.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-file=$1 # python file to run
+var=$1 # variable name
 
 for simulations in 'vco2_4xco2_land' 'vco2_4xco2_all' 'vco2_4xco2_land_mlo' 'vco2_4xco2_ocean'
 do
     echo "Simulation ${simulations}"
     # run the python script
-    sbatch 3python_submit.sh $file ${simulations}
+    sbatch 5python_submit.sh ${simulations} $var
 done
