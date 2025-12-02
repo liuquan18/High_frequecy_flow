0f4ebea287f283a9e342186195b8585a117af21c
Quan <quan.liu@mpimet.mpg.de>
2025-12-02 11:53:51 +0100

    new parallel

--- script_org/preprocess/collect_var/vco2_landocean_submit_split_merge_decade_allplev.sh
+++ script_org/preprocess/collect_var/vco2_landocean_submit_split_merge_decade_allplev.sh
@@ -3,9 +3,6 @@
 var=$1
 simulation=$2 #'vco2_4xco2_land', 'vco2_4xco2_ocean'
 #for loop 1-50
-for ens in {01..24}
-do
-    echo "Ensemble member ${ens}"
-    # run the python script
-    sbatch vco2_landocean_split_merge_decade_allplev.sh ${ens} ${var} ${simulation}
-done
+
+# run the python script
+sbatch vco2_landocean_split_merge_decade_allplev.sh ${var} ${simulation}
---
+++ script_org/preprocess/collect_var/vco2_landocean_submit_split_merge_decade_allplev_loopall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Loop over variables
+for var in u geopoth t
+do
+    # Loop over simulations
+    for simulation in vco2_4xco2_land vco2_4xco2_ocean vco2_4xco2_all
+    do
+        echo "Processing variable: ${var}, simulation: ${simulation}"
+        # run the python script
+        sbatch vco2_landocean_split_merge_decade_allplev.sh ${var} ${simulation}
+    done
+done
