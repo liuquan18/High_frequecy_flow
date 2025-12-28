c19b067d70b8e9323cea21f7d6f216ede24b74ef
Quan <quan.liu@mpimet.mpg.de>
2025-12-28 12:11:22 +0100

    reduce

--- script_org/0large_scale_flow/1wave_break_allisen_submitter.sh
+++ script_org/0large_scale_flow/1wave_break_allisen_submitter.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=wb
-#SBATCH --time=01:00:00
+#SBATCH --time=02:00:00
 #SBATCH --partition=compute
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=5
+#SBATCH --ntasks-per-node=2
 #SBATCH --mem=200G
 #SBATCH --mail-type=FAIL
 #SBATCH --account=mh0033
@@ -11,4 +11,4 @@
 
 
 
-mpirun -n 5 python 1wave_break_allisen.py $1
+mpirun -n 2 python 1wave_break_allisen.py $1
