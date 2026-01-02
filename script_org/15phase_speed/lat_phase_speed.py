b9b354091ddb6d3e321ca2b7a26ea0ae46a9abb5
Quan <quan.liu@mpimet.mpg.de>
2026-01-02 12:01:01 +0100

    get plots

--- script_org/15phase_speed/lat_phase_speed.py
+++ script_org/15phase_speed/lat_phase_speed.py
@@ -178,7 +178,7 @@ axes[0].legend(fontsize=10)
 # Right plot: Difference
 P_combined_diff = P_combined_last - P_combined_first
 
-levels_diff = np.arange(-0.3, 0.31, 0.05)
+levels_diff = np.arange(-0.4, 0.41, 0.04)
 levels_diff = levels_diff[levels_diff != 0]  # Remove zero level
 
 cs_diff = axes[1].contourf(
