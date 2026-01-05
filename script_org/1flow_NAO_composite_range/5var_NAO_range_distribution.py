071a17f9d81da6041635fd463db709455d2b83a5
Quan <quan.liu@mpimet.mpg.de>
2026-01-05 13:47:24 +0100

    allow scratch

--- script_org/1flow_NAO_composite_range/5var_NAO_range_distribution.py
+++ script_org/1flow_NAO_composite_range/5var_NAO_range_distribution.py
@@ -46,6 +46,7 @@ name = sys.argv[3] if len(sys.argv) > 3 else var
 model_dir = sys.argv[4] if len(sys.argv) > 4 else 'MPI_GE_CMIP6'
 plev = int(sys.argv[5]) if len(sys.argv) > 5 else None
 suffix = sys.argv[6] if len(sys.argv) > 6 else ''
+base_dir = sys.argv[7] if len(sys.argv) > 7 else '/work/mh0033/m300883/High_frequecy_flow/data/'
 
 # report the input
 logging.info(f"Rank {rank} of {size} is processing {var} for decade {decade}, model_dir {model_dir}, name {name}, suffix {suffix}, plev {plev}")
