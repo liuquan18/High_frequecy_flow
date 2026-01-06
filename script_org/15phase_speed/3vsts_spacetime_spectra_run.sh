8ce6a31aa9c9cf5f94fa9f4cbe7a79918b35aab0
Quan <quan.liu@mpimet.mpg.de>
2026-01-06 17:11:48 +0100

    nodes

--- script_org/15phase_speed/3vsts_spacetime_spectra_run.sh
+++ script_org/15phase_speed/3vsts_spacetime_spectra_run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=spacetime
 #SBATCH --output=spacetime.%j.out
-#SBATCH --nodes=10
-#SBATCH --ntasks-per-node=1
-#SBATCH --time=08:00:00
+#SBATCH --nodes=5
+#SBATCH --ntasks-per-node=2
+#SBATCH --time=02:00:00
 #SBATCH --partition=compute
 #SBATCH --mem=0
 #SBATCH --account=mh0033
