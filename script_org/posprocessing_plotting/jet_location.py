d61697ca6d5042f9a5d3a1c089d486f7c7b72696
Quan <quan.liu@mpimet.mpg.de>
2026-04-13 11:03:55 +0200

    for all decades

--- script_org/posprocessing_plotting/jet_location.py
+++ script_org/posprocessing_plotting/jet_location.py
@@ -3,37 +3,31 @@ import xarray as xr
 import pandas as pd
 import numpy as np
 # %%
-ua_first = xr.open_dataset("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6_allplev/ua_hat_monthly_ensmean/ua_hat_monmean_ensmean_185005_185909.nc")
-ua_last = xr.open_dataset("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6_allplev/ua_hat_monthly_ensmean/ua_hat_monmean_ensmean_209005_209909.nc")
-# %%
-eddy_ua_first = ua_first.sel(plev = slice(100000, 85000))
-eddy_ua_last = ua_last.sel(plev = slice(100000, 85000))
+def _eddy_jet (ua):
+    ua = ua.assign_coords(lon=(ua.lon + 180) % 360 - 180).sortby("lon") # -180 to 180
+    ua = ua.sel(lon=slice(-90, 40),lat = slice(0, 90)) # NAO region, the same for composite mean
+    ua = ua.groupby("time.year").mean(dim="time") # annual mean
+    return ua.sel(plev = slice(100000, 85000)).mean(dim = ('plev', 'lon')) # zonal mean eddy driven jet
 
-# %%
-eddy_ua_first = eddy_ua_first.ua.mean(dim = ('time', 'plev'))
-eddy_ua_last = eddy_ua_last.ua.mean(dim = ('time', 'plev'))
+def jet_latitude(ua):
+    # average over lon if present, then find lat of max ua
+    if "lon" in ua.dims:
+        ua = ua.mean(dim="lon")
+    jet_lat = ua.idxmax(dim="lat")
+    jet_lat_df = jet_lat.to_dataframe("jet_lat").reset_index()
+    return jet_lat_df
+
 #%%
-# only northern hemisphere
-eddy_ua_first = eddy_ua_first.sel(lat = slice(0, 90))
-eddy_ua_last = eddy_ua_last.sel(lat = slice(0, 90))
-# %%
-def jet_location(ua):
-    """Return the latitude of the maximum zonal mean zonal wind.
+base_dir = "/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6_allplev/ua_monthly_ensmean"
+ua_decades = xr.open_mfdataset(base_dir + "/*.nc", combine='by_coords', preprocess=_eddy_jet)
 
-    Parameters
-    ----------
-    ua : xr.DataArray
-        Zonal wind with at least a 'lat' dimension.
-
-    Returns
-    -------
-    float or xr.DataArray
-        Latitude of the jet stream maximum.
-    """
-    ua_zm = ua.mean(dim='lon') if 'lon' in ua.dims else ua
-    return ua_zm.lat.isel(lat=ua_zm.argmax(dim='lat'))
-
+#%%
+# asign the decade coordinate, 1859 to 1850
+ua_decades = ua_decades.assign_coords(decade=ua_decades['year'] // 10 * 10)
+#%%
+jet_lat_df = jet_latitude(ua_decades.ua)
+#%%
+jet_lat_df.plot(x = 'decade', y = 'jet_lat')
 # %%
-jet_loc_first = jet_location(eddy_ua_first) # 47.56392575
-jet_loc_last = jet_location(eddy_ua_last) # 49.4291537
-# %%
+# save
+jet_lat_df.to_csv("/work/mh0033/m300883/High_frequecy_flow/data/MPI_GE_CMIP6_allplev/0flux_climatology/jet_latitude_alldecades.csv", index=False)
