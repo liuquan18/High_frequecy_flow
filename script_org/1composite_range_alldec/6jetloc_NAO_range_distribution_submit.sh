0a6067dad9c0eb9e6edb4077f72f11d02c1d2cc1
Quan <quan.liu@mpimet.mpg.de>
2026-04-20 13:36:19 +0200

    name

--- script_org/1composite_range_alldec/6jetloc_NAO_range_distribution_submit.sh
+++ script_org/1composite_range_alldec/6jetloc_NAO_range_distribution_submit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=NAO_dis
-#SBATCH --time=01:00:00
+#SBATCH --time=00:30:00
 #SBATCH --partition=compute
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -18,4 +18,4 @@ plev=$5
 base_dir=$6
 suffix=$7
 # mpirun -n 5 python 5var_NAO_range_distribution.py $decade $var $name $model_dir $plev $suffix
-python 5var_NAO_range_distribution.py $decade $var $name $model_dir $plev $base_dir $suffix
+python 6jetloc_NAO_range_distribution.py $decade $var $name $model_dir $plev $base_dir $suffix
--- script_org/1composite_range_alldec/6jetloc_master.sh
+++ script_org/1composite_range_alldec/6jetloc_master.sh
@@ -8,5 +8,5 @@ plev=None # all levels
 for decade in {1850..2090..10}
 do
     echo "submit: $decade"
-    sbatch 5var_NAO_range_distribution_submit.sh $decade $var $var_name $model_dir $plev
+    sbatch 6jetloc_NAO_range_distribution_submit.sh $decade $var $var_name $model_dir $plev
 done
