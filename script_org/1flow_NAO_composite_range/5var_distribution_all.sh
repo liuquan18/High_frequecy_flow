457c3670b5131ab37ed4ee43a3bb932c8a45388f
Quan <quan.liu@mpimet.mpg.de>
2026-05-06 14:47:28 +0200

    all variable to composite

--- script_org/1flow_NAO_composite_range/5var_distribution_all.sh
+++ script_org/1flow_NAO_composite_range/5var_distribution_all.sh
@@ -25,7 +25,7 @@ sbatch 5var_NAO_range_distribution_submit.sh 1850 M2_steady M2 MPI_GE_CMIP6_allp
 sbatch 5var_NAO_range_distribution_submit.sh 2090 M2_steady M2 MPI_GE_CMIP6_allplev
 
 
-
+# EP flux divergence with ano
 sbatch ./5var_NAO_range_distribution_submit.sh 1850 Fdiv_p_transient div2 MPI_GE_CMIP6_allplev 25000 /work/mh0033/m300883/High_frequecy_flow/data/ _ano
 sbatch ./5var_NAO_range_distribution_submit.sh 2090 Fdiv_p_transient div2 MPI_GE_CMIP6_allplev 25000 /work/mh0033/m300883/High_frequecy_flow/data/ _ano
 
@@ -37,3 +37,19 @@ sbatch ./5var_NAO_range_distribution_submit.sh 2090 Fdiv_p_steady div2 MPI_GE_CM
 
 sbatch ./5var_NAO_range_distribution_submit.sh 1850 Fdiv_phi_steady div MPI_GE_CMIP6_allplev 25000 /work/mh0033/m300883/High_frequecy_flow/data/ _ano
 sbatch ./5var_NAO_range_distribution_submit.sh 2090 Fdiv_phi_steady div MPI_GE_CMIP6_allplev 25000 /work/mh0033/m300883/High_frequecy_flow/data/ _ano
+
+# eke with ano
+sbatch ./5var_NAO_range_distribution_submit.sh 1850 eke eke MPI_GE_CMIP6 25000 /work/mh0033/m300883/High_frequecy_flow/data/ _ano
+sbatch ./5var_NAO_range_distribution_submit.sh 2090 eke eke MPI_GE_CMIP6 25000 /work/mh0033/m300883/High_frequecy_flow/data/ _ano
+
+# baroclinicity with ano
+sbatch ./5var_NAO_range_distribution_submit.sh 1850 eady_growth_rate eady_growth_rate MPI_GE_CMIP6_allplev 85000 /work/mh0033/m300883/High_frequecy_flow/data/ _ano
+sbatch ./5var_NAO_range_distribution_submit.sh 2090 eady_growth_rate eady_growth_rate MPI_GE_CMIP6_allplev 85000 /work/mh0033/m300883/High_frequecy_flow/data/ _ano
+
+# transient eddy heat d2y2
+sbatch ./5var_NAO_range_distribution_submit.sh 1850 transient_eddy_heat_d2y2 eddy_heat_d2y2 MPI_GE_CMIP6_allplev 85000 /work/mh0033/m300883/High_frequecy_flow/data/ _ano
+sbatch ./5var_NAO_range_distribution_submit.sh 2090 transient_eddy_heat_d2y2 eddy_heat_d2y2 MPI_GE_CMIP6_allplev 85000 /work/mh0033/m300883/High_frequecy_flow/data/ _ano
+
+# steady eddy heat d2y2
+sbatch ./5var_NAO_range_distribution_submit.sh 1850 steady_eddy_heat_d2y2 eddy_heat_d2y2 MPI_GE_CMIP6_allplev 85000 /work/mh0033/m300883/High_frequecy_flow/data/ _ano
+sbatch ./5var_NAO_range_distribution_submit.sh 2090 steady_eddy_heat_d2y2 eddy_heat_d2y2 MPI_GE_CMIP6_allplev 85000 /work/mh0033/m300883/High_frequecy_flow/data/ _ano
