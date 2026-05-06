d49859667ed9dd2f59307be0d379eaa030676c3c
Quan <quan.liu@mpimet.mpg.de>
2026-05-06 11:54:33 +0200

    add ano

--- script_org/1flow_NAO_composite_range/5var_distribution_all.sh
+++ script_org/1flow_NAO_composite_range/5var_distribution_all.sh
@@ -26,14 +26,14 @@ sbatch 5var_NAO_range_distribution_submit.sh 2090 M2_steady M2 MPI_GE_CMIP6_allp
 
 
 
-sbatch ./5var_NAO_range_distribution_submit.sh 1850 Fdiv_p_transient div2 MPI_GE_CMIP6_allplev 85000 
-sbatch ./5var_NAO_range_distribution_submit.sh 2090 Fdiv_p_transient div2 MPI_GE_CMIP6_allplev 85000 
+sbatch ./5var_NAO_range_distribution_submit.sh 1850 Fdiv_p_transient div2 MPI_GE_CMIP6_allplev 25000 /work/mh0033/m300883/High_frequecy_flow/data/ _ano
+sbatch ./5var_NAO_range_distribution_submit.sh 2090 Fdiv_p_transient div2 MPI_GE_CMIP6_allplev 25000 /work/mh0033/m300883/High_frequecy_flow/data/ _ano
 
-sbatch ./5var_NAO_range_distribution_submit.sh 1850 Fdiv_phi_transient div MPI_GE_CMIP6_allplev 25000 
-sbatch ./5var_NAO_range_distribution_submit.sh 2090 Fdiv_phi_transient div MPI_GE_CMIP6_allplev 25000 
+sbatch ./5var_NAO_range_distribution_submit.sh 1850 Fdiv_phi_transient div MPI_GE_CMIP6_allplev 25000 /work/mh0033/m300883/High_frequecy_flow/data/ _ano
+sbatch ./5var_NAO_range_distribution_submit.sh 2090 Fdiv_phi_transient div MPI_GE_CMIP6_allplev 25000 /work/mh0033/m300883/High_frequecy_flow/data/ _ano
 
-sbatch ./5var_NAO_range_distribution_submit.sh 1850 Fdiv_p_steady div2 MPI_GE_CMIP6_allplev 85000 
-sbatch ./5var_NAO_range_distribution_submit.sh 2090 Fdiv_p_steady div2 MPI_GE_CMIP6_allplev 85000 
+sbatch ./5var_NAO_range_distribution_submit.sh 1850 Fdiv_p_steady div2 MPI_GE_CMIP6_allplev 25000 /work/mh0033/m300883/High_frequecy_flow/data/ _ano   
+sbatch ./5var_NAO_range_distribution_submit.sh 2090 Fdiv_p_steady div2 MPI_GE_CMIP6_allplev 25000 /work/mh0033/m300883/High_frequecy_flow/data/ _ano
 
-sbatch ./5var_NAO_range_distribution_submit.sh 1850 Fdiv_phi_steady div MPI_GE_CMIP6_allplev 25000 
-sbatch ./5var_NAO_range_distribution_submit.sh 2090 Fdiv_phi_steady div MPI_GE_CMIP6_allplev 25000 
+sbatch ./5var_NAO_range_distribution_submit.sh 1850 Fdiv_phi_steady div MPI_GE_CMIP6_allplev 25000 /work/mh0033/m300883/High_frequecy_flow/data/ _ano
+sbatch ./5var_NAO_range_distribution_submit.sh 2090 Fdiv_phi_steady div MPI_GE_CMIP6_allplev 25000 /work/mh0033/m300883/High_frequecy_flow/data/ _ano
