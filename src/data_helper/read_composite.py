6ab0883336e570d1bba3c73ea72cefff0bbb6608
Quan <quan.liu@mpimet.mpg.de>
2026-05-07 15:01:25 +0200

    fixed path

--- src/data_helper/read_composite.py
+++ src/data_helper/read_composite.py
@@ -24,9 +24,8 @@ def read_comp_var(var, phase, decade, time_window=(-5, 5), **kwargs):
     basedir = (
         f"/work/mh0033/m300883/High_frequecy_flow/data/{model_dir}/{comp_path}/"
     )
-    file_path = basedir + f"{var}{suffix}*{phase}_{decade}.nc"
-    file = glob(file_path)[0]
-    ds = xr.open_dataset(file, chunks=chunks)[name]
+    file_path = basedir + f"{var}{suffix}_NAO_{phase}_{decade}.nc"
+    ds = xr.open_dataset(file_path, chunks=chunks)[name]
     ds = ds.sel(time=slice(*time_window))
     if erase_empty:
         ds = erase_white_line(ds)
